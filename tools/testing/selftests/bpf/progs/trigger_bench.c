7c04a7a21cbd91fd9eb0b2999adb3f425bf9be2f
Toke Høiland-Jørgensen <toke@redhat.com>
2020-09-22 20:38:45 +0200

    selftests: Remove fmod_ret from benchmarks and test_overhead
    The benchmark code and the test_overhead prog_test included fmod_ret
    programs that attached to various functions in the kernel. However, these
    functions were never listed as allowed for return modification, so this
    only worked because of the verifier skipping tests when a trampoline
    already existed for the attach point. Now that the verifier checks have
    been fixed, remove fmod_ret from the affected tests so they all work again.
    
    Fixes: 4eaf0b5c5e04 ("selftest/bpf: Fmod_ret prog and implement test_overhead as part of bench")
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Acked-by: Andrii Nakryiko <andriin@fb.com>

--- tools/testing/selftests/bpf/progs/trigger_bench.c
+++ tools/testing/selftests/bpf/progs/trigger_bench.c
@@ -45,10 +45,3 @@ int bench_trigger_fentry_sleep(void *ctx)
 	__sync_add_and_fetch(&hits, 1);
 	return 0;
 }
-
-SEC("fmod_ret/__x64_sys_getpgid")
-int bench_trigger_fmodret(void *ctx)
-{
-	__sync_add_and_fetch(&hits, 1);
-	return -22;
-}
