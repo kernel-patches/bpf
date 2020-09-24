ed7f86b06523fbd7f95211d311cc23a16675edf7
Bimmy Pujari <bimmy.pujari@intel.com>
2020-09-23 19:25:56 -0700

    bpf: Add bpf_ktime_get_real_ns
    The existing bpf helper functions to get timestamp return the time
    elapsed since system boot. This timestamp is not particularly useful
    where epoch timestamp is required or more than one server is involved
    and time sync is required. Instead, you want to use CLOCK_REALTIME,
    which provides epoch timestamp.
    Hence add bfp_ktime_get_real_ns() based around CLOCK_REALTIME.
    
    Signed-off-by: Ashkan Nikravesh <ashkan.nikravesh@intel.com>
    Signed-off-by: Bimmy Pujari <bimmy.pujari@intel.com>
    Reviewed-by: Maciej Żenczykowski <maze@google.com>

--- kernel/bpf/helpers.c
+++ kernel/bpf/helpers.c
@@ -166,6 +166,17 @@ const struct bpf_func_proto bpf_ktime_get_boot_ns_proto = {
 	.gpl_only	= false,
 	.ret_type	= RET_INTEGER,
 };
+BPF_CALL_0(bpf_ktime_get_real_ns)
+{
+	/* NMI safe access to clock realtime */
+	return ktime_get_real_fast_ns();
+}
+
+const struct bpf_func_proto bpf_ktime_get_real_ns_proto = {
+	.func		= bpf_ktime_get_real_ns,
+	.gpl_only	= false,
+	.ret_type	= RET_INTEGER,
+};
 
 BPF_CALL_0(bpf_get_current_pid_tgid)
 {
@@ -657,6 +668,8 @@ bpf_base_func_proto(enum bpf_func_id func_id)
 		return &bpf_ktime_get_ns_proto;
 	case BPF_FUNC_ktime_get_boot_ns:
 		return &bpf_ktime_get_boot_ns_proto;
+	case BPF_FUNC_ktime_get_real_ns:
+		return &bpf_ktime_get_real_ns_proto;
 	case BPF_FUNC_ringbuf_output:
 		return &bpf_ringbuf_output_proto;
 	case BPF_FUNC_ringbuf_reserve:
