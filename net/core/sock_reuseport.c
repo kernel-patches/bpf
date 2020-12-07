f4d9e73c958620e8af42108298a5c11ffddc57c7
Kuniyuki Iwashima <kuniyu@amazon.co.jp>
2020-12-07 22:24:44 +0900

    tcp: Allow TCP_CLOSE sockets to hold the reuseport group.
    This patch is a preparation patch to migrate incoming connections in the
    later commits and adds a field (num_closed_socks) to the struct
    sock_reuseport to allow TCP_CLOSE sockets to access to the reuseport group.
    
    When we close a listening socket, to migrate its connections to another
    listener in the same reuseport group, we have to handle two kinds of child
    sockets. One is that a listening socket has a reference to, and the other
    is not.
    
    The former is the TCP_ESTABLISHED/TCP_SYN_RECV sockets, and they are in the
    accept queue of their listening socket. So, we can pop them out and push
    them into another listener's queue at close() or shutdown() syscalls. On
    the other hand, the latter, the TCP_NEW_SYN_RECV socket is during the
    three-way handshake and not in the accept queue. Thus, we cannot access
    such sockets at close() or shutdown() syscalls. Accordingly, we have to
    migrate immature sockets after their listening socket has been closed.
    
    Currently, if their listening socket has been closed, TCP_NEW_SYN_RECV
    sockets are freed at receiving the final ACK or retransmitting SYN+ACKs. At
    that time, if we could select a new listener from the same reuseport group,
    no connection would be aborted. However, it is impossible because
    reuseport_detach_sock() sets NULL to sk_reuseport_cb and forbids access to
    the reuseport group from closed sockets.
    
    This patch allows TCP_CLOSE sockets to hold sk_reuseport_cb while any child
    socket references to them. The point is that reuseport_detach_sock() is
    called twice from inet_unhash() and sk_destruct(). At first, it decrements
    num_socks and increments num_closed_socks. Later, when all migrated
    connections are accepted, it decrements num_closed_socks and sets NULL to
    sk_reuseport_cb.
    
    By this change, closed sockets can keep sk_reuseport_cb until all child
    requests have been freed or accepted. Consequently calling listen() after
    shutdown() can cause EADDRINUSE or EBUSY in reuseport_add_sock() or
    inet_csk_bind_conflict() which expect that such sockets should not have the
    reuseport group. Therefore, this patch also loosens such validation rules
    so that the socket can listen again if it has the same reuseport group with
    other listening sockets.
    
    Reviewed-by: Benjamin Herrenschmidt <benh@amazon.com>
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.co.jp>

--- net/core/sock_reuseport.c
+++ net/core/sock_reuseport.c
@@ -98,14 +98,15 @@ static struct sock_reuseport *reuseport_grow(struct sock_reuseport *reuse)
 		return NULL;
 
 	more_reuse->num_socks = reuse->num_socks;
+	more_reuse->num_closed_socks = reuse->num_closed_socks;
 	more_reuse->prog = reuse->prog;
 	more_reuse->reuseport_id = reuse->reuseport_id;
 	more_reuse->bind_inany = reuse->bind_inany;
 	more_reuse->has_conns = reuse->has_conns;
+	more_reuse->synq_overflow_ts = READ_ONCE(reuse->synq_overflow_ts);
 
 	memcpy(more_reuse->socks, reuse->socks,
 	       reuse->num_socks * sizeof(struct sock *));
-	more_reuse->synq_overflow_ts = READ_ONCE(reuse->synq_overflow_ts);
 
 	for (i = 0; i < reuse->num_socks; ++i)
 		rcu_assign_pointer(reuse->socks[i]->sk_reuseport_cb,
@@ -152,8 +153,10 @@ int reuseport_add_sock(struct sock *sk, struct sock *sk2, bool bind_inany)
 	reuse = rcu_dereference_protected(sk2->sk_reuseport_cb,
 					  lockdep_is_held(&reuseport_lock));
 	old_reuse = rcu_dereference_protected(sk->sk_reuseport_cb,
-					     lockdep_is_held(&reuseport_lock));
-	if (old_reuse && old_reuse->num_socks != 1) {
+					      lockdep_is_held(&reuseport_lock));
+	if (old_reuse == reuse) {
+		reuse->num_closed_socks--;
+	} else if (old_reuse && old_reuse->num_socks != 1) {
 		spin_unlock_bh(&reuseport_lock);
 		return -EBUSY;
 	}
@@ -174,8 +177,9 @@ int reuseport_add_sock(struct sock *sk, struct sock *sk2, bool bind_inany)
 
 	spin_unlock_bh(&reuseport_lock);
 
-	if (old_reuse)
+	if (old_reuse && old_reuse != reuse)
 		call_rcu(&old_reuse->rcu, reuseport_free_rcu);
+
 	return 0;
 }
 EXPORT_SYMBOL(reuseport_add_sock);
@@ -199,17 +203,28 @@ void reuseport_detach_sock(struct sock *sk)
 	 */
 	bpf_sk_reuseport_detach(sk);
 
-	rcu_assign_pointer(sk->sk_reuseport_cb, NULL);
-
-	for (i = 0; i < reuse->num_socks; i++) {
-		if (reuse->socks[i] == sk) {
-			reuse->socks[i] = reuse->socks[reuse->num_socks - 1];
-			reuse->num_socks--;
-			if (reuse->num_socks == 0)
-				call_rcu(&reuse->rcu, reuseport_free_rcu);
+	if (sk->sk_protocol == IPPROTO_TCP && sk->sk_state == TCP_CLOSE) {
+		reuse->num_closed_socks--;
+		rcu_assign_pointer(sk->sk_reuseport_cb, NULL);
+	} else {
+		for (i = 0; i < reuse->num_socks; i++) {
+			if (reuse->socks[i] != sk)
+				continue;
 			break;
 		}
+
+		reuse->num_socks--;
+		reuse->socks[i] = reuse->socks[reuse->num_socks];
+
+		if (sk->sk_protocol == IPPROTO_TCP)
+			reuse->num_closed_socks++;
+		else
+			rcu_assign_pointer(sk->sk_reuseport_cb, NULL);
 	}
+
+	if (reuse->num_socks + reuse->num_closed_socks == 0)
+		call_rcu(&reuse->rcu, reuseport_free_rcu);
+
 	spin_unlock_bh(&reuseport_lock);
 }
 EXPORT_SYMBOL(reuseport_detach_sock);
