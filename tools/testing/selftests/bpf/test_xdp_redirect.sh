f08b5136f410a7202de02fcde12f4e125bd04bfe
Björn Töpel <bjorn.topel@intel.com>
2021-02-06 10:26:54 +0100

    selftests/bpf: remove bash feature in test_xdp_redirect.sh
    The test_xdp_redirect.sh script uses a bash redirect feature,
    '&>/dev/null'. Use '>/dev/null 2>&1' instead.
    
    Also remove the 'set -e' since the script actually relies on that the
    return value can be used to determine pass/fail of the test.
    
    Acked-by: William Tu <u9012063@gmail.com>
    Fixes: 996139e801fd ("selftests: bpf: add a test for XDP redirect")
    Signed-off-by: Björn Töpel <bjorn.topel@intel.com>

--- tools/testing/selftests/bpf/test_xdp_redirect.sh
+++ tools/testing/selftests/bpf/test_xdp_redirect.sh
@@ -46,20 +46,20 @@ test_xdp_redirect()
 
 	setup
 
-	ip link set dev veth1 $xdpmode off &> /dev/null
+	ip link set dev veth1 $xdpmode off >/dev/null 2>&1
 	if [ $? -ne 0 ];then
 		echo "selftests: test_xdp_redirect $xdpmode [SKIP]"
 		return 0
 	fi
 
-	ip -n ns1 link set veth11 $xdpmode obj xdp_dummy.o sec xdp_dummy &> /dev/null
-	ip -n ns2 link set veth22 $xdpmode obj xdp_dummy.o sec xdp_dummy &> /dev/null
-	ip link set dev veth1 $xdpmode obj test_xdp_redirect.o sec redirect_to_222 &> /dev/null
-	ip link set dev veth2 $xdpmode obj test_xdp_redirect.o sec redirect_to_111 &> /dev/null
+	ip -n ns1 link set veth11 $xdpmode obj xdp_dummy.o sec xdp_dummy >/dev/null 2>&1
+	ip -n ns2 link set veth22 $xdpmode obj xdp_dummy.o sec xdp_dummy >/dev/null 2>&1
+	ip link set dev veth1 $xdpmode obj test_xdp_redirect.o sec redirect_to_222 >/dev/null 2>&1
+	ip link set dev veth2 $xdpmode obj test_xdp_redirect.o sec redirect_to_111 >/dev/null 2>&1
 
-	ip netns exec ns1 ping -c 1 10.1.1.22 &> /dev/null
+	ip netns exec ns1 ping -c 1 10.1.1.22 >/dev/null 2>&1
 	local ret1=$?
-	ip netns exec ns2 ping -c 1 10.1.1.11 &> /dev/null
+	ip netns exec ns2 ping -c 1 10.1.1.11 >/dev/null 2>&1
 	local ret2=$?
 
 	if [ $ret1 -eq 0 -a $ret2 -eq 0 ]; then
@@ -72,7 +72,6 @@ test_xdp_redirect()
 	cleanup
 }
 
-set -e
 trap cleanup 2 3 6 9
 
 test_xdp_redirect xdpgeneric
