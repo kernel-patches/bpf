bd4abdad41a84292d0f722332f05edc944e92c1d
Manu Bretelle <chantra@fb.com>
2021-03-10 10:23:05 -0800

    bpf: Add getter and setter for SO_REUSEPORT through bpf_{g,s}etsockopt
    Augment the current set of options that are accessible via
    bpf_{g,s}etsockopt to also support SO_REUSEPORT.
    
    Signed-off-by: Manu Bretelle <chantra@fb.com>
    Acked-by: Martin KaFai Lau <kafai@fb.com>

--- net/core/filter.c
+++ net/core/filter.c
@@ -4787,6 +4787,9 @@ static int _bpf_setsockopt(struct sock *sk, int level, int optname,
 				sk->sk_prot->keepalive(sk, valbool);
 			sock_valbool_flag(sk, SOCK_KEEPOPEN, valbool);
 			break;
+		case SO_REUSEPORT:
+			sk->sk_reuseport = valbool;
+			break;
 		default:
 			ret = -EINVAL;
 		}
@@ -4955,6 +4958,9 @@ static int _bpf_getsockopt(struct sock *sk, int level, int optname,
 			break;
 		case SO_BINDTOIFINDEX:
 			*((int *)optval) = sk->sk_bound_dev_if;
+			break;
+		case SO_REUSEPORT:
+			*((int *)optval) = sk->sk_reuseport;
 			break;
 		default:
 			goto err_clear;
