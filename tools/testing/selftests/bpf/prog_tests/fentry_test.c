8d01704ddb34c34f576cb400f2e19c56e8a2b870
Jiri Olsa <jolsa@kernel.org>
2021-04-06 23:29:10 +0200

    selftests/bpf: Add re-attach test to fentry_test
    Adding the test to re-attach (detach/attach again) tracing
    fentry programs, plus check that already linked program can't
    be attached again.
    
    Fixing the number of check-ed results, which should be 8.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/testing/selftests/bpf/prog_tests/fentry_test.c
+++ tools/testing/selftests/bpf/prog_tests/fentry_test.c
@@ -3,20 +3,24 @@
 #include <test_progs.h>
 #include "fentry_test.skel.h"
 
-void test_fentry_test(void)
-{
-	struct fentry_test *fentry_skel = NULL;
-	int err, prog_fd, i;
-	__u32 duration = 0, retval;
-	__u64 *result;
+static __u32 duration;
 
-	fentry_skel = fentry_test__open_and_load();
-	if (CHECK(!fentry_skel, "fentry_skel_load", "fentry skeleton failed\n"))
-		goto cleanup;
+static int fentry_test(struct fentry_test *fentry_skel)
+{
+	struct bpf_link *link;
+	int err, prog_fd, i;
+	__u64 *result;
+	__u32 retval;
 
 	err = fentry_test__attach(fentry_skel);
 	if (CHECK(err, "fentry_attach", "fentry attach failed: %d\n", err))
-		goto cleanup;
+		return err;
+
+	/* Check that already linked program can't be attached again. */
+	link = bpf_program__attach(fentry_skel->progs.test1);
+	if (CHECK(!IS_ERR(link), "fentry_attach_link",
+		  "re-attach without detach should not succeed"))
+		return -1;
 
 	prog_fd = bpf_program__fd(fentry_skel->progs.test1);
 	err = bpf_prog_test_run(prog_fd, 1, NULL, 0,
@@ -26,11 +30,35 @@ void test_fentry_test(void)
 	      err, errno, retval, duration);
 
 	result = (__u64 *)fentry_skel->bss;
-	for (i = 0; i < 6; i++) {
+	for (i = 0; i < 8; i++) {
 		if (CHECK(result[i] != 1, "result",
 			  "fentry_test%d failed err %lld\n", i + 1, result[i]))
-			goto cleanup;
+			return -1;
 	}
+
+	fentry_test__detach(fentry_skel);
+
+	/* zero results for re-attach test */
+	for (i = 0; i < 8; i++)
+		result[i] = 0;
+	return 0;
+}
+
+void test_fentry_test(void)
+{
+	struct fentry_test *fentry_skel = NULL;
+	int err;
+
+	fentry_skel = fentry_test__open_and_load();
+	if (CHECK(!fentry_skel, "fentry_skel_load", "fentry skeleton failed\n"))
+		goto cleanup;
+
+	err = fentry_test(fentry_skel);
+	if (CHECK(err, "fentry_test", "first attach failed\n"))
+		goto cleanup;
+
+	err = fentry_test(fentry_skel);
+	CHECK(err, "fentry_test", "second attach failed\n");
 
 cleanup:
 	fentry_test__destroy(fentry_skel);
