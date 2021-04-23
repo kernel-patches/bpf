e41cc5d254f8f5caf4b4a4f0460d7f83f034b0a1
Toke Høiland-Jørgensen <toke@redhat.com>
2021-04-23 13:05:17 +0200

    dev: add rcu_read_lock_bh_held() as a valid check when getting a RCU dev ref
    Some of the XDP helpers (in particular, xdp_do_redirect()) will get a
    struct net_device reference using dev_get_by_index_rcu(). These are called
    from a NAPI poll context, which means the RCU reference liveness is ensured
    by local_bh_disable(). Add rcu_read_lock_bh_held() as a condition to the
    RCU list traversal in dev_get_by_index_rcu() so lockdep understands that
    the dereferences are safe from *both* an rcu_read_lock() *and* with
    local_bh_disable().
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -1002,7 +1002,7 @@ struct net_device *dev_get_by_index_rcu(struct net *net, int ifindex)
 	struct net_device *dev;
 	struct hlist_head *head = dev_index_hash(net, ifindex);
 
-	hlist_for_each_entry_rcu(dev, head, index_hlist)
+	hlist_for_each_entry_rcu(dev, head, index_hlist, rcu_read_lock_bh_held())
 		if (dev->ifindex == ifindex)
 			return dev;
 
