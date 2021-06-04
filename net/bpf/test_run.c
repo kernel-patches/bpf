6bb2507bc429439935d6b533ed75007b32977150
Zvi Effron <zeffron@riotgames.com>
2021-06-04 22:02:34 +0000

    bpf: support specifying ingress via xdp_md context in BPF_PROG_TEST_RUN
    Support specifying the ingress_ifindex and rx_queue_index of xdp_md
    contexts for BPF_PROG_TEST_RUN.
    
    The intended use case is to allow testing XDP programs that make decisions
    based on the ingress interface or RX queue.
    
    If ingress_ifindex is specified, look up the device by the provided index
    in the current namespace and use its xdp_rxq for the xdp_buff. If the
    rx_queue_index is out of range, or is non-zero when the ingress_ifindex is
    0, return EINVAL.
    
    Co-developed-by: Cody Haas <chaas@riotgames.com>
    Signed-off-by: Cody Haas <chaas@riotgames.com>
    Co-developed-by: Lisa Watanabe <lwatanabe@riotgames.com>
    Signed-off-by: Lisa Watanabe <lwatanabe@riotgames.com>
    Signed-off-by: Zvi Effron <zeffron@riotgames.com>

--- net/bpf/test_run.c
+++ net/bpf/test_run.c
@@ -690,6 +690,8 @@ out:
 static int xdp_convert_md_to_buff(struct xdp_buff *xdp, struct xdp_md *xdp_md)
 {
 	void *data;
+	struct net_device *device;
+	struct netdev_rx_queue *rxqueue;
 
 	if (!xdp_md)
 		return 0;
@@ -702,8 +704,20 @@ static int xdp_convert_md_to_buff(struct xdp_buff *xdp, struct xdp_md *xdp_md)
 
 	xdp->data = xdp->data_meta + xdp_md->data;
 
-	if (xdp_md->ingress_ifindex != 0 || xdp_md->rx_queue_index != 0)
+	if (!xdp_md->ingress_ifindex && xdp_md->rx_queue_index)
 		return -EINVAL;
+
+	if (xdp_md->ingress_ifindex) {
+		device = dev_get_by_index(current->nsproxy->net_ns, xdp_md->ingress_ifindex);
+		if (!device)
+			return -EINVAL;
+
+		if (xdp_md->rx_queue_index >= device->real_num_rx_queues)
+			return -EINVAL;
+
+		rxqueue = __netif_get_rx_queue(device, xdp_md->rx_queue_index);
+		xdp->rxq = &rxqueue->xdp_rxq;
+	}
 
 	return 0;
 }
