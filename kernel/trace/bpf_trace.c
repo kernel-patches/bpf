08471123ec959c4f4af463e40624dfc9966b6b51
Hengqi Chen <hengqi.chen@gmail.com>
2021-07-13 00:24:24 +0800

    bpf: Expose bpf_d_path helper to vfs_read/vfs_write
    Add vfs_read and vfs_write to bpf_d_path allowlist.
    This will help tools like IOVisor's filetop to get
    full file path.
    
    Signed-off-by: Hengqi Chen <hengqi.chen@gmail.com>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Acked-by: Yonghong Song <yhs@fb.com>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -861,6 +861,8 @@ BTF_ID(func, vfs_fallocate)
 BTF_ID(func, dentry_open)
 BTF_ID(func, vfs_getattr)
 BTF_ID(func, filp_close)
+BTF_ID(func, vfs_read)
+BTF_ID(func, vfs_write)
 BTF_SET_END(btf_allowlist_d_path)
 
 static bool bpf_d_path_allowed(const struct bpf_prog *prog)
