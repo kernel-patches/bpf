a19f7386ab960dcb8bddcb153e3814b27a35add9
Ivan Khoronzhuk <ivan.khoronzhuk@linaro.org>
2021-07-27 16:11:17 +0200

    arm: include: asm: swab: mask rev16 instruction for clang
    The samples/bpf with clang -emit-llvm reuses linux headers to build
    bpf samples, and this w/a only for samples (samples/bpf/Makefile
    CLANG-bpf).
    
    It allows to build samples/bpf for arm bpf using clang.
    In another way clang -emit-llvm generates errors like:
    
    CLANG-bpf  samples/bpf/tc_l2_redirect_kern.o
    <inline asm>:1:2: error: invalid register/token name
    rev16 r3, r0
    
    This decision is arguable, probably there is another way, but
    it doesn't have impact on samples/bpf, so it's easier just ignore
    it for clang, at least for now.
    
    Signed-off-by: Ivan Khoronzhuk <ivan.khoronzhuk@linaro.org>

--- arch/arm/include/asm/swab.h
+++ arch/arm/include/asm/swab.h
@@ -25,8 +25,11 @@ static inline __attribute_const__ __u32 __arch_swahb32(__u32 x)
 	__asm__ ("rev16 %0, %1" : "=r" (x) : "r" (x));
 	return x;
 }
+
+#ifndef __clang__
 #define __arch_swahb32 __arch_swahb32
 #define __arch_swab16(x) ((__u16)__arch_swahb32(x))
+#endif
 
 static inline __attribute_const__ __u32 __arch_swab32(__u32 x)
 {
