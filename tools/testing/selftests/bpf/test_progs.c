a22e9c7c52a08c24d8c219f09cc35e3c891a38c9
Yucong Sun <fallentree@fb.com>
2021-08-09 16:36:31 -0700

    Correctly display subtest skip status
    In skip_account(), test->skip_cnt is set to 0 at the end, this makes next print statment never display SKIP status for the subtest. This patch moves the accounting logic after the print statement, fixing the issue.

--- tools/testing/selftests/bpf/test_progs.c
+++ tools/testing/selftests/bpf/test_progs.c
@@ -238,17 +238,17 @@ void test__end_subtest()
 	struct prog_test_def *test = env.test;
 	int sub_error_cnt = test->error_cnt - test->old_error_cnt;
 
-	if (sub_error_cnt)
-		env.fail_cnt++;
-	else if (test->skip_cnt == 0)
-		env.sub_succ_cnt++;
-	skip_account();
-
 	dump_test_log(test, sub_error_cnt);
 
 	fprintf(env.stdout, "#%d/%d %s:%s\n",
 	       test->test_num, test->subtest_num, test->subtest_name,
 	       sub_error_cnt ? "FAIL" : (test->skip_cnt ? "SKIP" : "OK"));
+
+	if (sub_error_cnt)
+		env.fail_cnt++;
+	else if (test->skip_cnt == 0)
+		env.sub_succ_cnt++;
+	skip_account();
 
 	free(test->subtest_name);
 	test->subtest_name = NULL;
