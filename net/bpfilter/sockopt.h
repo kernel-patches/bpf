d5ce991fd96086fad94d421e8a83355594c7562a
Dmitrii Banshchikov <me@ubique.spb.ru>
2021-08-29 22:36:06 +0400

    bpfilter: Add handling of setsockopt() calls
    Add support of iptables' setsockopt(2).
    The parameters of a setsockopt(2) call are passed by struct mbox_request
    which contains a type of the setsockopt(2) call and its memory buffer
    description. The supplied memory buffer is read-written by
    process_vm_readv(2)/process_vm_writev(2).
    
    Signed-off-by: Dmitrii Banshchikov <me@ubique.spb.ru>

---
+++ net/bpfilter/sockopt.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2021 Telegram FZ-LLC
+ */
+
+#ifndef NET_BPFILTER_SOCKOPT_H
+#define NET_BPFILTER_SOCKOPT_H
+
+struct context;
+struct mbox_request;
+
+int handle_sockopt_request(struct context *ctx, const struct mbox_request *req);
+
+#endif // NET_BPFILTER_SOCKOPT_H
