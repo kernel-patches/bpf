dfe0f4b0ae61672dfcf9b0dad98333ae0d0ea490
Joanne Koong <joannekoong@fb.com>
2021-08-31 15:50:05 -0700

    bpf/benchs: Add benchmarks for comparing hashmap lookups with vs. without bloom filter
    This patch adds some benchmarks for comparing the performance of hashmap
    lookups without the bloom filter vs. hashmap lookups with the bloom filter.
    
    Checking the bloom filter first for whether the element exists should
    overall enable a higher throughput for hashmap lookups, since if the
    element does not exist in the bloom filter, we can avoid a costly lookup in
    the hashmap.
    
    The benchmark results (running on 8 threads on a machine with one numa
    node, and taking the average of 3 runs) were as follows:
    1 hash function -
    	10k entries: 15% slower
    	50k entries: 25% faster
    	100k entries: 30% faster
    	500k entres: 50% faster
    	1 million entries: 55% faster
    	5 million entries: 40% faster
    
    2 hash functions -
    	10k entries: 20% slower
    	50k entries: 40% faster
    	100k entries: 50% faster
    	500k entres: 90% faster
    	1 million entries: 115% faster
    	5 million entries: 70% faster
    
    3 hash functions -
    	10k entries: 5% slower
    	50k entries: 15% faster
    	100k entries: 25% faster
    	500k entres: 180% faster
    	1 million entries: 200% faster
    	5 million entries: 140% faster
    
    4 hash functions -
    	10k entries: 20% slower
    	50k entries: 50% faster
    	100k entries: 60% faster
    	500k entres: 128% faster
    	1 million entries: 150% faster
    	5 million entries: 105% faster
    
    5 hash functions -
    	10k entries: 10% faster
    	50k entries: 25% faster
    	100k entries: 50% faster
    	500k entres: 100% faster
    	1 million entries: 120% faster
    	5 million entries: 170% faster
    
    6 hash functions -
    	10k entries: 1.5% faster
    	50k entries: 20% faster
    	100k entries: 25% faster
    	500k entres: 165% faster
    	1 million entries: 170% faster
    	5 million entries: 140% faster
    
    7 hash functions -
    	10k entries: 7% slower
    	50k entries: 5% faster
    	100k entries: 15% faster
    	500k entres: 145% faster
    	1 million entries: 155% faster
    	5 million entries: 120% faster
    
    8 hash functions -
    	10k entries: 15% slower
    	50k entries: 50% faster
    	100k entries: 60% faster
    	500k entres: 1305% faster
    	1 million entries: 135% faster
    	5 million entries: 105% faster
    
    9 hash functions -
    	10k entries: 25% slower
    	50k entries: 35% faster
    	100k entries: 60% faster
    	500k entres: 105% faster
    	1 million entries: 130% faster
    	5 million entries: 90% faster
    
    10 hash functions -
    	10k entries: 10% faster
    	50k entries: 30% faster
    	100k entries: 45% faster
    	500k entres: 90% faster
    	1 million entries: 105% faster
    	5 million entries: 140% faster
    
    The benchmark comparisions indicate that on average, using 3 hash functions
    generally is one of the most optimal choices.
    
    Signed-off-by: Joanne Koong <joannekoong@fb.com>

--- tools/testing/selftests/bpf/benchs/run_bench_bloom_filter_map.sh
+++ tools/testing/selftests/bpf/benchs/run_bench_bloom_filter_map.sh
@@ -26,3 +26,18 @@ header "Bloom filter map, multi-producer contention"
 for t in 1 2 3 4 8 12 16 20 24 28 32 36 40 44 48 52; do
 	summarize "$t threads - " "$($RUN_BENCH -p $t bloom-filter-map)"
 done
+
+header "Hashmap without bloom filter vs. hashmap with bloom filter (throughput, 8 threads)"
+for h in {1..10}; do
+subtitle "# hashes: $h"
+	for e in 10000 50000 75000 100000 250000 500000 750000 1000000 2500000 5000000; do
+		printf "%'d entries -\n" $e
+		printf "\t"
+		summarize_total "Hashmap without bloom filter: " \
+			"$($RUN_BENCH --nr_hashes $h --nr_entries $e -p 8 hashmap-without-bloom-filter)"
+		printf "\t"
+		summarize_total "Hashmap with bloom filter: " \
+			"$($RUN_BENCH --nr_hashes $h --nr_entries $e -p 8 hashmap-with-bloom-filter)"
+	done
+	printf "\n"
+done
