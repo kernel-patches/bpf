ca702ecf8345a7cdc34a3d3f5596220ab043151c
Joanne Koong <joannekoong@fb.com>
2021-08-31 15:50:04 -0700

    bpf/benchs: Add benchmark test for bloom filter maps
    This patch adds benchmark tests for the throughput and false positive
    rate of bloom filter map lookups for a given number of entries and a
    given number of hash functions.
    
    These benchmarks show that as the number of hash functions increases,
    the throughput and the false positive rate of the bloom filter map
    decreases. From the benchmark data, the approximate average
    false-positive rates are roughly as follows:
    
    1 hash function = ~30%
    2 hash functions = ~15%
    3 hash functions = ~5%
    4 hash functions = ~2.5%
    5 hash functions = ~1%
    6 hash functions = 0.5%
    7 hash functions  = ~0.35%
    8 hash functions = ~0.15%
    9 hash functions = ~0.1%
    10 hash functions = ~0%
    
    Signed-off-by: Joanne Koong <joannekoong@fb.com>

---
+++ tools/testing/selftests/bpf/benchs/run_bench_bloom_filter_map.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+
+source ./benchs/run_common.sh
+
+set -eufo pipefail
+
+header "Bloom filter map"
+for t in 1 4 8; do
+for h in {1..10}; do
+subtitle "# threads: $t, # hashes: $h"
+	for e in 10000 50000 75000 100000 250000 500000 750000 1000000 2500000 5000000; do
+		printf "%'d entries -\n" $e
+		printf "\t"
+		summarize "Total operations: " \
+			"$($RUN_BENCH -p $t --nr_hashes $h --nr_entries $e bloom-filter-map)"
+		printf "\t"
+		summarize_percentage "False positive rate: " \
+			"$($RUN_BENCH -p $t --nr_hashes $h --nr_entries $e bloom-filter-false-positive)"
+	done
+	printf "\n"
+done
+done
+
+header "Bloom filter map, multi-producer contention"
+for t in 1 2 3 4 8 12 16 20 24 28 32 36 40 44 48 52; do
+	summarize "$t threads - " "$($RUN_BENCH -p $t bloom-filter-map)"
+done
