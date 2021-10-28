cd5ea9f21015093a7f64fa2ca0f5a46fe2099a4e
Florent Revest <revest@chromium.org>
2021-10-28 18:43:57 +0200

    bpf: Allow bpf_d_path in perf_event_mmap
    Allow the helper to be called from the perf_event_mmap hook. This is
    convenient to lookup vma->vm_file and implement a similar logic as
    perf_event_mmap_event in BPF.
    
    Signed-off-by: Florent Revest <revest@chromium.org>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -922,6 +922,9 @@ BTF_ID(func, vfs_fallocate)
 BTF_ID(func, dentry_open)
 BTF_ID(func, vfs_getattr)
 BTF_ID(func, filp_close)
+#ifdef CONFIG_PERF_EVENTS
+BTF_ID(func, perf_event_mmap)
+#endif
 BTF_SET_END(btf_allowlist_d_path)
 
 static bool bpf_d_path_allowed(const struct bpf_prog *prog)
