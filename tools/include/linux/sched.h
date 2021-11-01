9c676cabd6faba1cb723e9f69b4a3c73876c741c
Yafang Shao <laoar.shao@gmail.com>
2021-11-01 06:04:16 +0000

    tools/perf/test: make perf test adopt to task comm size change
    kernel test robot reported a perf-test failure after I extended task comm
    size from 16 to 24. The failure as follows,
    
    2021-10-13 18:00:46 sudo /usr/src/perf_selftests-x86_64-rhel-8.3-317419b91ef4eff4e2f046088201e4dc4065caa0/tools/perf/perf test 15
    15: Parse sched tracepoints fields                                  : FAILED!
    
    The reason is perf-test requires a fixed-size task comm. If we extend
    task comm size to 24, it will not equil with the required size 16 in perf
    test.
    
    After some analyzation, I found perf itself can adopt to the size
    change, for example, below is the output of perf-sched after I extend
    comm size to 24 -
    
    task    614 (            kthreadd:        84), nr_events: 1
    task    615 (             systemd:       843), nr_events: 1
    task    616 (     networkd-dispat:      1026), nr_events: 1
    task    617 (             systemd:       846), nr_events: 1
    
    $ cat /proc/843/comm
    networkd-dispatcher
    
    The task comm can be displayed correctly as expected.
    
    Replace old hard-coded 16 with the new one can fix the warning, but we'd
    better make the test accept both old and new sizes, then it can be
    backward compatibility.
    
    After this patch, the perf-test succeeds no matter task comm is 16 or
    24 -
    
    15: Parse sched tracepoints fields                                  : Ok
    
    This patch is a preparation for the followup patch.
    
    Reported-by: kernel test robot <oliver.sang@intel.com>
    Suggested-by: Arnaldo Carvalho de Melo <arnaldo.melo@gmail.com>
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Arnaldo Carvalho de Melo <arnaldo.melo@gmail.com>
    Cc: Alexei Starovoitov <alexei.starovoitov@gmail.com>
    Cc: Andrii Nakryiko <andrii.nakryiko@gmail.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Petr Mladek <pmladek@suse.com>

---
+++ tools/include/linux/sched.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _TOOLS_LINUX_SCHED_H
+#define _TOOLS_LINUX_SCHED_H
+
+/* Keep both length for backward compatibility */
+enum {
+	TASK_COMM_LEN_16 = 16,
+	TASK_COMM_LEN = 24,
+};
+
+#endif  /* _TOOLS_LINUX_SCHED_H */
