24992193382fd8f957ee265be80f19d6b235b6da
Maciej Fijalkowski <maciej.fijalkowski@intel.com>
2021-12-10 18:14:25 +0100

    ice: xsk: borrow xdp_tx_active logic from i40e
    One of the things that commit 5574ff7b7b3d ("i40e: optimize AF_XDP Tx
    completion path") introduced was the @xdp_tx_active field. Its usage
    from i40e can be adjusted to ice driver and give us positive performance
    results.
    
    If the descriptor that @next_dd to points has been sent by HW (its DD
    bit is set), then we are sure that there are ICE_TX_THRESH count of
    descriptors ready to be cleaned. If @xdp_tx_active is 0 which means that
    related xdp_ring is not used for XDP_{TX, REDIRECT} workloads, then we
    know how many XSK entries should placed to completion queue, IOW walking
    through the ring can be skipped.
    
    Signed-off-by: Maciej Fijalkowski <maciej.fijalkowski@intel.com>
    Acked-by: Magnus Karlsson <magnus.karlsson@intel.com>

--- drivers/net/ethernet/intel/ice/ice_xsk.c
+++ drivers/net/ethernet/intel/ice/ice_xsk.c
@@ -686,6 +686,7 @@ static void
 ice_clean_xdp_tx_buf(struct ice_tx_ring *xdp_ring, struct ice_tx_buf *tx_buf)
 {
 	xdp_return_frame((struct xdp_frame *)tx_buf->raw_buf);
+	xdp_ring->xdp_tx_active--;
 	dma_unmap_single(xdp_ring->dev, dma_unmap_addr(tx_buf, dma),
 			 dma_unmap_len(tx_buf, len), DMA_TO_DEVICE);
 	dma_unmap_len_set(tx_buf, len, 0);
@@ -715,6 +716,11 @@ bool ice_clean_tx_irq_zc(struct ice_tx_ring *xdp_ring)
 
 again:
 	xsk_frames = 0;
+	if (likely(!xdp_ring->xdp_tx_active)) {
+		xsk_frames = ICE_TX_THRESH;
+		goto skip;
+	}
+
 	ntc = xdp_ring->next_to_clean;
 
 	for (i = 0; i < ICE_TX_THRESH; i++) {
@@ -731,6 +737,7 @@ again:
 		if (ntc >= xdp_ring->count)
 			ntc = 0;
 	}
+skip:
 	xdp_ring->next_to_clean += ICE_TX_THRESH;
 	if (xdp_ring->next_to_clean >= desc_cnt)
 		xdp_ring->next_to_clean -= desc_cnt;
