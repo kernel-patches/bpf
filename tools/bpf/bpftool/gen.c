defd64cbb85cd92a353c58f25dce14c1f7ce1e4d
Wei Fu <fuweid89@gmail.com>
2022-01-08 16:40:08 +0800

    tools/bpf: only set obj->skeleton without err
    After `bpftool gen skeleton`, the ${bpf_app}.skel.h will provide that
    ${bpf_app_name}__open helper to load bpf. If there is some error
    like ENOMEM, the ${bpf_app_name}__open will rollback(free) the allocated
    object, including `bpf_object_skeleton`.
    
    Since the ${bpf_app_name}__create_skeleton set the obj->skeleton first
    and not rollback it when error, it will cause double-free in
    ${bpf_app_name}__destory at ${bpf_app_name}__open. Therefore, we should
    set the obj->skeleton before return 0;
    
    Signed-off-by: Wei Fu <fuweid89@gmail.com>

--- tools/bpf/bpftool/gen.c
+++ tools/bpf/bpftool/gen.c
@@ -927,7 +927,6 @@ static int do_skeleton(int argc, char **argv)
 			s = (struct bpf_object_skeleton *)calloc(1, sizeof(*s));\n\
 			if (!s)						    \n\
 				goto err;				    \n\
-			obj->skeleton = s;				    \n\
 									    \n\
 			s->sz = sizeof(*s);				    \n\
 			s->name = \"%1$s\";				    \n\
@@ -999,6 +998,8 @@ static int do_skeleton(int argc, char **argv)
 		\n\
 									    \n\
 			s->data = (void *)%2$s__elf_bytes(&s->data_sz);	    \n\
+									    \n\
+			obj->skeleton = s;				    \n\
 									    \n\
 			return 0;					    \n\
 		err:							    \n\
