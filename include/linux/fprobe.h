2db62d1dcce90d18434f878297d788ebd4e02555
Masami Hiramatsu <mhiramat@kernel.org>
2022-01-12 23:03:10 +0900

    fprobe: Add ftrace based probe APIs
    The fprobe is a wrapper API for ftrace function tracer.
    Unlike kprobes, this probes only supports the function entry, but
    it can probe multiple functions by one fprobe. The usage is almost
    same as the kprobe, user will specify the function names by
    fprobe::syms, the number of syms by fprobe::nentry,
    and the user handler by fprobe::entry_handler.
    
    struct fprobe fp = { 0 };
    const char *targets[] = { "func1", "func2", "func3"};
    
    fp.handler = user_handler;
    fp.nentry = ARRAY_SIZE(targets);
    fp.syms = targets;
    
    ret = register_fprobe(&fp);
    
    Signed-off-by: Masami Hiramatsu <mhiramat@kernel.org>

---
+++ include/linux/fprobe.h
@@ -0,0 +1,53 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Simple ftrace probe wrapper */
+#ifndef _LINUX_FPROBE_H
+#define _LINUX_FPROBE_H
+
+#include <linux/compiler.h>
+#include <linux/ftrace.h>
+
+struct fprobe {
+	const char		**syms;
+	unsigned long		*addrs;
+	unsigned int		nentry;
+
+	struct ftrace_ops	ftrace;
+	unsigned long		nmissed;
+	unsigned int		flags;
+	void (*entry_handler)(struct fprobe *fp, unsigned long entry_ip, struct pt_regs *regs);
+};
+
+#define FPROBE_FL_DISABLED	1
+
+static inline bool fprobe_disabled(struct fprobe *fp)
+{
+	return (fp) ? fp->flags & FPROBE_FL_DISABLED : false;
+}
+
+#ifdef CONFIG_FPROBE
+int register_fprobe(struct fprobe *fp);
+int unregister_fprobe(struct fprobe *fp);
+#else
+static inline int register_fprobe(struct fprobe *fp)
+{
+	return -EOPNOTSUPP;
+}
+static inline int unregister_fprobe(struct fprobe *fp)
+{
+	return -EOPNOTSUPP;
+}
+#endif
+
+static inline void disable_fprobe(struct fprobe *fp)
+{
+	if (fp)
+		fp->flags |= FPROBE_FL_DISABLED;
+}
+
+static inline void enable_fprobe(struct fprobe *fp)
+{
+	if (fp)
+		fp->flags &= ~FPROBE_FL_DISABLED;
+}
+
+#endif
