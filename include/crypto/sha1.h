5482115cda1cb18065e53dbf03fc589839e0b0f1
Jason A. Donenfeld <Jason@zx2c4.com>
2022-01-14 15:20:15 +0100

    crypto: sha1_generic - import lib/sha1.c locally
    With no non-crypto API users of this function, we can move it into the
    generic crypto/ code where it belongs.
    
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Herbert Xu <herbert@gondor.apana.org.au>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- include/crypto/sha1.h
+++ include/crypto/sha1.h
@@ -33,14 +33,4 @@ extern int crypto_sha1_update(struct shash_desc *desc, const u8 *data,
 extern int crypto_sha1_finup(struct shash_desc *desc, const u8 *data,
 			     unsigned int len, u8 *hash);
 
-/*
- * An implementation of SHA-1's compression function.  Don't use in new code!
- * You shouldn't be using SHA-1, and even if you *have* to use SHA-1, this isn't
- * the correct way to hash something with SHA-1 (use crypto_shash instead).
- */
-#define SHA1_DIGEST_WORDS	(SHA1_DIGEST_SIZE / 4)
-#define SHA1_WORKSPACE_WORDS	16
-void sha1_init(__u32 *buf);
-void sha1_transform(__u32 *digest, const char *data, __u32 *W);
-
 #endif /* _CRYPTO_SHA1_H */
