ac240eb0add240f4b3981afd5a593c1b46099d9f
Jiri Olsa <jolsa@kernel.org>
2022-02-22 18:05:52 +0100

    bpf: Add multi kprobe link
    Adding new link type BPF_LINK_TYPE_KPROBE_MULTI that attaches kprobe
    program through fprobe API.
    
    The fprobe API allows to attach probe on multiple functions at once
    very fast, because it works on top of ftrace. On the other hand this
    limits the probe point to the function entry or return.
    
    The kprobe program gets the same pt_regs input ctx as when it's attached
    through the perf API.
    
    Adding new attach type BPF_TRACE_KPROBE_MULTI that allows attachment
    kprobe to multiple function with new link.
    
    User provides array of addresses or symbols with count to attach the
    kprobe program to. The new link_create uapi interface looks like:
    
      struct {
              __aligned_u64   syms;
              __aligned_u64   addrs;
              __u32           cnt;
              __u32           flags;
      } kprobe_multi;
    
    The flags field allows single BPF_TRACE_KPROBE_MULTI bit to create
    return multi kprobe.
    
    Signed-off-by: Masami Hiramatsu <mhiramat@kernel.org>
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- include/linux/trace_events.h
+++ include/linux/trace_events.h
@@ -736,6 +736,7 @@ void bpf_put_raw_tracepoint(struct bpf_raw_event_map *btp);
 int bpf_get_perf_event_info(const struct perf_event *event, u32 *prog_id,
 			    u32 *fd_type, const char **buf,
 			    u64 *probe_offset, u64 *probe_addr);
+int bpf_kprobe_multi_link_attach(const union bpf_attr *attr, struct bpf_prog *prog);
 #else
 static inline unsigned int trace_call_bpf(struct trace_event_call *call, void *ctx)
 {
@@ -774,6 +775,11 @@ static inline int bpf_get_perf_event_info(const struct perf_event *event,
 					  u32 *prog_id, u32 *fd_type,
 					  const char **buf, u64 *probe_offset,
 					  u64 *probe_addr)
+{
+	return -EOPNOTSUPP;
+}
+static inline int
+bpf_kprobe_multi_link_attach(const union bpf_attr *attr, struct bpf_prog *prog)
 {
 	return -EOPNOTSUPP;
 }
