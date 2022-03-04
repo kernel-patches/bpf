01b580ba7e9a18f10ba716d5b7dc7650b0213fc7
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2022-03-04 18:28:29 +0100

    selftests/bpf: add tests for the HID-bpf initial implementation
    The test is pretty basic:
    - create a virtual uhid device that no userspace will like (to not mess
      up the running system)
    - attach a BPF prog to it
    - open the matching hidraw node
    - inject one event and check:
      * that the BPF program can do something on the event stream
      * can modify the event stream
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>

---
+++ tools/testing/selftests/bpf/progs/hid.c
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (c) 2022 Red hat */
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <linux/bpf_hid.h>
+
+char _license[] SEC("license") = "GPL";
+
+__u64 callback_check = 52;
+__u64 callback2_check = 52;
+
+SEC("hid/device_event")
+int hid_first_event(struct hid_bpf_ctx *ctx)
+{
+	callback_check = ctx->data[1];
+
+	ctx->data[2] = ctx->data[1] + 5;
+
+	return 0;
+}
