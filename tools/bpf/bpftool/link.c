fbc8c204bac4bbfab9e2da352697c9d8147469d3
Milan Landaverde <milan@mdaverde.com>
2022-03-31 11:45:54 -0400

    bpf/bpftool: add missing link types
    Will display the link type names in bpftool link show output
    
    Signed-off-by: Milan Landaverde <milan@mdaverde.com>

--- tools/bpf/bpftool/link.c
+++ tools/bpf/bpftool/link.c
@@ -20,6 +20,8 @@ static const char * const link_type_name[] = {
 	[BPF_LINK_TYPE_CGROUP]			= "cgroup",
 	[BPF_LINK_TYPE_ITER]			= "iter",
 	[BPF_LINK_TYPE_NETNS]			= "netns",
+	[BPF_LINK_TYPE_XDP]				= "xdp",
+	[BPF_LINK_TYPE_PERF_EVENT]		= "perf_event",
 };
 
 static struct hashmap *link_table;
