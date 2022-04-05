60632834aad7305e220e34748dfaff577a779a9a
Alan Maguire <alan.maguire@oracle.com>
2022-04-05 22:45:56 +0100

    selftests/bpf: uprobe tests should verify param/return values
    uprobe/uretprobe tests don't do any validation of arguments/return values,
    and without this we can't be sure we are attached to the right function,
    or that we are indeed attached to a uprobe or uretprobe.  To fix this
    validate argument and return value for auto-attached functions.
    
    Suggested-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Alan Maguire <alan.maguire@oracle.com>

--- tools/testing/selftests/bpf/prog_tests/uprobe_autoattach.c
+++ tools/testing/selftests/bpf/prog_tests/uprobe_autoattach.c
@@ -5,14 +5,16 @@
 #include "test_uprobe_autoattach.skel.h"
 
 /* uprobe attach point */
-static void autoattach_trigger_func(void)
+static noinline int autoattach_trigger_func(int arg)
 {
-	asm volatile ("");
+	return arg + 1;
 }
 
 void test_uprobe_autoattach(void)
 {
 	struct test_uprobe_autoattach *skel;
+	int trigger_val = 100;
+	size_t malloc_sz = 1;
 	char *mem;
 
 	skel = test_uprobe_autoattach__open_and_load();
@@ -22,12 +24,18 @@ void test_uprobe_autoattach(void)
 	if (!ASSERT_OK(test_uprobe_autoattach__attach(skel), "skel_attach"))
 		goto cleanup;
 
+	skel->bss->uprobe_byname_parm1 = trigger_val;
+	skel->bss->uretprobe_byname_rc  = trigger_val + 1;
+	skel->bss->uprobe_byname2_parm1 = malloc_sz;
+	skel->bss->uretprobe_byname2_rc = getpid();
+
 	/* trigger & validate uprobe & uretprobe */
-	autoattach_trigger_func();
+	(void) autoattach_trigger_func(trigger_val);
 
 	/* trigger & validate shared library u[ret]probes attached by name */
-	mem = malloc(1);
+	mem = malloc(malloc_sz);
 	free(mem);
+	(void) getpid();
 
 	ASSERT_EQ(skel->bss->uprobe_byname_res, 1, "check_uprobe_byname_res");
 	ASSERT_EQ(skel->bss->uretprobe_byname_res, 2, "check_uretprobe_byname_res");
--- tools/testing/selftests/bpf/progs/test_uprobe_autoattach.c
+++ tools/testing/selftests/bpf/progs/test_uprobe_autoattach.c
@@ -1,15 +1,20 @@
 // SPDX-License-Identifier: GPL-2.0
 /* Copyright (c) 2022, Oracle and/or its affiliates. */
 
-#include <linux/ptrace.h>
-#include <linux/bpf.h>
+#include "vmlinux.h"
+
+#include <bpf/bpf_core_read.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 
+int uprobe_byname_parm1 = 0;
 int uprobe_byname_res = 0;
+int uretprobe_byname_rc = 0;
 int uretprobe_byname_res = 0;
+size_t uprobe_byname2_parm1 = 0;
 int uprobe_byname2_res = 0;
-int uretprobe_byname2_res = 0;
+int uretprobe_byname2_rc = 0;
+pid_t uretprobe_byname2_res = 0;
 
 /* This program cannot auto-attach, but that should not stop other
  * programs from attaching.
@@ -23,14 +28,16 @@ int handle_uprobe_noautoattach(struct pt_regs *ctx)
 SEC("uprobe//proc/self/exe:autoattach_trigger_func")
 int handle_uprobe_byname(struct pt_regs *ctx)
 {
-	uprobe_byname_res = 1;
+	if (PT_REGS_PARM1_CORE(ctx) == uprobe_byname_parm1)
+		uprobe_byname_res = 1;
 	return 0;
 }
 
 SEC("uretprobe//proc/self/exe:autoattach_trigger_func")
 int handle_uretprobe_byname(struct pt_regs *ctx)
 {
-	uretprobe_byname_res = 2;
+	if (PT_REGS_RC_CORE(ctx) == uretprobe_byname_rc)
+		uretprobe_byname_res = 2;
 	return 0;
 }
 
@@ -38,14 +45,16 @@ int handle_uretprobe_byname(struct pt_regs *ctx)
 SEC("uprobe/libc.so.6:malloc")
 int handle_uprobe_byname2(struct pt_regs *ctx)
 {
-	uprobe_byname2_res = 3;
+	if (PT_REGS_PARM1_CORE(ctx) == uprobe_byname2_parm1)
+		uprobe_byname2_res = 3;
 	return 0;
 }
 
-SEC("uretprobe/libc.so.6:free")
+SEC("uretprobe/libc.so.6:getpid")
 int handle_uretprobe_byname2(struct pt_regs *ctx)
 {
-	uretprobe_byname2_res = 4;
+	if (PT_REGS_RC_CORE(ctx) == uretprobe_byname2_rc)
+		uretprobe_byname2_res = 4;
 	return 0;
 }
 
