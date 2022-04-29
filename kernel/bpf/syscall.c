877b137ea2ab8ae9b65dce52a1b70c34e797ef3c
Pu Lehui <pulehui@huawei.com>
2022-04-29 09:42:39 +0800

    bpf: Unify data extension operation of jited_ksyms and jited_linfo
    We found that 32-bit environment can not print bpf line info due
    to data inconsistency between jited_ksyms[0] and jited_linfo[0].
    
    For example:
    jited_kyms[0] = 0xb800067c, jited_linfo[0] = 0xffffffffb800067c
    
    We know that both of them store bpf func address, but due to the
    different data extension operations when extended to u64, they may
    not be the same. We need to unify the data extension operations of
    them.
    
    Signed-off-by: Pu Lehui <pulehui@huawei.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -4095,13 +4095,16 @@ static int bpf_prog_get_info_by_fd(struct file *file,
 		info.nr_jited_line_info = 0;
 	if (info.nr_jited_line_info && ulen) {
 		if (bpf_dump_raw_ok(file->f_cred)) {
+			unsigned long jited_linfo_addr;
 			__u64 __user *user_linfo;
 			u32 i;
 
 			user_linfo = u64_to_user_ptr(info.jited_line_info);
 			ulen = min_t(u32, info.nr_jited_line_info, ulen);
 			for (i = 0; i < ulen; i++) {
-				if (put_user((__u64)(long)prog->aux->jited_linfo[i],
+				jited_linfo_addr = (unsigned long)
+					prog->aux->jited_linfo[i];
+				if (put_user((__u64) jited_linfo_addr,
 					     &user_linfo[i]))
 					return -EFAULT;
 			}
