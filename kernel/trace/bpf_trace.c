4364f1b2d03a4a6b5c24820b1b23bc7643c51d00
Wan Jiabing <wanjiabing@vivo.com>
2022-05-12 22:17:10 +0800

    bpf: use vmemdup_user instead of kvmalloc and copy_from_user
    Fix following coccicheck warning:
    ./kernel/trace/bpf_trace.c:2488:12-20: WARNING opportunity for vmemdup_user
    
    Use vmemdup_user instead of kvmalloc and copy_from_user.
    
    Signed-off-by: Wan Jiabing <wanjiabing@vivo.com>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -2485,14 +2485,10 @@ int bpf_kprobe_multi_link_attach(const union bpf_attr *attr, struct bpf_prog *pr
 
 	ucookies = u64_to_user_ptr(attr->link_create.kprobe_multi.cookies);
 	if (ucookies) {
-		cookies = kvmalloc(size, GFP_KERNEL);
-		if (!cookies) {
-			err = -ENOMEM;
+		cookies = vmemdup_user(ucookies, size);
+		if (IS_ERR(cookies)) {
+			err = PTR_ERR(cookies);
 			goto error_addrs;
-		}
-		if (copy_from_user(cookies, ucookies, size)) {
-			err = -EFAULT;
-			goto error_cookies;
 		}
 	}
 
