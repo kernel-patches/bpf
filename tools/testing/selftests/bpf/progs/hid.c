4e3138854b03bb53ba434f9bf6518c352c4c3c46
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2022-05-18 22:59:19 +0200

    selftests/bpf: add tests for bpf_hid_hw_request
    Add tests for the newly implemented function.
    We test here only the GET_REPORT part because the other calls are pure
    HID protocol and won't infer the result of the test of the bpf hook.
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>

--- tools/testing/selftests/bpf/progs/hid.c
+++ tools/testing/selftests/bpf/progs/hid.c
@@ -10,6 +10,13 @@ extern __u8 *hid_bpf_get_data(struct hid_bpf_ctx *ctx,
 			      unsigned int offset,
 			      const size_t __sz) __ksym;
 extern int hid_bpf_attach_prog(unsigned int hid_id, int prog_fd, u32 flags) __ksym;
+extern struct hid_bpf_ctx *hid_bpf_allocate_context(unsigned int hid_id) __ksym;
+extern void hid_bpf_release_context(struct hid_bpf_ctx *ctx) __ksym;
+extern int hid_bpf_hw_request(struct hid_bpf_ctx *ctx,
+			      __u8 *data,
+			      size_t len,
+			      enum hid_report_type type,
+			      int reqtype) __ksym;
 
 struct attach_prog_args {
 	int prog_fd;
@@ -55,4 +62,56 @@ int attach_prog(struct attach_prog_args *ctx)
 					  ctx->prog_fd,
 					  0);
 	return 0;
+}
+
+struct hid_hw_request_syscall_args {
+	/* data needs to come at offset 0 so we can do a memcpy into it */
+	__u8 data[10];
+	unsigned int hid;
+	int retval;
+	size_t size;
+	enum hid_report_type type;
+	__u8 request_type;
+};
+
+SEC("syscall")
+int hid_user_raw_request(struct hid_hw_request_syscall_args *args)
+{
+	struct hid_bpf_ctx *ctx;
+	int i, ret = 0;
+	__u8 *data;
+
+	ctx = hid_bpf_allocate_context(args->hid);
+	if (!ctx)
+		return 0; /* EPERM check */
+
+	/* We can not use the context data memory directly in the hid_bpf call,
+	 * so we rely on the PTR_TO_MEM allocated in the hid_bpf_context
+	 */
+	data = hid_bpf_get_data(ctx, 0 /* offset */, 10 /* size */);
+	if (!data)
+		goto out; /* EPERM check */
+
+	__builtin_memcpy(data, args->data, sizeof(args->data));
+
+	if (args->size <= sizeof(args->data)) {
+		ret = hid_bpf_hw_request(ctx,
+					 data,
+					 args->size,
+					 args->type,
+					 args->request_type);
+		args->retval = ret;
+		if (ret < 0)
+			goto out;
+	} else {
+		ret = -7; /* -E2BIG */
+		goto out;
+	}
+
+	__builtin_memcpy(args->data, data, sizeof(args->data));
+
+ out:
+	hid_bpf_release_context(ctx);
+
+	return ret;
 }
