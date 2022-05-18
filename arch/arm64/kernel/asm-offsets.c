697e895a358f1d4c7373e309e81d174b7cf4f8fb
Xu Kuohai <xukuohai@huawei.com>
2022-05-18 09:16:33 -0400

    arm64: ftrace: Add ftrace direct call support
    Add ftrace direct support for arm64.
    
    1. When there is custom trampoline only, replace the fentry nop to a
       jump instruction that jumps directly to the custom trampoline.
    
    2. When ftrace trampoline and custom trampoline coexist, jump from
       fentry to ftrace trampoline first, then jump to custom trampoline
       when ftrace trampoline exits. The current unused register
       pt_regs->orig_x0 is used as an intermediary for jumping from ftrace
       trampoline to custom trampoline.
    
    Signed-off-by: Xu Kuohai <xukuohai@huawei.com>
    Acked-by: Song Liu <songliubraving@fb.com>
    Acked-by: KP Singh <kpsingh@kernel.org>

--- arch/arm64/kernel/asm-offsets.c
+++ arch/arm64/kernel/asm-offsets.c
@@ -80,6 +80,7 @@ int main(void)
   DEFINE(S_SDEI_TTBR1,		offsetof(struct pt_regs, sdei_ttbr1));
   DEFINE(S_PMR_SAVE,		offsetof(struct pt_regs, pmr_save));
   DEFINE(S_STACKFRAME,		offsetof(struct pt_regs, stackframe));
+  DEFINE(S_ORIG_X0,		offsetof(struct pt_regs, orig_x0));
   DEFINE(PT_REGS_SIZE,		sizeof(struct pt_regs));
   BLANK();
 #ifdef CONFIG_COMPAT
