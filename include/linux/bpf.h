6a6df476285b34fceb790e8d342d0a49a39f4cb0
Roberto Sassu <roberto.sassu@huawei.com>
2022-06-14 15:06:18 +0200

    bpf: Export bpf_dynptr_get_size()
    Export bpf_dynptr_get_size(), so that kernel code dealing with eBPF dynamic
    pointers can obtain the real size of data carried by this data structure.
    
    Cc: Joanne Koong <joannelkoong@gmail.com>
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -2508,5 +2508,6 @@ void bpf_dynptr_init(struct bpf_dynptr_kern *ptr, void *data,
 		     enum bpf_dynptr_type type, u32 offset, u32 size);
 void bpf_dynptr_set_null(struct bpf_dynptr_kern *ptr);
 int bpf_dynptr_check_size(u32 size);
+u32 bpf_dynptr_get_size(struct bpf_dynptr_kern *ptr);
 
 #endif /* _LINUX_BPF_H */
