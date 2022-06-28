94b68f925e344746267dbe958acb57b1733e66a1
Alexander Lobakin <alexandr.lobakin@intel.com>
2022-06-28 21:47:41 +0200

    net, xdp: allow metadata > 32
    Hardware/driver-prepended XDP metadata might be much bigger than 32
    bytes, especially if it includes a piece of a descriptor.
    Relax the restriction and allow metadata larger than 32 bytes and
    make __skb_metadata_differs() work with bigger lengths. The new
    restriction is pretty much mechanical -- skb_shared_info::meta_len
    is a u8 and XDP_PACKET_HEADROOM is 256 (minus
    `sizeof(struct xdp_frame)`).
    The requirement of having its length aligned to 4 bytes is still
    valid.
    
    Signed-off-by: Alexander Lobakin <alexandr.lobakin@intel.com>

--- include/net/xdp_meta.h
+++ include/net/xdp_meta.h
@@ -5,6 +5,7 @@
 #define __LINUX_NET_XDP_META_H__
 
 #include <net/xdp.h>
+#include <uapi/linux/bpf.h>
 
 /* Drivers not supporting XDP metadata can use this helper, which
  * rejects any room expansion for metadata as a result.
@@ -21,9 +22,27 @@ xdp_data_meta_unsupported(const struct xdp_buff *xdp)
 	return unlikely(xdp->data_meta > xdp->data);
 }
 
+/**
+ * xdp_metalen_invalid -- check if the length of a frame's metadata is valid
+ * @metalen: the length of the frame's metadata
+ *
+ * skb_shared_info::meta_len is of 1 byte long, thus it can't be longer than
+ * 255, but this always can change. XDP_PACKET_HEADROOM is 256, and this is a
+ * UAPI. sizeof(struct xdp_frame) is reserved since xdp_frame is being placed
+ * at xdp_buff::data_hard_start whilst being constructed on XDP_REDIRECT.
+ * The 32-bit alignment requirement is arbitrary, kept for simplicity and,
+ * sometimes, speed.
+ */
 static inline bool xdp_metalen_invalid(unsigned long metalen)
 {
-	return (metalen & (sizeof(__u32) - 1)) || (metalen > 32);
+	typeof(metalen) max;
+
+	max = min_t(typeof(max),
+		    (typeof_member(struct skb_shared_info, meta_len))~0UL,
+		    XDP_PACKET_HEADROOM - sizeof(struct xdp_frame));
+	BUILD_BUG_ON(!__builtin_constant_p(max));
+
+	return (metalen & (sizeof(u32) - 1)) || metalen > max;
 }
 
 #endif /* __LINUX_NET_XDP_META_H__ */
