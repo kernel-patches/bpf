281731c6a293b15d2291a4590b8ce32cfe43c6cc
Frederick Lawler <fred@cloudflare.com>
2022-07-07 17:32:26 -0500

    bpf-lsm: Make bpf_lsm_create_user_ns() sleepable
    Users may want to audit calls to security_create_user_ns() and access
    user space memory. Also create_user_ns() runs without
    pagefault_disabled(). Therefore, make bpf_lsm_create_user_ns() sleepable
    for mandatory access control policies.
    
    Signed-off-by: Frederick Lawler <fred@cloudflare.com>

--- kernel/bpf/bpf_lsm.c
+++ kernel/bpf/bpf_lsm.c
@@ -331,6 +331,7 @@ BTF_ID(func, bpf_lsm_task_getsecid_obj)
 BTF_ID(func, bpf_lsm_task_prctl)
 BTF_ID(func, bpf_lsm_task_setscheduler)
 BTF_ID(func, bpf_lsm_task_to_inode)
+BTF_ID(func, bpf_lsm_create_user_ns)
 BTF_SET_END(sleepable_lsm_hooks)
 
 bool bpf_lsm_is_sleepable_hook(u32 btf_id)
