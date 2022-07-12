ca7d4978baab9344502efb6825a1984f1afb16e8
Roberto Sassu <roberto.sassu@huawei.com>
2022-07-12 20:41:28 +0200

    selftests/bpf: Add test for bpf_verify_pkcs7_signature() helper
    Perform several tests to ensure the correct implementation of the
    bpf_verify_pkcs7_signature() helper.
    
    Do the tests with data signed with a generated testing key (by using
    sign-file from scripts/) and with the tcp_bic.ko kernel module if it is
    found in the system. The test does not fail if tcp_bic.ko is not found.
    
    First, ensure that bpf_verify_pkcs7_signature() rejects invalid parameters.
    
    Then, perform a successful signature verification with the session keyring
    and a new one created for testing.
    
    Then, ensure that permission and validation checks are done properly on the
    keyring provided to bpf_verify_pkcs7_signature(), despite those checks were
    deferred at the time the keyring was retrieved with bpf_lookup_user_key().
    The tests expect to encounter an error if the Search permission is removed
    from the keyring, or the keyring is expired.
    
    Finally, perform a successful and unsuccessful signature verification with
    the keyrings with pre-determined IDs (the last test fails because the key
    is not in the platform keyring).
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>

---
+++ tools/testing/selftests/bpf/progs/test_verify_pkcs7_sig.c
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: GPL-2.0
+
+/*
+ * Copyright (C) 2022 Huawei Technologies Duesseldorf GmbH
+ *
+ * Author: Roberto Sassu <roberto.sassu@huawei.com>
+ */
+
+#include <errno.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+#define MAX_DATA_SIZE (1024 * 1024)
+#define MAX_SIG_SIZE 1024
+
+typedef __u8 u8;
+typedef __u16 u16;
+typedef __u32 u32;
+typedef __u64 u64;
+
+u32 monitored_pid;
+u32 user_keyring_serial;
+u64 system_keyring;
+
+struct data {
+	u8 data[MAX_DATA_SIZE];
+	u32 data_len;
+	u8 sig[MAX_SIG_SIZE];
+	u32 sig_len;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__uint(max_entries, 1);
+	__type(key, __u32);
+	__type(value, struct data);
+} data_input SEC(".maps");
+
+char _license[] SEC("license") = "GPL";
+
+SEC("lsm.s/bpf")
+int BPF_PROG(bpf, int cmd, union bpf_attr *attr, unsigned int size)
+{
+	struct bpf_dynptr data_ptr, sig_ptr;
+	struct data *data_val;
+	struct key *user_keyring = NULL;
+	u32 pid;
+	u64 value;
+	int ret, zero = 0;
+
+	pid = bpf_get_current_pid_tgid() >> 32;
+	if (pid != monitored_pid)
+		return 0;
+
+	data_val = bpf_map_lookup_elem(&data_input, &zero);
+	if (!data_val)
+		return 0;
+
+	bpf_probe_read(&value, sizeof(value), &attr->value);
+
+	bpf_copy_from_user(data_val, sizeof(struct data),
+			   (void *)(unsigned long)value);
+
+	if (data_val->data_len > sizeof(data_val->data))
+		return -EINVAL;
+
+	bpf_dynptr_from_mem(data_val->data, data_val->data_len, 0, &data_ptr);
+
+	if (data_val->sig_len > sizeof(data_val->sig))
+		return -EINVAL;
+
+	bpf_dynptr_from_mem(data_val->sig, data_val->sig_len, 0, &sig_ptr);
+
+	if (user_keyring_serial) {
+		user_keyring = bpf_lookup_user_key(user_keyring_serial, 0);
+		if (!user_keyring)
+			return -ENOENT;
+	}
+
+	ret = bpf_verify_pkcs7_signature(&data_ptr, &sig_ptr, user_keyring,
+					 system_keyring);
+
+	if (user_keyring)
+		bpf_key_put(user_keyring);
+
+	return ret;
+}
