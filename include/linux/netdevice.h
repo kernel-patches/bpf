20bccb26abccf2fb2e7fc416f7124ad30ef874ad
Toke Høiland-Jørgensen <toke@redhat.com>
2022-08-18 18:59:03 +0200

    dev: Move received_rps counter next to RPS members in softnet data
    Move the received_rps counter value next to the other RPS-related members
    in softnet_data. This closes two four-byte holes in the structure, making
    room for another pointer in the first two cache lines without bumping the
    xmit struct to its own line.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -3100,7 +3100,6 @@ struct softnet_data {
 	/* stats */
 	unsigned int		processed;
 	unsigned int		time_squeeze;
-	unsigned int		received_rps;
 #ifdef CONFIG_RPS
 	struct softnet_data	*rps_ipi_list;
 #endif
@@ -3133,6 +3132,7 @@ struct softnet_data {
 	unsigned int		cpu;
 	unsigned int		input_queue_tail;
 #endif
+	unsigned int		received_rps;
 	unsigned int		dropped;
 	struct sk_buff_head	input_pkt_queue;
 	struct napi_struct	backlog;
