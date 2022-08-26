2b3c3ec5d592dd41f5e0a8a5db970e9e6c7c285f
Tiezhu Yang <yangtiezhu@loongson.cn>
2022-08-26 10:12:07 +0800

    bpf, mips: No need to use min() to get MAX_TAIL_CALL_CNT
    MAX_TAIL_CALL_CNT is 33, so min(MAX_TAIL_CALL_CNT, 0xffff) is always
    MAX_TAIL_CALL_CNT, it is better to use MAX_TAIL_CALL_CNT directly.
    
    Signed-off-by: Tiezhu Yang <yangtiezhu@loongson.cn>

--- arch/mips/net/bpf_jit_comp64.c
+++ arch/mips/net/bpf_jit_comp64.c
@@ -552,7 +552,7 @@ void build_prologue(struct jit_context *ctx)
 	 * On a tail call, the calling function jumps into the prologue
 	 * after this instruction.
 	 */
-	emit(ctx, ori, tc, MIPS_R_ZERO, min(MAX_TAIL_CALL_CNT, 0xffff));
+	emit(ctx, ori, tc, MIPS_R_ZERO, MAX_TAIL_CALL_CNT);
 
 	/* === Entry-point for tail calls === */
 
