24b31c38dab01399ebdc719995c34b566eb44f68
Martin Rodriguez Reboredo <yakoyoku@gmail.com>
2022-08-28 20:33:17 -0300

    bpf: Add config for skipping BTF enum64s
    After the release of pahole 1.24 some people in the dwarves mailing list
    notified issues related to building the kernel with the BTF_DEBUG_INFO
    option toggled. They seem to be happenning due to the kernel and
    resolve_btfids interpreting btf types erroneously. In the dwarves list
    I've proposed a change to the scripts that I've written while testing
    the Rust kernel, it simply passes the --skip_encoding_btf_enum64 to
    pahole if it has version 1.24.
    
    v1 -> v2:
    - Switch to off by default and remove the config option.
    - Send it to stable instead.
    
    Signed-off-by: Martin Rodriguez Reboredo <yakoyoku@gmail.com>

--- scripts/pahole-flags.sh
+++ scripts/pahole-flags.sh
@@ -19,5 +19,8 @@ fi
 if [ "${pahole_ver}" -ge "122" ]; then
 	extra_paholeopt="${extra_paholeopt} -j"
 fi
+if [ "${pahole_ver}" -ge "124" ]; then
+	extra_paholeopt="${extra_paholeopt} --skip_encoding_btf_enum64"
+fi
 
 echo ${extra_paholeopt}
