4b229a3da810e65b4f360b3c72e51c263cf64919
Jesper Dangaard Brouer <brouer@redhat.com>
2022-09-07 17:45:46 +0200

    bpf: export btf functions for modules
    This allows modules to lookup their own module BTF info.
    
    These are get and set operations that bump the refcount.
    Thus, modules can use this to control the lifetime.
    
    Suggested-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- include/linux/btf.h
+++ include/linux/btf.h
@@ -148,6 +148,8 @@ u32 btf_obj_id(const struct btf *btf);
 bool btf_is_kernel(const struct btf *btf);
 bool btf_is_module(const struct btf *btf);
 struct module *btf_try_get_module(const struct btf *btf);
+struct btf *btf_get_module_btf(const struct module *module);
+void btf_put_module_btf(struct btf *btf);
 u32 btf_nr_types(const struct btf *btf);
 bool btf_member_is_reg_int(const struct btf *btf, const struct btf_type *s,
 			   const struct btf_member *m,
