085e6d9f05a357bd0d848b18c388a182e9b09ea0
Shmulik Ladkani <shmulik@metanetworks.com>
2022-09-11 15:23:27 +0300

    selftests/bpf: Simplify test_tunnel setup for allowing non-local tunnel traffic
    Commit 1115169f47ae ("selftests/bpf: Don't assign outer source IP to host")
    removed the secondary IP (IP4_ADDR2_VETH1) assigned to veth1, in order
    to test bpf_skb_set_tunnel_key's functionality when tunnel destination
    isn't assigned to an interface.
    
    The chosen setup for testing the "tunnel to unassigned outer IP"
    scenario was rather complex: (1) static ARP entries in order to
    bypass ARP (o/w requests will fail as the target address isn't assigned
    locally), and (2) a BPF program running on veth1 ingress which
    manipulates the IP header's daddr to the actual IP assigned to the
    interface (o/w tunnel traffic won't be accepted locally).
    
    This is complex, and adds a dependency on this hidden "dnat"-like eBPF
    program, that needs to be replicated when new tunnel tests are added.
    
    Instead, we can have a much simpler setup: Add the secondary IP as a
    *local route* in a table pointed by a custom fib rule. No static arp
    entries are needed, and the special eBPF program that "dnats" the outer
    destination can be removed.
    
    This commit is a revert of 1115169f47ae, with the addition of the local
    route of IP4_ADDR2_VETH1 (instead of the original address assignment).
    
    Signed-off-by: Shmulik Ladkani <shmulik.ladkani@gmail.com>

--- tools/testing/selftests/bpf/prog_tests/test_tunnel.c
+++ tools/testing/selftests/bpf/prog_tests/test_tunnel.c
@@ -82,7 +82,6 @@
 
 #define MAC_TUNL_DEV0 "52:54:00:d9:01:00"
 #define MAC_TUNL_DEV1 "52:54:00:d9:02:00"
-#define MAC_VETH1 "52:54:00:d9:03:00"
 
 #define VXLAN_TUNL_DEV0 "vxlan00"
 #define VXLAN_TUNL_DEV1 "vxlan11"
@@ -109,9 +108,15 @@
 static int config_device(void)
 {
 	SYS("ip netns add at_ns0");
-	SYS("ip link add veth0 address " MAC_VETH1 " type veth peer name veth1");
+	SYS("ip link add veth0 type veth peer name veth1");
 	SYS("ip link set veth0 netns at_ns0");
 	SYS("ip addr add " IP4_ADDR1_VETH1 "/24 dev veth1");
+	/* Create a custom rule routing IP4_ADDR2_VETH1 as local.
+	 * Do not place it in "local" table, to avoid this IP being considered
+	 * assigned to a device.
+	 */
+	SYS("ip rule add to " IP4_ADDR2_VETH1 " table 20");
+	SYS("ip route add local " IP4_ADDR2_VETH1 "/32 dev veth1 table 20");
 	SYS("ip link set dev veth1 up mtu 1500");
 	SYS("ip netns exec at_ns0 ip addr add " IP4_ADDR_VETH0 "/24 dev veth0");
 	SYS("ip netns exec at_ns0 ip link set dev veth0 up mtu 1500");
@@ -125,6 +130,7 @@ static void cleanup(void)
 {
 	SYS_NOFAIL("test -f /var/run/netns/at_ns0 && ip netns delete at_ns0");
 	SYS_NOFAIL("ip link del veth1 2> /dev/null");
+	SYS_NOFAIL("ip rule del to %s table 20 2> /dev/null", IP4_ADDR2_VETH1);
 	SYS_NOFAIL("ip link del %s 2> /dev/null", VXLAN_TUNL_DEV1);
 	SYS_NOFAIL("ip link del %s 2> /dev/null", IP6VXLAN_TUNL_DEV1);
 }
@@ -140,8 +146,6 @@ static int add_vxlan_tunnel(void)
 	    VXLAN_TUNL_DEV0, IP4_ADDR_TUNL_DEV0);
 	SYS("ip netns exec at_ns0 ip neigh add %s lladdr %s dev %s",
 	    IP4_ADDR_TUNL_DEV1, MAC_TUNL_DEV1, VXLAN_TUNL_DEV0);
-	SYS("ip netns exec at_ns0 ip neigh add %s lladdr %s dev veth0",
-	    IP4_ADDR2_VETH1, MAC_VETH1);
 
 	/* root namespace */
 	SYS("ip link add dev %s type vxlan external gbp dstport 4789",
@@ -277,17 +281,6 @@ static void test_vxlan_tunnel(void)
 	if (!ASSERT_GE(set_src_prog_fd, 0, "bpf_program__fd"))
 		goto done;
 	if (attach_tc_prog(&tc_hook, get_src_prog_fd, set_src_prog_fd))
-		goto done;
-
-	/* load and attach bpf prog to veth dev tc hook point */
-	ifindex = if_nametoindex("veth1");
-	if (!ASSERT_NEQ(ifindex, 0, "veth1 ifindex"))
-		goto done;
-	tc_hook.ifindex = ifindex;
-	set_dst_prog_fd = bpf_program__fd(skel->progs.veth_set_outer_dst);
-	if (!ASSERT_GE(set_dst_prog_fd, 0, "bpf_program__fd"))
-		goto done;
-	if (attach_tc_prog(&tc_hook, set_dst_prog_fd, -1))
 		goto done;
 
 	/* load and attach prog set_md to tunnel dev tc hook point at_ns0 */
