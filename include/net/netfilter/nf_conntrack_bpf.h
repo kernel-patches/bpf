c468b0dddca6a107252cd57dcf50fc7a21139312
Daniel Xu <dxu@dxuuu.xyz>
2022-09-20 08:15:24 -0600

    bpf: Move nf_conn extern declarations to filter.h
    We're seeing the following new warnings on netdev/build_32bit and
    netdev/build_allmodconfig_warn CI jobs:
    
        ../net/core/filter.c:8608:1: warning: symbol
        'nf_conn_btf_access_lock' was not declared. Should it be static?
        ../net/core/filter.c:8611:5: warning: symbol 'nfct_bsa' was not
        declared. Should it be static?
    
    Fix by ensuring extern declaration is present while compiling filter.o.
    
    Fixes: 864b656f82cc ("bpf: Add support for writing to nf_conn:mark")
    Signed-off-by: Daniel Xu <dxu@dxuuu.xyz>

--- include/net/netfilter/nf_conntrack_bpf.h
+++ include/net/netfilter/nf_conntrack_bpf.h
@@ -12,12 +12,6 @@
 extern int register_nf_conntrack_bpf(void);
 extern void cleanup_nf_conntrack_bpf(void);
 
-extern struct mutex nf_conn_btf_access_lock;
-extern int (*nfct_btf_struct_access)(struct bpf_verifier_log *log, const struct btf *btf,
-				     const struct btf_type *t, int off, int size,
-				     enum bpf_access_type atype, u32 *next_btf_id,
-				     enum bpf_type_flag *flag);
-
 #else
 
 static inline int register_nf_conntrack_bpf(void)
