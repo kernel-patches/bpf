e770c922341f234e4548cfbaa343532eba4ce560
Eduard Zingerman <eddyz87@gmail.com>
2022-12-17 04:17:09 +0200

    selftests/bpf: convenience macro for use with 'asm volatile' blocks
    A set of macros useful for writing naked BPF functions using inline
    assembly. E.g. as follows:
    
    struct map_struct {
    	...
    } map SEC(".maps");
    
    SEC(...)
    __naked int foo_test(void)
    {
    	asm volatile(
    		"r0 = 0;"
    		"*(u64*)(r10 - 8) = r0;"
    		"r1 = %[map] ll;"
    		"r2 = r10;"
    		"r2 += -8;"
    		"call %[bpf_map_lookup_elem];"
    		"r0 = 0;"
    		"exit;"
    		:
    		: __imm(bpf_map_lookup_elem),
    		  __imm_addr(map)
    		: __clobber_all);
    }
    
    Signed-off-by: Eduard Zingerman <eddyz87@gmail.com>
    Acked-by: Yonghong Song <yhs@fb.com>

--- tools/testing/selftests/bpf/progs/bpf_misc.h
+++ tools/testing/selftests/bpf/progs/bpf_misc.h
@@ -8,6 +8,12 @@
 #define __log_level(lvl)	__attribute__((btf_decl_tag("comment:test_log_level="#lvl)))
 #define __test_state_freq	__attribute__((btf_decl_tag("comment:test_state_freq")))
 
+/* Convenience macro for use with 'asm volatile' blocks */
+#define __naked __attribute__((naked))
+#define __clobber_all "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "memory"
+#define __imm(name) [name]"i"(name)
+#define __imm_addr(name) [name]"i"(&name)
+
 #if defined(__TARGET_ARCH_x86)
 #define SYSCALL_WRAPPER 1
 #define SYS_PREFIX "__x64_"
