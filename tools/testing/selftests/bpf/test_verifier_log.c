017b06b582c8fe311ba22501ea0fa886138ecbfa
Roberto Valenzuela <valenzuelarober@gmail.com>
2023-01-15 20:56:23 -0500

    selftests/bpf: add missing SPDX license headers
    Add the missing "SDPX-License-Identifier" license header
    to the test_verifier_log.c and urandom_read.c.
    
    These changes will resolve the following checkpatch.pl
    script warning:
    
    WARNING: Missing or malformed SPDX-License-Identifier tag in line 1
    Signed-off-by: Roberto Valenzuela <valenzuelarober@gmail.com>

--- tools/testing/selftests/bpf/test_verifier_log.c
+++ tools/testing/selftests/bpf/test_verifier_log.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
--- tools/testing/selftests/bpf/urandom_read.c
+++ tools/testing/selftests/bpf/urandom_read.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
