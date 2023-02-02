44532756f4b29aff2fc1cc0b245d2a361295a190
Yafang Shao <laoar.shao@gmail.com>
2023-02-02 01:41:55 +0000

    mm: util: introduce kvsize()
    Introduce a new help kvsize() to report full size of underlying allocation
    of a kmalloc'ed addr or vmalloc'ed addr.
    
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>

--- include/linux/slab.h
+++ include/linux/slab.h
@@ -762,6 +762,7 @@ static inline __alloc_size(1, 2) void *kvcalloc(size_t n, size_t size, gfp_t fla
 
 extern void *kvrealloc(const void *p, size_t oldsize, size_t newsize, gfp_t flags)
 		      __realloc_size(3);
+extern size_t kvsize(void *addr);
 extern void kvfree(const void *addr);
 extern void kvfree_sensitive(const void *addr, size_t len);
 
