95329225f8fb52a3f2b9c3f43cfaf5d2532a21b9
Jiri Olsa <jolsa@kernel.org>
2023-02-28 10:32:03 +0100

    selftests/bpf: Add err.h header
    Moving error macros from profiler.inc.h to new err.h header.
    It will be used in following changes.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

---
+++ tools/testing/selftests/bpf/progs/err.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __ERR_H__
+#define __ERR_H__
+
+#define MAX_ERRNO 4095
+#define IS_ERR_VALUE(x) (unsigned long)(void *)(x) >= (unsigned long)-MAX_ERRNO
+
+static inline int IS_ERR_OR_NULL(const void *ptr)
+{
+	return !ptr || IS_ERR_VALUE((unsigned long)ptr);
+}
+
+#endif /* __ERR_H__ */
