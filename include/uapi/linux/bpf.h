be42fa23f2f125c351924390f632291b4b9981a6
Florian Westphal <fw@strlen.de>
2023-03-02 18:27:55 +0100

    bpf: add bpf_link support for BPF_NETFILTER programs
    Add bpf_link support skeleton.  To keep this reviewable, no bpf program
    can be invoked here, if a program would be attached only a c-stub is called.
    
    This defaults to 'y' if both netfilter and bpf syscall are enabled in
    kconfig.
    
    Uapi example usage:
    	union bpf_attr attr = { };
    
    	attr.link_create.prog_fd = progfd;
    	attr.link_create.attach_type = BPF_NETFILTER;
    	attr.link_create.netfilter.pf = PF_INET;
    	attr.link_create.netfilter.hooknum = NF_INET_LOCAL_IN;
    	attr.link_create.netfilter.priority = -128;
    
    	err = bpf(BPF_LINK_CREATE, &attr, sizeof(attr));
    
    ... this would attach progfd to ipv4:input hook.
    
    Such hook gets removed automatically if the calling program exits.
    
    BPF_NETFILTER program invocation is added in followup change.
    
    NF_HOOK_OP_BPF enum will eventually be read from nfnetlink_hook, it
    allows to tell userspace which program is attached at the given hook
    when user runs 'nft hook list' command rather than just the priority
    and not-very-helpful 'this hook runs a bpf prog but I can't tell which
    one'.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -986,6 +986,7 @@ enum bpf_prog_type {
 	BPF_PROG_TYPE_LSM,
 	BPF_PROG_TYPE_SK_LOOKUP,
 	BPF_PROG_TYPE_SYSCALL, /* a program that can execute syscalls */
+	BPF_PROG_TYPE_NETFILTER,
 };
 
 enum bpf_attach_type {
@@ -1049,6 +1050,7 @@ enum bpf_link_type {
 	BPF_LINK_TYPE_PERF_EVENT = 7,
 	BPF_LINK_TYPE_KPROBE_MULTI = 8,
 	BPF_LINK_TYPE_STRUCT_OPS = 9,
+	BPF_LINK_TYPE_NETFILTER = 10,
 
 	MAX_BPF_LINK_TYPE,
 };
@@ -1543,6 +1545,11 @@ union bpf_attr {
 				 */
 				__u64		cookie;
 			} tracing;
+			struct {
+				__u32		pf;
+				__u32		hooknum;
+				__s32		prio;
+			} netfilter;
 		};
 	} link_create;
 
@@ -6373,6 +6380,11 @@ struct bpf_link_info {
 		struct {
 			__u32 ifindex;
 		} xdp;
+		struct {
+			__u32 pf;
+			__u32 hooknum;
+			__s32 priority;
+		} netfilter;
 	};
 } __attribute__((aligned(8)));
 
