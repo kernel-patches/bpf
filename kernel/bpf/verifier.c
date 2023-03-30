80d9d80975aad584fb3b7933b0a3612af572d362
Yonghong Song <yhs@fb.com>
2023-03-29 22:56:05 -0700

    bpf: Improve verifier JEQ/JNE insn branch taken checking
    Currently, for BPF_JEQ/BPF_JNE insn, verifier determines
    whether the branch is taken or not only if both operands
    are constants. Therefore, for the following code snippet,
      0: (85) call bpf_ktime_get_ns#5       ; R0_w=scalar()
      1: (a5) if r0 < 0x3 goto pc+2         ; R0_w=scalar(umin=3)
      2: (b7) r2 = 2                        ; R2_w=2
      3: (1d) if r0 == r2 goto pc+2 6
    
    At insn 3, since r0 is not a constant, verifier assumes both branch
    can be taken which may lead inproper verification failure.
    
    Add comparing umin value and the constant. If the umin value
    is greater than the constant, for JEQ the branch must be
    not-taken, and for JNE the branch must be taken.
    The jmp32 mode JEQ/JNE branch taken checking is also
    handled similarly.
    
    Signed-off-by: Yonghong Song <yhs@fb.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -12606,10 +12606,14 @@ static int is_branch32_taken(struct bpf_reg_state *reg, u32 val, u8 opcode)
 	case BPF_JEQ:
 		if (tnum_is_const(subreg))
 			return !!tnum_equals_const(subreg, val);
+		else if (reg->u32_min_value > val)
+			return 0;
 		break;
 	case BPF_JNE:
 		if (tnum_is_const(subreg))
 			return !tnum_equals_const(subreg, val);
+		else if (reg->u32_min_value > val)
+			return 1;
 		break;
 	case BPF_JSET:
 		if ((~subreg.mask & subreg.value) & val)
@@ -12679,10 +12683,14 @@ static int is_branch64_taken(struct bpf_reg_state *reg, u64 val, u8 opcode)
 	case BPF_JEQ:
 		if (tnum_is_const(reg->var_off))
 			return !!tnum_equals_const(reg->var_off, val);
+		else if (reg->umin_value > val)
+			return 0;
 		break;
 	case BPF_JNE:
 		if (tnum_is_const(reg->var_off))
 			return !tnum_equals_const(reg->var_off, val);
+		else if (reg->umin_value > val)
+			return 1;
 		break;
 	case BPF_JSET:
 		if ((~reg->var_off.mask & reg->var_off.value) & val)
