bc44aca8987f8d5d793520a02a52431ecc883f32
Florian Westphal <fw@strlen.de>
2023-04-05 18:11:12 +0200

    bpf: minimal support for programs hooked into netfilter framework
    This adds minimal support for BPF_PROG_TYPE_NETFILTER bpf programs
    that will be invoked via the NF_HOOK() points in the ip stack.
    
    Invocation incurs an indirect call.  This is not a necessity: Its
    possible to add 'DEFINE_BPF_DISPATCHER(nf_progs)' and handle the
    program invocation with the same method already done for xdp progs.
    
    This isn't done here to keep the size of this chunk down.
    
    Verifier restricts verdicts to either DROP or ACCEPT.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>

--- include/net/netfilter/nf_bpf_link.h
+++ include/net/netfilter/nf_bpf_link.h
@@ -1,2 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0 */
+
+struct bpf_nf_ctx {
+	const struct nf_hook_state *state;
+	struct sk_buff *skb;
+};
+
 int bpf_nf_link_attach(const union bpf_attr *attr, struct bpf_prog *prog);
