6d36bba27f7a8fea6c810bb3de4cf2e329fd22bf
Jiri Olsa <jolsa@kernel.org>
2023-04-24 18:04:32 +0200

    libbpf: Add uprobe_multi attach type and link names
    Adding new uprobe_multi attach type and link names,
    so the functions can resolve the new values.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/lib/bpf/libbpf.c
+++ tools/lib/bpf/libbpf.c
@@ -117,6 +117,7 @@ static const char * const attach_type_name[] = {
 	[BPF_PERF_EVENT]		= "perf_event",
 	[BPF_TRACE_KPROBE_MULTI]	= "trace_kprobe_multi",
 	[BPF_STRUCT_OPS]		= "struct_ops",
+	[BPF_TRACE_UPROBE_MULTI]	= "trace_uprobe_multi",
 };
 
 static const char * const link_type_name[] = {
@@ -131,6 +132,7 @@ static const char * const link_type_name[] = {
 	[BPF_LINK_TYPE_KPROBE_MULTI]		= "kprobe_multi",
 	[BPF_LINK_TYPE_STRUCT_OPS]		= "struct_ops",
 	[BPF_LINK_TYPE_NETFILTER]		= "netfilter",
+	[BPF_LINK_TYPE_UPROBE_MULTI]		= "uprobe_multi",
 };
 
 static const char * const map_type_name[] = {
