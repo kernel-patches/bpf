eed1ffd115a72545c38ccc3d61b5d490d401acb2
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2023-05-09 15:24:33 +0200

    bpf: Remove in_atomic() from bpf_link_put().
    bpf_free_inode() is invoked as a RCU callback. Usually RCU callbacks are
    invoked within softirq context. By setting rcutree.use_softirq=0 boot
    option the RCU callbacks will be invoked in a per-CPU kthread with
    bottom halves disabled which implies a RCU read section.
    
    On PREEMPT_RT the context remains fully preemptible. The RCU read
    section however does not allow schedule() invocation. The latter happens
    in mutex_lock() performed by bpf_trampoline_unlink_prog() originated
    from bpf_link_put().
    
    Remove the context checks and use the workqueue unconditionally.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -2785,12 +2785,8 @@ void bpf_link_put(struct bpf_link *link)
 	if (!atomic64_dec_and_test(&link->refcnt))
 		return;
 
-	if (in_atomic()) {
-		INIT_WORK(&link->work, bpf_link_put_deferred);
-		schedule_work(&link->work);
-	} else {
-		bpf_link_free(link);
-	}
+	INIT_WORK(&link->work, bpf_link_put_deferred);
+	schedule_work(&link->work);
 }
 EXPORT_SYMBOL(bpf_link_put);
 
