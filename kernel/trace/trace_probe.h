299d8e5c9b290d5a2f7a5afa5f79042b82616ff8
Masami Hiramatsu (Google) <mhiramat@kernel.org>
2023-05-17 19:59:23 +0900

    tracing/probes: Add tracepoint support on fprobe_events
    Allow fprobe_events to trace raw tracepoints so that user can trace
    tracepoints which don't have traceevent wrappers. This new event is
    always available if the fprobe_events is enabled (thus no kconfig),
    because the fprobe_events depends on the trace-event and traceporint.
    
    e.g.
     # echo 't sched_overutilized_tp' >> dynamic_events
     # echo 't 9p_client_req' >> dynamic_events
     # cat dynamic_events
    t:tracepoints/sched_overutilized_tp sched_overutilized_tp
    t:tracepoints/_9p_client_req 9p_client_req
    
    The event name is based on the tracepoint name, but if it is started
    with digit character, an underscore '_' will be added.
    
    NOTE: to avoid further confusion, this renames TPARG_FL_TPOINT to
    TPARG_FL_TEVENT because this flag is used for eprobe (trace-event probe).
    And reuse TPARG_FL_TPOINT for this raw tracepoint probe.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Link: https://lore.kernel.org/oe-kbuild-all/202305020453.afTJ3VVp-lkp@intel.com/
    Signed-off-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>

--- kernel/trace/trace_probe.h
+++ kernel/trace/trace_probe.h
@@ -360,10 +360,19 @@ int trace_probe_print_args(struct trace_seq *s, struct probe_arg *args, int nr_a
 #define TPARG_FL_RETURN BIT(0)
 #define TPARG_FL_KERNEL BIT(1)
 #define TPARG_FL_FENTRY BIT(2)
-#define TPARG_FL_TPOINT BIT(3)
+#define TPARG_FL_TEVENT BIT(3)
 #define TPARG_FL_USER   BIT(4)
 #define TPARG_FL_FPROBE BIT(5)
-#define TPARG_FL_MASK	GENMASK(5, 0)
+#define TPARG_FL_TPOINT BIT(6)
+#define TPARG_FL_MASK	GENMASK(6, 0)
+
+static inline bool tparg_is_function_entry(unsigned int flags)
+{
+	/* Tracpoint probe is also function entry */
+	unsigned int mask = TPARG_FL_MASK & ~TPARG_FL_TPOINT;
+
+	return (flags & mask) == (TPARG_FL_KERNEL | TPARG_FL_FENTRY);
+}
 
 extern int traceprobe_parse_probe_arg(struct trace_probe *tp, int i,
 				const char *argv, unsigned int flags);
@@ -410,6 +419,7 @@ extern int traceprobe_define_arg_fields(struct trace_event_call *event_call,
 	C(MAXACT_TOO_BIG,	"Maxactive is too big"),		\
 	C(BAD_PROBE_ADDR,	"Invalid probed address or symbol"),	\
 	C(BAD_RETPROBE,		"Retprobe address must be an function entry"), \
+	C(NO_TRACEPOINT,	"Tracepoint is not found"),		\
 	C(BAD_ADDR_SUFFIX,	"Invalid probed address suffix"), \
 	C(NO_GROUP_NAME,	"Group name is not specified"),		\
 	C(GROUP_TOO_LONG,	"Group name is too long"),		\
