3bd1c9acdf0db8f3956657735fe6d36888a8f99f
Maxim Mikityanskiy <maxim@isovalent.com>
2023-05-19 22:52:57 +0300

    bpf: Track spilled unbounded scalars
    Support the pattern where an unbounded scalar is spilled to the stack,
    then boundary checks are performed on the src register, after which the
    stack frame slot is refilled into a register.
    
    Before this commit, the verifier didn't treat the src register and the
    stack slot as related if the src register was an unbounded scalar. The
    register state wasn't copied, the id wasn't preserved, and the stack
    slot was marked as STACK_MISC. Subsequent boundary checks on the src
    register wouldn't result in updating the boundaries of the spilled
    variable on the stack.
    
    After this commit, the verifier will preserve the bond between src and
    dst even if src is unbounded, which permits to do boundary checks on src
    and refill dst later, still remembering its boundaries. Such a pattern
    is sometimes generated by clang when compiling complex long functions.
    
    One test is adjusted to reflect the fact that an untracked register is
    marked as precise at an earlier stage.
    
    Signed-off-by: Maxim Mikityanskiy <maxim@isovalent.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -4222,20 +4222,6 @@ static bool register_is_const(struct bpf_reg_state *reg)
 	return reg->type == SCALAR_VALUE && tnum_is_const(reg->var_off);
 }
 
-static bool __is_scalar_unbounded(struct bpf_reg_state *reg)
-{
-	return tnum_is_unknown(reg->var_off) &&
-	       reg->smin_value == S64_MIN && reg->smax_value == S64_MAX &&
-	       reg->umin_value == 0 && reg->umax_value == U64_MAX &&
-	       reg->s32_min_value == S32_MIN && reg->s32_max_value == S32_MAX &&
-	       reg->u32_min_value == 0 && reg->u32_max_value == U32_MAX;
-}
-
-static bool register_is_bounded(struct bpf_reg_state *reg)
-{
-	return reg->type == SCALAR_VALUE && !__is_scalar_unbounded(reg);
-}
-
 static bool __is_pointer_value(bool allow_ptr_leaks,
 			       const struct bpf_reg_state *reg)
 {
@@ -4348,7 +4334,7 @@ static int check_stack_write_fixed_off(struct bpf_verifier_env *env,
 		return err;
 
 	mark_stack_slot_scratched(env, spi);
-	if (reg && !(off % BPF_REG_SIZE) && register_is_bounded(reg) &&
+	if (reg && !(off % BPF_REG_SIZE) && reg->type == SCALAR_VALUE &&
 	    !register_is_null(reg) && env->bpf_capable) {
 		bool reg_value_fits;
 
