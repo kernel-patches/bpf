4e4ed38b88a001271a44f363afbff00b9873b651
Maninder Singh <maninder1.s@samsung.com>
2023-05-31 09:56:00 +0530

    kallsyms: add kallsyms_show_value defination in all cases
    kallsyms_show_value return false if KALLSYMS is disabled,
    but its usage is done by module.c also.
    Thus when KALLSYMS is disabled, system will not print module
    load address:
    
    Earlier:
    =======
    / # insmod  test.ko
    / # lsmod
    test 12288 0 - Live 0x0000000000000000 (O)  // No Module Load address
    / #
    
    With change:
    ==========
    / # insmod test.ko
    / # lsmod
    test 12288 0 - Live 0xffff800000fc0000 (O)  // Module address
    / # cat /proc/modules
    test 12288 0 - Live 0xffff800000fc0000 (O)
    
    New file knosyms.c is added (name suggested by Kees Cook),
    which includes common defination irrespective of CONFIG_KALLSYMS.
    
    BPF code has high dependency on kallsyms,
    so bpf_dump_raw_ok check is not changed.
    
    Co-developed-by: Onkarnath <onkarnath.1@samsung.com>
    Signed-off-by: Onkarnath <onkarnath.1@samsung.com>
    Signed-off-by: Maninder Singh <maninder1.s@samsung.com>

--- include/linux/filter.h
+++ include/linux/filter.h
@@ -923,6 +923,7 @@ bool bpf_jit_supports_kfunc_call(void);
 bool bpf_jit_supports_far_kfunc_call(void);
 bool bpf_helper_changes_pkt_data(void *func);
 
+#ifdef CONFIG_KALLSYMS
 static inline bool bpf_dump_raw_ok(const struct cred *cred)
 {
 	/* Reconstruction of call-sites is dependent on kallsyms,
@@ -930,6 +931,12 @@ static inline bool bpf_dump_raw_ok(const struct cred *cred)
 	 */
 	return kallsyms_show_value(cred);
 }
+#else
+static inline bool bpf_dump_raw_ok(const struct cred *cred)
+{
+	return false;
+}
+#endif
 
 struct bpf_prog *bpf_patch_insn_single(struct bpf_prog *prog, u32 off,
 				       const struct bpf_insn *patch, u32 len);
