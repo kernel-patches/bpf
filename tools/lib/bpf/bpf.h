0bb35e330417b70aa8e3ca3cbe5338dd6a3c247a
Andrii Nakryiko <andrii@kernel.org>
2023-06-02 08:00:10 -0700

    libbpf: add BPF token support to bpf_prog_load() API
    Wire through token_fd into bpf_prog_load(). Also make sure to pass
    allowed_{prog,attach}_types to kernel in bpf_token_create().
    
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>

--- tools/lib/bpf/bpf.h
+++ tools/lib/bpf/bpf.h
@@ -104,9 +104,10 @@ struct bpf_prog_load_opts {
 	 * If kernel doesn't support this feature, log_size is left unchanged.
 	 */
 	__u32 log_true_size;
+	__u32 token_fd;
 	size_t :0;
 };
-#define bpf_prog_load_opts__last_field log_true_size
+#define bpf_prog_load_opts__last_field token_fd
 
 LIBBPF_API int bpf_prog_load(enum bpf_prog_type prog_type,
 			     const char *prog_name, const char *license,
@@ -560,9 +561,11 @@ struct bpf_token_create_opts {
 	__u32 token_fd;
 	__u64 allowed_cmds;
 	__u64 allowed_map_types;
+	__u64 allowed_prog_types;
+	__u64 allowed_attach_types;
 	size_t :0;
 };
-#define bpf_token_create_opts__last_field allowed_map_types
+#define bpf_token_create_opts__last_field allowed_attach_types
 
 LIBBPF_API int bpf_token_create(struct bpf_token_create_opts *opts);
 
