bf801892344c2a1e52eed341a75a763cabe62d47
Hou Tao <houtao1@huawei.com>
2023-06-13 16:09:21 +0800

    selftests/bpf: Add benchmark for bpf memory allocator
    The benchmark could be used to compare the performance of hash map
    operations and the memory usage between different flavors of bpf memory
    allocator (e.g., no bpf ma vs bpf ma vs reuse-after-gp bpf ma). It also
    could be used to check the performance improvement or the memory saving
    provided by optimization.
    
    The benchmark creates a non-preallocated hash map which uses bpf memory
    allocator and shows the operation performance and the memory usage of
    the hash map under different use cases:
    (1) overwrite
    Each CPU overwrites nonoverlapping part of hash map. When each CPU
    completes overwriting of 64 elements in hash map, it increases the
    op_count.
    (2) batch_add_batch_del
    Each CPU adds then deletes nonoverlapping part of hash map in batch.
    When each CPU adds and deletes 64 elements in hash map, it increases
    the op_count twice.
    (3) add_del_on_diff_cpu
    Each two-CPUs pair adds and deletes nonoverlapping part of map
    cooperatively. When each CPU adds or deletes 64 elements in hash map,
    it will increase the op_count.
    
    The following is the benchmark results when comparing between different
    flavors of bpf memory allocator. These tests are conducted on a KVM guest
    with 8 CPUs and 16 GB memory. The command line below is used to do all
    the following benchmarks:
    
      ./bench htab-mem --use-case $name ${OPTS} -w3 -d10 -a -p8
    
    These results show that preallocated hash map has both better performance
    and smaller memory usage.
    
    (1) non-preallocated + no bpf memory allocator (v6.0.19)
    use kmalloc() + call_rcu
    
    overwrite            per-prod-op :  8.54 ± 0.07k/s, avg mem: 48.11 ± 14.83MiB, peak mem: 91.84MiB
    batch_add_batch_del  per-prod-op : 12.99 ± 0.12k/s, avg mem: 32.88 ± 5.42MiB, peak mem: 67.50MiB
    add_del_on_diff_cpu  per-prod-op :  9.19 ± 0.07k/s, avg mem:  3.19 ± 0.40MiB, peak mem:  5.11MiB
    
    (2) preallocated
    OPTS=--preallocated
    
    overwrite            per-prod-op : 93.56 ± 0.03k/s, avg mem: 1.23 ± 0.00MiB, peak mem: 1.24MiB
    batch_add_batch_del  per-prod-op : 139.67 ± 0.08k/s, avg mem: 1.23 ± 0.00MiB, peak mem: 1.49MiB
    add_del_on_diff_cpu  per-prod-op : 15.51 ± 0.07k/s, avg mem: 1.77 ± 0.12MiB, peak mem: 1.95MiB
    
    (3) normal bpf memory allocator
    
    overwrite            per-prod-op : 89.45 ± 0.27k/s, avg mem: 2.26 ± 0.00MiB, peak mem: 2.74MiB
    batch_add_batch_del  per-prod-op : 59.17 ± 0.11k/s, avg mem: 2.34 ± 0.14MiB, peak mem: 2.74MiB
    add_del_on_diff_cpu  per-prod-op : 15.60 ± 0.20k/s, avg mem: 14.75 ± 2.77MiB, peak mem: 23.05MiB
    
    Signed-off-by: Hou Tao <houtao1@huawei.com>

---
+++ tools/testing/selftests/bpf/progs/htab_mem_bench.c
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (C) 2023. Huawei Technologies Co., Ltd */
+#include <stdbool.h>
+#include <errno.h>
+#include <linux/types.h>
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+struct update_ctx {
+	unsigned int from;
+	unsigned int step;
+};
+
+#define MAX_ENTRIES 8192
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(key_size, 4);
+	__uint(map_flags, BPF_F_NO_PREALLOC);
+	__uint(max_entries, MAX_ENTRIES);
+} htab SEC(".maps");
+
+char _license[] SEC("license") = "GPL";
+
+unsigned char zeroed_value[4096];
+unsigned int nr_thread = 0;
+long op_cnt = 0;
+
+static int write_htab(unsigned int i, struct update_ctx *ctx, unsigned int flags)
+{
+	if (ctx->from >= MAX_ENTRIES)
+		return 1;
+
+	bpf_map_update_elem(&htab, &ctx->from, zeroed_value, flags);
+	ctx->from += ctx->step;
+
+	return 0;
+}
+
+static int overwrite_htab(unsigned int i, struct update_ctx *ctx)
+{
+	return write_htab(i, ctx, 0);
+}
+
+static int newwrite_htab(unsigned int i, struct update_ctx *ctx)
+{
+	return write_htab(i, ctx, BPF_NOEXIST);
+}
+
+static int del_htab(unsigned int i, struct update_ctx *ctx)
+{
+	if (ctx->from >= MAX_ENTRIES)
+		return 1;
+
+	bpf_map_delete_elem(&htab, &ctx->from);
+	ctx->from += ctx->step;
+
+	return 0;
+}
+
+SEC("?tp/syscalls/sys_enter_getpgid")
+int overwrite(void *ctx)
+{
+	struct update_ctx update;
+
+	update.from = bpf_get_smp_processor_id();
+	update.step = nr_thread;
+	bpf_loop(64, overwrite_htab, &update, 0);
+	__sync_fetch_and_add(&op_cnt, 1);
+	return 0;
+}
+
+SEC("?tp/syscalls/sys_enter_getpgid")
+int batch_add_batch_del(void *ctx)
+{
+	struct update_ctx update;
+
+	update.from = bpf_get_smp_processor_id();
+	update.step = nr_thread;
+	bpf_loop(64, overwrite_htab, &update, 0);
+
+	update.from = bpf_get_smp_processor_id();
+	bpf_loop(64, del_htab, &update, 0);
+
+	__sync_fetch_and_add(&op_cnt, 2);
+	return 0;
+}
+
+SEC("?tp/syscalls/sys_enter_getpgid")
+int add_del_on_diff_cpu(void *ctx)
+{
+	struct update_ctx update;
+	unsigned int from;
+
+	from = bpf_get_smp_processor_id();
+	update.from = from / 2;
+	update.step = nr_thread / 2;
+
+	if (from & 1)
+		bpf_loop(64, newwrite_htab, &update, 0);
+	else
+		bpf_loop(64, del_htab, &update, 0);
+
+	__sync_fetch_and_add(&op_cnt, 1);
+	return 0;
+}
