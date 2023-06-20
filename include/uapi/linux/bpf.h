08acba885dc88dfe4d598d10cf7f036fa6a49513
Yafang Shao <laoar.shao@gmail.com>
2023-06-20 16:29:58 +0000

    bpf: Support ->fill_link_info for kprobe_multi
    With the addition of support for fill_link_info to the kprobe_multi link,
    users will gain the ability to inspect it conveniently using the
    `bpftool link show`. This enhancement provides valuable information to the
    user, including the count of probed functions and their respective
    addresses. It's important to note that if the kptr_restrict setting is not
    permitted, the probed address will not be exposed, ensuring security.
    
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -6438,6 +6438,11 @@ struct bpf_link_info {
 			__s32 priority;
 			__u32 flags;
 		} netfilter;
+		struct {
+			__aligned_u64 addrs; /* in/out: addresses buffer ptr */
+			__u32 count;
+			__u32 flags;
+		} kprobe_multi;
 	};
 } __attribute__((aligned(8)));
 
