006fa8a162887f53cc528a368d2faeed978d7fc3
Geliang Tang <geliang.tang@suse.com>
2023-07-29 17:57:23 +0800

    bpf: Register mptcp modret set
    This patch defines a mod_ret set named bpf_mptcp_fmodret_ids, Add
    update_socket_protocol into this set, and register it in
    bpf_mptcp_kfunc_init().
    
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>

--- net/mptcp/bpf.c
+++ net/mptcp/bpf.c
@@ -12,6 +12,23 @@
 #include <linux/bpf.h>
 #include "protocol.h"
 
+#ifdef CONFIG_BPF_JIT
+BTF_SET8_START(bpf_mptcp_fmodret_ids)
+BTF_ID_FLAGS(func, update_socket_protocol)
+BTF_SET8_END(bpf_mptcp_fmodret_ids)
+
+static const struct btf_kfunc_id_set bpf_mptcp_fmodret_set = {
+	.owner = THIS_MODULE,
+	.set   = &bpf_mptcp_fmodret_ids,
+};
+
+static int __init bpf_mptcp_kfunc_init(void)
+{
+	return register_btf_fmodret_id_set(&bpf_mptcp_fmodret_set);
+}
+late_initcall(bpf_mptcp_kfunc_init);
+#endif /* CONFIG_BPF_JIT */
+
 struct mptcp_sock *bpf_mptcp_sock_from_subflow(struct sock *sk)
 {
 	if (sk && sk_fullsock(sk) && sk->sk_protocol == IPPROTO_TCP && sk_is_mptcp(sk))
