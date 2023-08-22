0528b8cfc2ccc110ef0e4825ea2d90118639d837
Dave Marchevsky <davemarchevsky@fb.com>
2023-08-21 22:05:57 -0700

    bpf: Introduce task_vma open-coded iterator kfuncs
    This patch adds kfuncs bpf_iter_task_vma_{new,next,destroy} which allow
    creation and manipulation of struct bpf_iter_task_vma in open-coded
    iterator style. BPF programs can use these kfuncs directly or through
    bpf_for_each macro for natural-looking iteration of all task vmas.
    
    The implementation borrows heavily from bpf_find_vma helper's locking -
    differing only in that it holds the mmap_read lock for all iterations
    while the helper only executes its provided callback on a maximum of 1
    vma. Aside from locking, struct vma_iterator and vma_next do all the
    heavy lifting.
    
    The newly-added struct bpf_iter_task_vma has a name collision with a
    selftest for the seq_file task_vma iter's bpf skel, so the selftests/bpf/progs
    file is renamed in order to avoid the collision.
    
    A pointer to an inner data struct, struct bpf_iter_task_vma_kern_data, is the
    only field in struct bpf_iter_task_vma. This is because the inner data
    struct contains a struct vma_iterator (not ptr), whose size is likely to
    change under us. If bpf_iter_task_vma_kern contained vma_iterator directly
    such a change would require change in opaque bpf_iter_task_vma struct's
    size. So better to allocate vma_iterator using BPF allocator, and since
    that alloc must already succeed, might as well allocate all iter fields,
    thereby freezing struct bpf_iter_task_vma size.
    
    Signed-off-by: Dave Marchevsky <davemarchevsky@fb.com>
    Cc: Nathan Slingerland <slinger@meta.com>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -7311,4 +7311,8 @@ struct bpf_iter_num {
 	__u64 __opaque[1];
 } __attribute__((aligned(8)));
 
+struct bpf_iter_task_vma {
+	__u64 __opaque[1]; /* See bpf_iter_num comment above */
+} __attribute__((aligned(8)));
+
 #endif /* _UAPI__LINUX_BPF_H__ */
