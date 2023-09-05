7d005890d25f878d4b3a8fa230e2b69eff59e2e5
Hengqi Chen <hengqi.chen@gmail.com>
2023-09-05 15:12:57 +0000

    selftests/bpf: Add tests for symbol versioning for uprobe
    This exercises the newly added dynsym symbol versioning logics.
    Now we accept symbols in form of func, func@LIB_VERSION or
    func@@LIB_VERSION.
    
    The test rely on liburandom_read.so. For liburandom_read.so, we have:
    
        $ nm -D liburandom_read.so
                         w __cxa_finalize@GLIBC_2.17
                         w __gmon_start__
                         w _ITM_deregisterTMCloneTable
                         w _ITM_registerTMCloneTable
        0000000000000000 A LIBURANDOM_READ_1.0.0
        0000000000000000 A LIBURANDOM_READ_2.0.0
        000000000000081c T urandlib_api@@LIBURANDOM_READ_2.0.0
        0000000000000814 T urandlib_api@LIBURANDOM_READ_1.0.0
        0000000000000824 T urandlib_api_sameoffset@LIBURANDOM_READ_1.0.0
        0000000000000824 T urandlib_api_sameoffset@@LIBURANDOM_READ_2.0.0
        000000000000082c T urandlib_read_without_sema@@LIBURANDOM_READ_1.0.0
        00000000000007c4 T urandlib_read_with_sema@@LIBURANDOM_READ_1.0.0
        0000000000011018 D urandlib_read_with_sema_semaphore@@LIBURANDOM_READ_1.0.0
    
    For `urandlib_api`, specifying `urandlib_api` will cause a conflict because
    there are two symbols named urandlib_api and both are global bind.
    For `urandlib_api_sameoffset`, there are also two symbols in the .so, but
    both are at the same offset and essentially they refer to the same function
    so no conflict.
    
    Signed-off-by: Hengqi Chen <hengqi.chen@gmail.com>

--- tools/testing/selftests/bpf/urandom_read_lib1.c
+++ tools/testing/selftests/bpf/urandom_read_lib1.c
@@ -11,3 +11,44 @@ void urandlib_read_with_sema(int iter_num, int iter_cnt, int read_sz)
 {
 	STAP_PROBE3(urandlib, read_with_sema, iter_num, iter_cnt, read_sz);
 }
+
+/* Symbol versioning is different between static and shared library.
+ * Properly versioned symbols are needed for shared library, but
+ * only the symbol of the new version is needed for static library.
+ * Starting with GNU C 10, use symver attribute instead of .symver assembler
+ * directive, which works better with GCC LTO builds.
+ */
+#if defined(__GNUC__) && __GNUC__ >= 10
+
+#define DEFAULT_VERSION(internal_name, api_name, version) \
+	__attribute__((symver(#api_name "@@" #version)))
+#define COMPAT_VERSION(internal_name, api_name, version) \
+	__attribute__((symver(#api_name "@" #version)))
+
+#else
+
+#define COMPAT_VERSION(internal_name, api_name, version) \
+	asm(".symver " #internal_name "," #api_name "@" #version);
+#define DEFAULT_VERSION(internal_name, api_name, version) \
+	asm(".symver " #internal_name "," #api_name "@@" #version);
+
+#endif
+
+COMPAT_VERSION(urandlib_api_v1, urandlib_api, LIBURANDOM_READ_1.0.0)
+int urandlib_api_v1(void)
+{
+	return 1;
+}
+
+DEFAULT_VERSION(urandlib_api_v2, urandlib_api, LIBURANDOM_READ_2.0.0)
+int urandlib_api_v2(void)
+{
+	return 2;
+}
+
+COMPAT_VERSION(urandlib_api_sameoffset, urandlib_api_sameoffset, LIBURANDOM_READ_1.0.0)
+DEFAULT_VERSION(urandlib_api_sameoffset, urandlib_api_sameoffset, LIBURANDOM_READ_2.0.0)
+int urandlib_api_sameoffset(void)
+{
+	return 3;
+}
