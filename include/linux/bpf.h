26e9368437bfebd3f9cfaed22d7f3ff01f59d92a
Andrii Nakryiko <andrii@kernel.org>
2023-09-12 14:29:01 -0700

    bpf: consistenly use BPF token throughout BPF verifier logic
    Remove remaining direct queries to perfmon_capable() and bpf_capable()
    in BPF verifier logic and instead use BPF token (if available) to make
    decisions about privileges.
    
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -2172,24 +2172,26 @@ static inline void bpf_map_dec_elem_count(struct bpf_map *map)
 
 extern int sysctl_unprivileged_bpf_disabled;
 
-static inline bool bpf_allow_ptr_leaks(void)
+bool bpf_token_capable(const struct bpf_token *token, int cap);
+
+static inline bool bpf_allow_ptr_leaks(const struct bpf_token *token)
 {
-	return perfmon_capable();
+	return bpf_token_capable(token, CAP_PERFMON);
 }
 
-static inline bool bpf_allow_uninit_stack(void)
+static inline bool bpf_allow_uninit_stack(const struct bpf_token *token)
 {
-	return perfmon_capable();
+	return bpf_token_capable(token, CAP_PERFMON);
 }
 
-static inline bool bpf_bypass_spec_v1(void)
+static inline bool bpf_bypass_spec_v1(const struct bpf_token *token)
 {
-	return perfmon_capable();
+	return bpf_token_capable(token, CAP_PERFMON);
 }
 
-static inline bool bpf_bypass_spec_v4(void)
+static inline bool bpf_bypass_spec_v4(const struct bpf_token *token)
 {
-	return perfmon_capable();
+	return bpf_token_capable(token, CAP_PERFMON);
 }
 
 int bpf_map_new_fd(struct bpf_map *map, int flags);
