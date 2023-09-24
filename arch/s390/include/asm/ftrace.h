4b0ce92ee42564ded78473e8aaa2020601b4b2b9
Masami Hiramatsu (Google) <mhiramat@kernel.org>
2023-09-24 22:37:25 +0900

    tracing: Add ftrace_fill_perf_regs() for perf event
    Add ftrace_fill_perf_regs() which should be compatible with the
    perf_fetch_caller_regs(). In other words, the pt_regs returned from the
    ftrace_fill_perf_regs() must satisfy 'user_mode(regs) == false' and can be
    used for stack tracing.
    
    Signed-off-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>

--- arch/s390/include/asm/ftrace.h
+++ arch/s390/include/asm/ftrace.h
@@ -99,6 +99,11 @@ ftrace_regs_set_instruction_pointer(struct ftrace_regs *fregs,
 #define ftrace_regs_query_register_offset(name) \
 	regs_query_register_offset(name)
 
+#define arch_ftrace_fill_perf_regs(fregs, _regs)	 do {		\
+		(_regs)->psw.addr = (fregs)->regs.psw.addr;		\
+		(_regs)->gprs[15] = (fregs)->regs.gprs[15];		\
+	} while (0)
+
 #endif /* CONFIG_FUNCTION_TRACER */
 
 #ifdef CONFIG_DYNAMIC_FTRACE_WITH_DIRECT_CALLS
