915794539a0b7ff672ccb5b35ded780ceeef2d25
Andrii Nakryiko <andrii@kernel.org>
2023-09-27 15:57:59 -0700

    bpf: introduce BPF token object
    Add new kind of BPF kernel object, BPF token. BPF token is meant to
    allow delegating privileged BPF functionality, like loading a BPF
    program or creating a BPF map, from privileged process to a *trusted*
    unprivileged process, all while have a good amount of control over which
    privileged operations could be performed using provided BPF token.
    
    This is achieved through mounting BPF FS instance with extra delegation
    mount options, which determine what operations are delegatable, and also
    constraining it to the owning user namespace (as mentioned in the
    previous patch).
    
    BPF token itself is just a derivative from BPF FS and can be created
    through a new bpf() syscall command, BPF_TOKEN_CREAT, which accepts
    a path specification (using the usual fd + string path combo) to a BPF
    FS mount. Currently, BPF token "inherits" delegated command, map types,
    prog type, and attach type bit sets from BPF FS as is. In the future,
    having an BPF token as a separate object with its own FD, we can allow
    to further restrict BPF token's allowable set of things either at the creation
    time or after the fact, allowing the process to guard itself further
    from, e.g., unintentionally trying to load undesired kind of BPF
    programs. But for now we keep things simple and just copy bit sets as is.
    
    When BPF token is created from BPF FS mount, we take reference to the
    BPF super block's owning user namespace, and then use that namespace for
    checking all the {CAP_BPF, CAP_PERFMON, CAP_NET_ADMIN, CAP_SYS_ADMIN}
    capabilities that are normally only checked against init userns (using
    capable()), but now we check them using ns_capable() instead (if BPF
    token is provided). See bpf_token_capable() for details.
    
    Such setup means that BPF token in itself is not sufficient to grant BPF
    functionality. User namespaced process has to *also* have necessary
    combination of capabilities inside that user namespace. So while
    previously CAP_BPF was useless when granted within user namespace, now
    it gains a meaning and allows container managers and sys admins to have
    a flexible control over which processes can and need to use BPF
    functionality within the user namespace (i.e., container in practice).
    And BPF FS delegation mount options and derived BPF tokens serve as
    a per-container "flag" to grant overall ability to use bpf() (plus further
    restrict on which parts of bpf() syscalls are treated as namespaced).
    
    The alternative to creating BPF token object was:
      a) not having any extra object and just pasing BPF FS path to each
         relevant bpf() command. This seems suboptimal as it's racy (mount
         under the same path might change in between checking it and using it
         for bpf() command). And also less flexible if we'd like to further
         restrict ourselves compared to all the delegated functionality
         allowed on BPF FS.
      b) use non-bpf() interface, e.g., ioctl(), but otherwise also create
         a dedicated FD that would represent a token-like functionality. This
         doesn't seem superior to having a proper bpf() command, so
         BPF_TOKEN_CREATE was chosen.
    
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>

--- kernel/bpf/inode.c
+++ kernel/bpf/inode.c
@@ -99,9 +99,9 @@ static const struct inode_operations bpf_prog_iops = { };
 static const struct inode_operations bpf_map_iops  = { };
 static const struct inode_operations bpf_link_iops  = { };
 
-static struct inode *bpf_get_inode(struct super_block *sb,
-				   const struct inode *dir,
-				   umode_t mode)
+struct inode *bpf_get_inode(struct super_block *sb,
+			    const struct inode *dir,
+			    umode_t mode)
 {
 	struct inode *inode;
 
@@ -603,11 +603,13 @@ static int bpf_show_options(struct seq_file *m, struct dentry *root)
 {
 	struct bpf_mount_opts *opts = root->d_sb->s_fs_info;
 	umode_t mode = d_inode(root)->i_mode & S_IALLUGO & ~S_ISVTX;
+	u64 mask;
 
 	if (mode != S_IRWXUGO)
 		seq_printf(m, ",mode=%o", mode);
 
-	if (opts->delegate_cmds == ~0ULL)
+	mask = (1ULL << __MAX_BPF_CMD) - 1;
+	if ((opts->delegate_cmds & mask) == mask)
 		seq_printf(m, ",delegate_cmds=any");
 	else if (opts->delegate_cmds)
 		seq_printf(m, ",delegate_cmds=0x%llx", opts->delegate_cmds);
