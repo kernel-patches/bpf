bc841bf086aaa32161c563daeeec38f52e8af72a
Jiri Olsa <jolsa@kernel.org>
2023-10-02 19:53:18 +0200

    resolve_btfids: Check .BTF_ids section for work

--- tools/bpf/resolve_btfids/main.c
+++ tools/bpf/resolve_btfids/main.c
@@ -133,6 +133,11 @@ struct object {
 	int nr_typedefs;
 };
 
+static bool has_ids(struct object *obj)
+{
+	return obj->efile.ids.idx != -1;
+}
+
 static int verbose;
 
 static int eprintf(int level, int var, const char *fmt, ...)
@@ -748,13 +753,9 @@ int main(int argc, const char **argv)
 	if (elf_collect(&obj))
 		goto out;
 
-	/*
-	 * We did not find .BTF_ids section or symbols section,
-	 * nothing to do..
-	 */
-	if (obj.efile.ids.idx == -1 ||
-	    obj.efile.symbols.idx == -1) {
-		pr_debug("Cannot find .BTF_ids or symbols sections, nothing to do\n");
+	/* We did not find .BTF_ids section, nothing to do..  */
+	if (!has_ids(&obj)) {
+		pr_debug("Cannot find .BTF_ids section, nothing to do\n");
 		err = 0;
 		goto out;
 	}
