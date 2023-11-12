c721e6a60e8ec2b4ec67fe3942d999b4fd66e7aa
Alan Maguire <alan.maguire@oracle.com>
2023-11-12 12:48:34 +0000

    selftests/bpf: update btf_module test to ensure standalone BTF works
    btf_module test verifies that loading split BTF from bpf_testmod.ko
    is successful.  To test standalone BTF, add a test that loads
    BTF from bpf_testmod_standalone.ko and verifies we can look up BTF,
    just as we could for real split BTF.
    
    Signed-off-by: Alan Maguire <alan.maguire@oracle.com>

--- tools/testing/selftests/bpf/prog_tests/btf_module.c
+++ tools/testing/selftests/bpf/prog_tests/btf_module.c
@@ -5,11 +5,13 @@
 #include <bpf/btf.h>
 
 static const char *module_name = "bpf_testmod";
+static const char *standalone_module_name = "bpf_testmod_standalone";
 static const char *symbol_name = "bpf_testmod_test_read";
+static const char *standalone_symbol_name = "bpf_testmod_standalone_test_read";
 
 void test_btf_module()
 {
-	struct btf *vmlinux_btf, *module_btf;
+	struct btf *vmlinux_btf, *module_btf, *standalone_module_btf = NULL;
 	__s32 type_id;
 
 	if (!env.has_testmod) {
@@ -26,9 +28,22 @@ void test_btf_module()
 		goto cleanup;
 
 	type_id = btf__find_by_name(module_btf, symbol_name);
-	ASSERT_GT(type_id, 0, "func not found");
+	if (!ASSERT_GT(type_id, 0, "func not found"))
+		goto cleanup;
+
+	if (!ASSERT_OK(load_bpf_testmod(standalone_module_name, false), "load standalone BTF module"))
+		goto cleanup;
+
+	standalone_module_btf = btf__load_module_btf(standalone_module_name, vmlinux_btf);
+	if (!ASSERT_OK_PTR(standalone_module_btf, "could not load standalone module BTF"))
+		goto cleanup;
+
+	type_id = btf__find_by_name(standalone_module_btf, standalone_symbol_name);
+	ASSERT_GT(type_id, 0, "func not found in standalone");
 
 cleanup:
+	btf__free(standalone_module_btf);
 	btf__free(module_btf);
 	btf__free(vmlinux_btf);
+	unload_bpf_testmod(standalone_module_name, false);
 }
