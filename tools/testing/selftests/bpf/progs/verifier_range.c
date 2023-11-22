00c7df9ea7cf586ca4ed1de0ec8ab0e0a52ed25a
Tao Lyu <tao.lyu@epfl.ch>
2023-11-22 15:40:18 +0100

    C inlined assembly for reproducing max<min
    Hi Yonghong,
    
    Thanks for your reply.
    The C inlined assembly code is attached.
    I'm using clang-16, but it still fails.
    
    Best,
    Tao
    
    Signed-off-by: Tao Lyu <tao.lyu@epfl.ch>

---
+++ tools/testing/selftests/bpf/progs/verifier_range.c
@@ -0,0 +1,25 @@
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include "bpf_misc.h"
+
+SEC("?tc")
+__log_level(2)
+int test_verifier_range(void)
+{
+    asm volatile (
+        "r5 = 100; \
+        r5 /= 3; \
+        w5 >>= 7; \
+        r5 &= -386969681; \
+        r5 -= -884670597; \
+        w0 = w5; \
+        if w0 & 0x894b6a55 goto +2; \
+        r2 = 1; \
+        r2 = 1; \
+        r0 = 0; \
+        "
+    );
+    return 0;
+}
+
+char _license[] SEC("license") = "GPL";
