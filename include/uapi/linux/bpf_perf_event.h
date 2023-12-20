9d14df5c190be65bc3ee8b010d2a4a517f7e967a
Eduard Zingerman <eddyz87@gmail.com>
2023-12-20 15:34:09 +0200

    bpf: Mark virtual BPF context structures as preserve_static_offset
    Add __attribute__((preserve_static_offset)) for the following BPF
    related structures:
    - __sk_buff (*)
    - bpf_cgroup_dev_ctx (*)
    - bpf_nf_ctx
    - bpf_perf_event_data (*)
    - bpf_raw_tracepoint_args
    - bpf_sk_lookup (*)
    - bpf_sock (*)
    - bpf_sock_addr (*)
    - bpf_sock_ops (*)
    - bpf_sockopt (*)
    - bpf_sysctl (*)
    - sk_msg_md (*)
    - sk_reuseport_md (*)
    - xdp_md (*)
    
    Access to structures marked with (*) is rewritten by BPF verifier.
    (See verifier.c:convert_ctx_access). The rewrite requires that offsets
    used in access to fields of these structures are constant values.
    For the rest of the structures verifier just disallows access
    via modified context pointer in the following code path:
    
      check_mem_access
        check_ptr_off_reg
          __check_ptr_off_reg
            if (!fixed_off_ok && reg->off)
              "dereference of modified %s ptr R%d off=%d disallowed\n"
    
    Attribute preserve_static_offset [0] is a hint to clang that
    ensures that constant offsets are used.
    
    Attribute __attribute__((btf_decl_tag("preserve_static_offset")))
    would be used as a marker for bpftool to emit preserve_static_offset
    attribute when vmlinux.h is generated from BTF.
    
    Type 'pt_regs' is not handled yet.
    
    Note: !defined(__cplusplus) is necessary only to make
    tools/testing/selftests/bpf/test_cpp.cpp selftest compile:
    this test includes bpf.h and is compiled as C++.
    Without !defined(__cplusplus) the following error message
    is reported: "error: 'btf_decl_tag' attribute ignored".
    
    Apparently, when compiling C++, clang's implementation of
    __has_attribute returns true for attributes declared as COnly.
    This is possibly incorrect [1], but even if fixed in newer clang
    versions would be necessary for backwards compatibility.
    
    [0] https://clang.llvm.org/docs/AttributeReference.html#preserve-static-offset
    [1] https://discourse.llvm.org/t/when-compiling-c-has-attribute-returns-1-for-attributes-declared-as-conly-in-attr-td/
    
    Signed-off-by: Eduard Zingerman <eddyz87@gmail.com>

--- include/uapi/linux/bpf_perf_event.h
+++ include/uapi/linux/bpf_perf_event.h
@@ -10,10 +10,20 @@
 
 #include <asm/bpf_perf_event.h>
 
+#if __has_attribute(preserve_static_offset) && defined(__bpf__)
+#define __bpf_ctx __attribute__((preserve_static_offset))
+#elif __has_attribute(btf_decl_tag) && !defined(__cplusplus)
+#define __bpf_ctx __attribute__((btf_decl_tag(("preserve_static_offset"))))
+#else
+#define __bpf_ctx
+#endif
+
 struct bpf_perf_event_data {
 	bpf_user_pt_regs_t regs;
 	__u64 sample_period;
 	__u64 addr;
-};
+} __bpf_ctx;
+
+#undef __bpf_ctx
 
 #endif /* _UAPI__LINUX_BPF_PERF_EVENT_H__ */
