91aafb149024387b546d16e76601ec2f5290e977
Tiezhu Yang <yangtiezhu@loongson.cn>
2023-12-25 17:18:30 +0800

    bpf: Return -ENOTSUPP if callbacks are not allowed in non-JITed programs
    If CONFIG_BPF_JIT_ALWAYS_ON is not set and bpf_jit_enable is 0, there
    exist 6 failed tests.
    
      [root@linux bpf]# echo 0 > /proc/sys/net/core/bpf_jit_enable
      [root@linux bpf]# ./test_verifier | grep FAIL
      #107/p inline simple bpf_loop call FAIL
      #108/p don't inline bpf_loop call, flags non-zero FAIL
      #109/p don't inline bpf_loop call, callback non-constant FAIL
      #110/p bpf_loop_inline and a dead func FAIL
      #111/p bpf_loop_inline stack locations for loop vars FAIL
      #112/p inline bpf_loop call in a big program FAIL
      Summary: 505 PASSED, 266 SKIPPED, 6 FAILED
    
    The test log shows that callbacks are not allowed in non-JITed programs,
    interpreter doesn't support them yet, thus these tests should be skipped
    if jit is disabled, just return -ENOTSUPP instead of -EINVAL for pseudo
    calls in fixup_call_args().
    
    With this patch:
    
      [root@linux bpf]# echo 0 > /proc/sys/net/core/bpf_jit_enable
      [root@linux bpf]# ./test_verifier | grep FAIL
      Summary: 505 PASSED, 272 SKIPPED, 0 FAILED
    
    Signed-off-by: Tiezhu Yang <yangtiezhu@loongson.cn>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -19084,7 +19084,7 @@ static int fixup_call_args(struct bpf_verifier_env *env)
 			 * have to be rejected, since interpreter doesn't support them yet.
 			 */
 			verbose(env, "callbacks are not allowed in non-JITed programs\n");
-			return -EINVAL;
+			return -ENOTSUPP;
 		}
 
 		if (!bpf_pseudo_call(insn))
