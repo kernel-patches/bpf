31caeea870b997001bbbdec52cd7f54ef94dc409
Masami Hiramatsu (Google) <mhiramat@kernel.org>
2024-01-12 19:16:19 +0900

    tracing: Add ftrace_partial_regs() for converting ftrace_regs to pt_regs
    Add ftrace_partial_regs() which converts the ftrace_regs to pt_regs.
    If the architecture defines its own ftrace_regs, this copies partial
    registers to pt_regs and returns it. If not, ftrace_regs is the same as
    pt_regs and ftrace_partial_regs() will return ftrace_regs::regs.
    
    Signed-off-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    Acked-by: Florent Revest <revest@chromium.org>

--- arch/arm64/include/asm/ftrace.h
+++ arch/arm64/include/asm/ftrace.h
@@ -143,6 +143,17 @@ ftrace_override_function_with_return(struct ftrace_regs *fregs)
 	fregs->pc = fregs->lr;
 }
 
+static __always_inline struct pt_regs *
+ftrace_partial_regs(const struct ftrace_regs *fregs, struct pt_regs *regs)
+{
+	memcpy(regs->regs, fregs->regs, sizeof(u64) * 9);
+	regs->sp = fregs->sp;
+	regs->pc = fregs->pc;
+	regs->regs[29] = fregs->fp;
+	regs->regs[30] = fregs->lr;
+	return regs;
+}
+
 int ftrace_regs_query_register_offset(const char *name);
 
 int ftrace_init_nop(struct module *mod, struct dyn_ftrace *rec);
