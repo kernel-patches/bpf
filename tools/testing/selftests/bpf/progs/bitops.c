2298ba022ffeb92ed0b397803abd24c2b98f3a19
Leon Hwang <hffilwlqm@gmail.com>
2024-01-30 23:30:44 +0800

    bpf: Add generic kfunc bpf_ffs64()
    Signed-off-by: Leon Hwang <hffilwlqm@gmail.com>

---
+++ tools/testing/selftests/bpf/progs/bitops.c
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright Leon Hwang */
+
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+
+unsigned long bpf_ffs64(u64 word) __ksym;
+
+SEC("tc")
+int tc_ffs64(struct __sk_buff* skb)
+{
+	u64 *data = (u64 *)(long)skb->data;
+	void *data_end = (void *)(long)skb->data_end;
+	if ((void *)(u64)(data + 1) > data_end)
+		return -1;
+
+	return bpf_ffs64(*data);
+}
+
+char _license[] SEC("license") = "GPL";
