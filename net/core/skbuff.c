371b293bcbd93485027d9ccbae64704f923f44c9
Yunsheng Lin <linyunsheng@huawei.com>
2024-03-28 21:38:34 +0800

    mm: page_frag: add two inline helper for page_frag API
    Add two inline helpers for page_frag API to avoid calling
    accessing the field of 'struct page_frag_cache'.
    
    Signed-off-by: Yunsheng Lin <linyunsheng@huawei.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -742,12 +742,12 @@ struct sk_buff *__netdev_alloc_skb(struct net_device *dev, unsigned int len,
 	if (in_hardirq() || irqs_disabled()) {
 		nc = this_cpu_ptr(&netdev_alloc_cache);
 		data = page_frag_alloc_va(nc, len, gfp_mask);
-		pfmemalloc = nc->pfmemalloc;
+		pfmemalloc = page_frag_cache_is_pfmemalloc(nc);
 	} else {
 		local_bh_disable();
 		nc = this_cpu_ptr(&napi_alloc_cache.page);
 		data = page_frag_alloc_va(nc, len, gfp_mask);
-		pfmemalloc = nc->pfmemalloc;
+		pfmemalloc = page_frag_cache_is_pfmemalloc(nc);
 		local_bh_enable();
 	}
 
