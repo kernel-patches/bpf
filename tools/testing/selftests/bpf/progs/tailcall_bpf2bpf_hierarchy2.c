763ccb0b837cac90d32b343034d11d8b61c3db4c
Leon Hwang <hffilwlqm@gmail.com>
2024-04-02 22:11:01 +0800

    selftests/bpf: Add testcases for tailcall hierarchy fixing
    Add some test cases to confirm the tailcall hierarchy issue has been fixed.
    
    tools/testing/selftests/bpf/test_progs -t tailcalls
    311/18  tailcalls/tailcall_bpf2bpf_hierarchy_1:OK
    311/19  tailcalls/tailcall_bpf2bpf_hierarchy_fentry:OK
    311/20  tailcalls/tailcall_bpf2bpf_hierarchy_fexit:OK
    311/21  tailcalls/tailcall_bpf2bpf_hierarchy_fentry_fexit:OK
    311/22  tailcalls/tailcall_bpf2bpf_hierarchy_2:OK
    311/23  tailcalls/tailcall_bpf2bpf_hierarchy_3:OK
    311     tailcalls:OK
    Summary: 1/23 PASSED, 0 SKIPPED, 0 FAILED
    
    Signed-off-by: Leon Hwang <hffilwlqm@gmail.com>

---
+++ tools/testing/selftests/bpf/progs/tailcall_bpf2bpf_hierarchy2.c
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include "bpf_legacy.h"
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PROG_ARRAY);
+	__uint(max_entries, 2);
+	__uint(key_size, sizeof(__u32));
+	__uint(value_size, sizeof(__u32));
+} jmp_table SEC(".maps");
+
+int count0 = 0;
+int count1 = 0;
+
+static __noinline
+int subprog_tail0(struct __sk_buff *skb)
+{
+	bpf_tail_call_static(skb, &jmp_table, 0);
+	return 0;
+}
+
+SEC("tc")
+int classifier_0(struct __sk_buff *skb)
+{
+	if (count0 >= 100)
+		/* exit for abnormal case */
+		return count0;
+
+	count0++;
+	subprog_tail0(skb);
+	return 0;
+}
+
+static __noinline
+int subprog_tail1(struct __sk_buff *skb)
+{
+	bpf_tail_call_static(skb, &jmp_table, 1);
+	return 0;
+}
+
+SEC("tc")
+int classifier_1(struct __sk_buff *skb)
+{
+	if (count1 >= 100)
+		/* exit for abnormal case */
+		return count1;
+
+	count1++;
+	subprog_tail1(skb);
+	return 0;
+}
+
+SEC("tc")
+int entry(struct __sk_buff *skb)
+{
+	subprog_tail0(skb);
+	subprog_tail1(skb);
+
+	return 1;
+}
+
+char __license[] SEC("license") = "GPL";
