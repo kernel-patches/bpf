65a372785d94aa0be5ad2c06b7eecffd7025a98a
Vadim Fedorenko <vadfed@meta.com>
2024-04-16 13:40:03 -0700

    selftests: bpf: crypto skcipher algo selftests
    Add simple tc hook selftests to show the way to work with new crypto
    BPF API. Some tricky dynptr initialization is used to provide empty iv
    dynptr. Simple AES-ECB algo is used to demonstrate encryption and
    decryption of fixed size buffers.
    
    Signed-off-by: Vadim Fedorenko <vadfed@meta.com>

---
+++ tools/testing/selftests/bpf/progs/crypto_share.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (c) 2024 Meta Platforms, Inc. and affiliates. */
+
+#define UDP_TEST_PORT 7777
+
+struct crypto_syscall_args {
+	u32 key_len;
+	u32 authsize;
+};
+
