8d19a030d2adb884ff6c140ac42e62c11c2d8b69
Mykyta Yatsenko <yatsenko@meta.com>
2024-05-24 14:18:40 +0100

    libbpf: configure log verbosity with env variable
    Configure logging verbosity by setting LIBBPF_LOG_LEVEL environment
    variable, which is applied only to default logger. Once user set their
    custom logging callback, it is up to them to handle filtering.
    
    Signed-off-by: Mykyta Yatsenko <yatsenko@meta.com>

--- tools/lib/bpf/libbpf.c
+++ tools/lib/bpf/libbpf.c
@@ -229,7 +229,29 @@ static const char * const prog_type_name[] = {
 static int __base_pr(enum libbpf_print_level level, const char *format,
 		     va_list args)
 {
-	if (level == LIBBPF_DEBUG)
+	static enum libbpf_print_level min_level = LIBBPF_INFO;
+	static const char *env_var = "LIBBPF_LOG_LEVEL";
+	static bool initialized;
+
+	if (!initialized) {
+		char *verbosity;
+
+		initialized = true;
+		verbosity = getenv(env_var);
+		if (verbosity) {
+			if (strcasecmp(verbosity, "warn") == 0)
+				min_level = LIBBPF_WARN;
+			else if (strcasecmp(verbosity, "debug") == 0)
+				min_level = LIBBPF_DEBUG;
+			else if (strcasecmp(verbosity, "info") == 0)
+				min_level = LIBBPF_INFO;
+			else
+				fprintf(stderr, "Unexpected value of %s env variable\n", env_var);
+		}
+	}
+
+	/* if too verbose, skip logging  */
+	if (level > min_level)
 		return 0;
 
 	return vfprintf(stderr, format, args);
