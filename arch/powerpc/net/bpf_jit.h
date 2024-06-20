59b6979667b58365572a9be7fe85a7d3f77a014e
Naveen N Rao <naveen@kernel.org>
2024-06-21 00:39:11 +0530

    powerpc64/bpf: Add support for bpf trampolines
    Add support for bpf_arch_text_poke() and arch_prepare_bpf_trampoline()
    for 64-bit powerpc.
    
    BPF prog JIT is extended to mimic 64-bit powerpc approach for ftrace
    having a single nop at function entry, followed by the function
    profiling sequence out-of-line and a separate long branch stub for calls
    to trampolines that are out of range. A dummy_tramp is provided to
    simplify synchronization similar to arm64.
    
    BPF Trampolines adhere to the existing ftrace ABI utilizing a
    two-instruction profiling sequence, as well as the newer ABI utilizing a
    three-instruction profiling sequence enabling return with a 'blr'. The
    trampoline code itself closely follows x86 implementation.
    
    While the code is generic, BPF trampolines are only enabled on 64-bit
    powerpc. 32-bit powerpc will need testing and some updates.
    
    Signed-off-by: Naveen N Rao <naveen@kernel.org>

--- arch/powerpc/net/bpf_jit.h
+++ arch/powerpc/net/bpf_jit.h
@@ -21,6 +21,9 @@
 
 #define CTX_NIA(ctx) ((unsigned long)ctx->idx * 4)
 
+#define SZL			sizeof(unsigned long)
+#define BPF_INSN_SAFETY		64
+
 #define PLANT_INSTR(d, idx, instr)					      \
 	do { if (d) { (d)[idx] = instr; } idx++; } while (0)
 #define EMIT(instr)		PLANT_INSTR(image, ctx->idx, instr)
@@ -81,6 +84,13 @@
 				EMIT(PPC_RAW_ORI(d, d, (uintptr_t)(i) &       \
 							0xffff));             \
 		} } while (0)
+#define PPC_LI_ADDR	PPC_LI64
+#define PPC64_LOAD_PACA()						      \
+	EMIT(PPC_RAW_LD(_R2, _R13, offsetof(struct paca_struct, kernel_toc)))
+#else
+#define PPC_LI64	BUILD_BUG
+#define PPC_LI_ADDR	PPC_LI32
+#define PPC64_LOAD_PACA() BUILD_BUG()
 #endif
 
 /*
@@ -165,6 +175,7 @@ int bpf_jit_build_body(struct bpf_prog *fp, u32 *image, u32 *fimage, struct code
 		       u32 *addrs, int pass, bool extra_pass);
 void bpf_jit_build_prologue(u32 *image, struct codegen_context *ctx);
 void bpf_jit_build_epilogue(u32 *image, struct codegen_context *ctx);
+void bpf_jit_build_fentry_stubs(u32 *image, struct codegen_context *ctx);
 void bpf_jit_realloc_regs(struct codegen_context *ctx);
 int bpf_jit_emit_exit_insn(u32 *image, struct codegen_context *ctx, int tmp_reg, long exit_addr);
 
