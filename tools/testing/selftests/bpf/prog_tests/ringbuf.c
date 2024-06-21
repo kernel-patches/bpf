fece2c43e667fafec0711989620a546e0e4c78be
Daniel Borkmann <daniel@iogearbox.net>
2024-06-21 16:08:28 +0200

    selftests/bpf: Add more ring buffer test coverage
    Add test coverage for reservations beyond the ring buffer size in order
    to validate that bpf_ringbuf_reserve() rejects the request with NULL, all
    other ring buffer tests keep passing as well:
    
      # ./vmtest.sh -- ./test_progs -t ringbuf
      [...]
      ./test_progs -t ringbuf
      [    1.165434] bpf_testmod: loading out-of-tree module taints kernel.
      [    1.165825] bpf_testmod: module verification failed: signature and/or required key missing - tainting kernel
      [    1.284001] tsc: Refined TSC clocksource calibration: 3407.982 MHz
      [    1.286871] clocksource: tsc: mask: 0xffffffffffffffff max_cycles: 0x311fc34e357, max_idle_ns: 440795379773 ns
      [    1.289555] clocksource: Switched to clocksource tsc
      #274/1   ringbuf/ringbuf:OK
      #274/2   ringbuf/ringbuf_n:OK
      #274/3   ringbuf/ringbuf_map_key:OK
      #274/4   ringbuf/ringbuf_write:OK
      #274     ringbuf:OK
      #275     ringbuf_multi:OK
      [...]
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/20240621140828.18238-2-daniel@iogearbox.net

--- tools/testing/selftests/bpf/prog_tests/ringbuf.c
+++ tools/testing/selftests/bpf/prog_tests/ringbuf.c
@@ -12,9 +12,11 @@
 #include <sys/sysinfo.h>
 #include <linux/perf_event.h>
 #include <linux/ring_buffer.h>
+
 #include "test_ringbuf.lskel.h"
 #include "test_ringbuf_n.lskel.h"
 #include "test_ringbuf_map_key.lskel.h"
+#include "test_ringbuf_write.lskel.h"
 
 #define EDONE 7777
 
@@ -82,6 +84,49 @@ static void *poll_thread(void *input)
 	long timeout = (long)input;
 
 	return (void *)(long)ring_buffer__poll(ringbuf, timeout);
+}
+
+static void ringbuf_write_subtest(void)
+{
+	struct test_ringbuf_write_lskel *skel;
+	int page_size = getpagesize();
+	size_t *mmap_ptr;
+	int err, rb_fd;
+
+	skel = test_ringbuf_write_lskel__open();
+	if (!ASSERT_OK_PTR(skel, "skel_load"))
+		return;
+
+	skel->maps.ringbuf.max_entries = 0x4000;
+
+	err = test_ringbuf_write_lskel__load(skel);
+	if (!ASSERT_OK(err, "ringbuf_write"))
+		goto cleanup;
+
+	rb_fd = skel->maps.ringbuf.map_fd;
+
+	mmap_ptr = mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_SHARED, rb_fd, 0);
+	if (!ASSERT_OK_PTR(mmap_ptr, "rw_cons_pos"))
+		goto cleanup;
+	*mmap_ptr = 0x3000;
+	ASSERT_OK(munmap(mmap_ptr, page_size), "unmap_rw");
+
+	skel->bss->pid = getpid();
+
+	ringbuf = ring_buffer__new(rb_fd, process_sample, NULL, NULL);
+	if (!ASSERT_OK_PTR(ringbuf, "ringbuf_create"))
+		goto cleanup;
+
+	err = test_ringbuf_write_lskel__attach(skel);
+	if (!ASSERT_OK(err, "ringbuf_write"))
+		goto cleanup;
+
+	trigger_samples();
+	ASSERT_GE(skel->bss->discarded, 1, "discarded");
+	ASSERT_EQ(skel->bss->passed, 0, "passed");
+cleanup:
+	ring_buffer__free(ringbuf);
+	test_ringbuf_write_lskel__destroy(skel);
 }
 
 static void ringbuf_subtest(void)
@@ -451,4 +496,6 @@ void test_ringbuf(void)
 		ringbuf_n_subtest();
 	if (test__start_subtest("ringbuf_map_key"))
 		ringbuf_map_key_subtest();
+	if (test__start_subtest("ringbuf_write"))
+		ringbuf_write_subtest();
 }
