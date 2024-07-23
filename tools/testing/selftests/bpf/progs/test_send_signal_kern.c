7ea75798963b90a2f6ef47677f8d05627041f04b
Puranjay Mohan <puranjay@kernel.org>
2024-07-23 15:50:53 +0000

    selftests/bpf: Augment send_signal test with remote signaling
    Add testcases to test bpf_send_signal_remote(). In these new test cases,
    the main process triggers the BPF program and the forked process
    receives the signals.
    
    Signed-off-by: Puranjay Mohan <puranjay@kernel.org>

--- tools/testing/selftests/bpf/progs/test_send_signal_kern.c
+++ tools/testing/selftests/bpf/progs/test_send_signal_kern.c
@@ -4,23 +4,52 @@
 #include <linux/version.h>
 #include <bpf/bpf_helpers.h>
 
-__u32 sig = 0, pid = 0, status = 0, signal_thread = 0;
+enum pid_type {
+	PIDTYPE_PID,
+	PIDTYPE_TGID,
+	PIDTYPE_PGID,
+	PIDTYPE_SID,
+	PIDTYPE_MAX,
+};
+
+struct task_struct *bpf_task_from_pid(int pid) __ksym;
+void bpf_task_release(struct task_struct *p) __ksym;
+int bpf_send_signal_remote(struct task_struct *task, int sig, enum pid_type type) __ksym;
+
+__u32 sig = 0, pid = 0, status = 0, signal_thread = 0, target_pid = 0;
 
 static __always_inline int bpf_send_signal_test(void *ctx)
 {
+	struct task_struct *target_task = NULL;
 	int ret;
 
 	if (status != 0 || pid == 0)
 		return 0;
 
 	if ((bpf_get_current_pid_tgid() >> 32) == pid) {
-		if (signal_thread)
-			ret = bpf_send_signal_thread(sig);
-		else
-			ret = bpf_send_signal(sig);
+		if (target_pid) {
+			target_task = bpf_task_from_pid(target_pid);
+			if (!target_task)
+				return 0;
+		}
+
+		if (signal_thread) {
+			if (target_pid)
+				ret = bpf_send_signal_remote(target_task, sig, PIDTYPE_PID);
+			else
+				ret = bpf_send_signal_thread(sig);
+		} else {
+			if (target_pid)
+				ret = bpf_send_signal_remote(target_task, sig, PIDTYPE_TGID);
+			else
+				ret = bpf_send_signal(sig);
+		}
 		if (ret == 0)
 			status = 1;
 	}
+
+	if (target_task)
+		bpf_task_release(target_task);
 
 	return 0;
 }
