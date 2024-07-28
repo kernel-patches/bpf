c8c0f105bbe5a2b5091c75967a43bb181dd19be2
Tony Ambardar <tony.ambardar@gmail.com>
2024-07-27 17:56:51 -0700

    squash! selftests/bpf: Fix order-of-include compile errors in lwt_reroute.c
    FIXME FIXME
    
    rewrite message, not out-of-order but glibc compat with linux headers
    vs needing libc headers ahead of linux for non-glibc
    
    see c0bace798436 ("uapi libc compat: add fallback for unsupported libcs")
    for details

--- tools/testing/selftests/bpf/prog_tests/lwt_helpers.h
+++ tools/testing/selftests/bpf/prog_tests/lwt_helpers.h
@@ -6,8 +6,9 @@
 #include <time.h>
 #include <net/if.h>
 #include <linux/if_tun.h>
-#include "test_progs.h" /* between <net/if.h> and <linux/icmp.h> or errors */
 #include <linux/icmp.h>
+
+#include "test_progs.h"
 
 #define log_err(MSG, ...) \
 	fprintf(stderr, "(%s:%d: errno: %s) " MSG "\n", \
