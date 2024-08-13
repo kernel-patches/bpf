95d5d59108721563329c37a2fdde9a94b254c6f7
Thorsten Blum <thorsten.blum@toblux.com>
2024-08-13 17:17:53 +0200

    bpf: Annotate struct bpf_cand_cache with __counted_by()
    Add the __counted_by compiler attribute to the flexible array member
    cands to improve access bounds-checking via CONFIG_UBSAN_BOUNDS and
    CONFIG_FORTIFY_SOURCE.
    
    Increment cnt before adding a new struct to the cands array.
    
    Signed-off-by: Thorsten Blum <thorsten.blum@toblux.com>

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -7243,7 +7243,7 @@ struct bpf_cand_cache {
 	struct {
 		const struct btf *btf;
 		u32 id;
-	} cands[];
+	} cands[] __counted_by(cnt);
 };
 
 static DEFINE_MUTEX(cand_cache_mutex);
@@ -8787,9 +8787,9 @@ bpf_core_add_cands(struct bpf_cand_cache *cands, const struct btf *targ_btf,
 		memcpy(new_cands, cands, sizeof_cands(cands->cnt));
 		bpf_free_cands(cands);
 		cands = new_cands;
-		cands->cands[cands->cnt].btf = targ_btf;
-		cands->cands[cands->cnt].id = i;
 		cands->cnt++;
+		cands->cands[cands->cnt - 1].btf = targ_btf;
+		cands->cands[cands->cnt - 1].id = i;
 	}
 	return cands;
 }
