413c4addb7b9eaaaaef7d6056f6fb5c6b5e0aacf
Kui-Feng Lee <thinker.li@gmail.com>
2024-08-16 12:12:13 -0700

    selftests/bpf: Some basic __uptr tests
    Make sure the memory of uptrs have been mapped to the kernel properly.
    Also ensure the values of uptrs in the kernel haven't been copied
    to userspace.
    
    It also has the syscall update_elem/delete_elem test to test the
    pin/unpin code paths.
    
    Signed-off-by: Kui-Feng Lee <thinker.li@gmail.com>
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>

---
+++ tools/testing/selftests/bpf/uptr_test_common.h
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (c) 2024 Meta Platforms, Inc. and affiliates. */
+
+#ifndef _UPTR_TEST_COMMON_H
+#define _UPTR_TEST_COMMON_H
+
+#define MAGIC_VALUE 0xabcd1234
+
+#ifndef __BPF__
+struct cgroup {
+	int level;
+};
+#define __uptr
+#define __kptr
+#endif
+
+struct user_data {
+	int a;
+	int b;
+	int result;
+	int nested_result;
+};
+
+struct nested_udata {
+	struct user_data __uptr *udata;
+};
+
+struct value_type {
+	struct user_data __uptr *udata;
+	struct cgroup __kptr *cgrp;
+	struct nested_udata nested;
+};
+
+#endif
