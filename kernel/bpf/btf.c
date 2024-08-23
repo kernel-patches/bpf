7da73fdba42c2df41e136bc718e49df311161da0
Jeongjun Park <aha310510@gmail.com>
2024-08-23 19:43:10 +0900

    bpf: add check for invalid name in btf_name_valid_section()
    If the length of the name string is 1 and the value of name[0] is NULL
    byte, an OOB vulnerability occurs in btf_name_valid_section() and the
    return value is true, so the invalid name passes the check.
    
    To solve this, you need to check if the first position is NULL byte.
    
    Fixes: bd70a8fb7ca4 ("bpf: Allow all printable characters in BTF DATASEC names")
    Signed-off-by: Jeongjun Park <aha310510@gmail.com>

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -823,6 +823,9 @@ static bool btf_name_valid_section(const struct btf *btf, u32 offset)
 	const char *src = btf_str_by_offset(btf, offset);
 	const char *src_limit;
 
+	if (!*src)
+		return false;
+
 	/* set a limit on identifier length */
 	src_limit = src + KSYM_NAME_LEN;
 	src++;
