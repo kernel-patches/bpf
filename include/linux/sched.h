753eb867cbea35a9ba64f10011d32e157eaebe36
Yafang Shao <laoar.shao@gmail.com>
2024-08-28 11:03:14 +0800

    Get rid of __get_task_comm()
    We want to eliminate the use of __get_task_comm() for the following
    reasons:
    
    - The task_lock() is unnecessary
      Quoted from Linus [0]:
      : Since user space can randomly change their names anyway, using locking
      : was always wrong for readers (for writers it probably does make sense
      : to have some lock - although practically speaking nobody cares there
      : either, but at least for a writer some kind of race could have
      : long-term mixed results
    
    - The BUILD_BUG_ON() doesn't add any value
      The only requirement is to ensure that the destination buffer is a valid
      array.
    
    - Zeroing is not necessary in current use cases
      To avoid confusion, we should remove it. Moreover, not zeroing could
      potentially make it easier to uncover bugs. If the caller needs a
      zero-padded task name, it should be explicitly handled at the call site.
    
    Suggested-by: Linus Torvalds <torvalds@linux-foundation.org>
    Link: https://lore.kernel.org/all/CAHk-=wivfrF0_zvf+oj6==Sh=-npJooP8chLPEfaFV0oNYTTBA@mail.gmail.com [0]
    Link: https://lore.kernel.org/all/CAHk-=whWtUC-AjmGJveAETKOMeMFSTwKwu99v7+b6AyHMmaDFA@mail.gmail.com/
    Suggested-by: Alejandro Colomar <alx@kernel.org>
    Link: https://lore.kernel.org/all/2jxak5v6dfxlpbxhpm3ey7oup4g2lnr3ueurfbosf5wdo65dk4@srb3hsk72zwq
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Christian Brauner <brauner@kernel.org>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Eric Biederman <ebiederm@xmission.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Alexei Starovoitov <alexei.starovoitov@gmail.com>
    Cc: Matus Jokay <matus.jokay@stuba.sk>
    Cc: Alejandro Colomar <alx@kernel.org>
    Cc: "Serge E. Hallyn" <serge@hallyn.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1096,9 +1096,12 @@ struct task_struct {
 	/*
 	 * executable name, excluding path.
 	 *
-	 * - normally initialized setup_new_exec()
-	 * - access it with [gs]et_task_comm()
-	 * - lock it with task_lock()
+	 * - normally initialized begin_new_exec()
+	 * - set it with set_task_comm()
+	 *   - strscpy_pad() to ensure it is always NUL-terminated and
+	 *     zero-padded
+	 *   - task_lock() to ensure the operation is atomic and the name is
+	 *     fully updated.
 	 */
 	char				comm[TASK_COMM_LEN];
 
@@ -1914,10 +1917,27 @@ static inline void set_task_comm(struct task_struct *tsk, const char *from)
 	__set_task_comm(tsk, from, false);
 }
 
-extern char *__get_task_comm(char *to, size_t len, struct task_struct *tsk);
+/*
+ * - Why not use task_lock()?
+ *   User space can randomly change their names anyway, so locking for readers
+ *   doesn't make sense. For writers, locking is probably necessary, as a race
+ *   condition could lead to long-term mixed results.
+ *   The strscpy_pad() in __set_task_comm() can ensure that the task comm is
+ *   always NUL-terminated and zero-padded. Therefore the race condition between
+ *   reader and writer is not an issue.
+ *
+ * - Why not use strscpy_pad()?
+ *   While strscpy_pad() prevents writing garbage past the NUL terminator, which
+ *   is useful when using the task name as a key in a hash map, most use cases
+ *   don't require this. Zero-padding might confuse users if it’s unnecessary,
+ *   and not zeroing might even make it easier to expose bugs. If you need a
+ *   zero-padded task name, please handle that explicitly at the call site.
+ *
+ * - ARRAY_SIZE() can help ensure that @buf is indeed an array.
+ */
 #define get_task_comm(buf, tsk) ({			\
-	BUILD_BUG_ON(sizeof(buf) != TASK_COMM_LEN);	\
-	__get_task_comm(buf, sizeof(buf), tsk);		\
+	strscpy(buf, (tsk)->comm, ARRAY_SIZE(buf));	\
+	buf;						\
 })
 
 #ifdef CONFIG_SMP
