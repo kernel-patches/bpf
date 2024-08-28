c97bac030870ce6d05bde9584247bd3c61d8ff82
Juntong Deng <juntong.deng@outlook.com>
2024-08-28 21:04:33 +0100

    bpf: Add KF_OBTAIN for obtaining objects without reference count
    Not all structures in the kernel contain reference count, such as
    struct socket (its reference count is actually in struct file),
    so it makes no sense to use a combination of KF_ACQUIRE and KF_RELEASE
    to trick the verifier to make the pointer to struct socket valid.
    
    In this example, we cannot just use BTF_TYPE_SAFE_TRUSTED method,
    because not all files are sockets. It is not enough to make private_data
    in struct file to be trusted. We need a kfunc like bpf_socket_from_file.
    
    This patch adds KF_OBTAIN flag for the cases where a valid pointer can
    be obtained but there is no need to manipulate the reference count
    (e.g. the structure itself has no reference count, the actual reference
    count is in another structure).
    
    For KF_OBTAIN kfuncs, the passed argument must be valid pointers.
    KF_OBTAIN kfuncs guarantees that if the pointer passed in is valid,
    then the pointer returned by KF_OBTAIN kfuncs is also valid.
    
    For example, bpf_socket_from_file is a KF_OBTAIN kfunc, and if the
    struct file pointer passed in is valid, then the struct socket pointer
    returned is also valid.
    
    KF_OBTAIN kfuncs use ref_obj_id to ensure that the returned pointer has
    the correct ownership and lifetime. For example, if we pass pointer A to
    KF_OBTAIN kfunc and get returned pointer B, then once pointer A is
    released, pointer B will become invalid.
    
    Signed-off-by: Juntong Deng <juntong.deng@outlook.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -11001,9 +11001,15 @@ static bool is_kfunc_release(struct bpf_kfunc_call_arg_meta *meta)
 	return meta->kfunc_flags & KF_RELEASE;
 }
 
+static bool is_kfunc_obtain(struct bpf_kfunc_call_arg_meta *meta)
+{
+	return meta->kfunc_flags & KF_OBTAIN;
+}
+
 static bool is_kfunc_trusted_args(struct bpf_kfunc_call_arg_meta *meta)
 {
-	return (meta->kfunc_flags & KF_TRUSTED_ARGS) || is_kfunc_release(meta);
+	return (meta->kfunc_flags & KF_TRUSTED_ARGS) || is_kfunc_release(meta) ||
+		is_kfunc_obtain(meta);
 }
 
 static bool is_kfunc_sleepable(struct bpf_kfunc_call_arg_meta *meta)
@@ -12861,6 +12867,12 @@ static int check_kfunc_call(struct bpf_verifier_env *env, struct bpf_insn *insn,
 			/* For mark_ptr_or_null_reg, see 93c230e3f5bd6 */
 			regs[BPF_REG_0].id = ++env->id_gen;
 		}
+
+		if (is_kfunc_obtain(&meta)) {
+			regs[BPF_REG_0].type |= PTR_TRUSTED;
+			regs[BPF_REG_0].ref_obj_id = meta.ref_obj_id;
+		}
+
 		mark_btf_func_reg_size(env, BPF_REG_0, sizeof(void *));
 		if (is_kfunc_acquire(&meta)) {
 			int id = acquire_reference_state(env, insn_idx);
