c5ffdbfd93d905c9f8c76ab84e0ac189aa4fb412
Juntong Deng <juntong.deng@outlook.com>
2024-08-29 21:13:15 +0100

    selftests/bpf: Add tests for iter next method returning valid pointer
    This patch adds test cases for iter next method returning valid
    pointer, which can also used as usage examples.
    
    Currently iter next method should return valid pointer.
    
    iter_next_trusted is the correct usage and test if iter next method
    return valid pointer. bpf_iter_task_vma_next has KF_RET_NULL flag,
    so the returned pointer may be NULL. We need to check if the pointer
    is NULL before using it.
    
    iter_next_trusted_or_null is the incorrect usage. There is no checking
    before using the pointer, so it will be rejected by the verifier.
    
    iter_next_rcu and iter_next_rcu_or_null are similar test cases for
    KF_RCU_PROTECTED iterators.
    
    iter_next_rcu_not_trusted is used to test that the pointer returned by
    iter next method of KF_RCU_PROTECTED iterator cannot be passed in
    KF_TRUSTED_ARGS kfuncs.
    
    iter_next_ptr_mem_not_trusted is used to test that base type
    PTR_TO_MEM should not be combined with type flag PTR_TRUSTED.
    
    Signed-off-by: Juntong Deng <juntong.deng@outlook.com>

--- tools/testing/selftests/bpf/bpf_testmod/bpf_testmod_kfunc.h
+++ tools/testing/selftests/bpf/bpf_testmod/bpf_testmod_kfunc.h
@@ -148,4 +148,9 @@ struct sk_buff *bpf_kfunc_nested_acquire_nonzero_offset_test(struct sk_buff_head
 struct sk_buff *bpf_kfunc_nested_acquire_zero_offset_test(struct sock_common *ptr) __ksym;
 void bpf_kfunc_nested_release_test(struct sk_buff *ptr) __ksym;
 
+void bpf_kfunc_trusted_vma_test(struct vm_area_struct *ptr) __ksym;
+void bpf_kfunc_trusted_task_test(struct task_struct *ptr) __ksym;
+void bpf_kfunc_trusted_num_test(int *ptr) __ksym;
+void bpf_kfunc_rcu_task_test(struct task_struct *ptr) __ksym;
+
 #endif /* _BPF_TESTMOD_KFUNC_H */
