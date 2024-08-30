25875401ce6a434e4f8eb3b523493ef3c5dfd0b0
Leon Hwang <leon.hwang@linux.dev>
2024-08-30 22:53:40 +0800

    selftests/bpf: Add testcases for another tailcall infinite loop fixing
    The issue has been fixed on both x64 and arm64.
    
    On x64:
    
    cd tools/testing/selftests/bpf; ./test_progs -t tailcalls
    334/26  tailcalls/tailcall_bpf2bpf_freplace:OK
    334/27  tailcalls/tailcall_bpf2bpf_hierarchy_freplace_1:OK
    334/28  tailcalls/tailcall_bpf2bpf_hierarchy_freplace_2:OK
    334/29  tailcalls/tailcall_bpf2bpf_hierarchy_freplace_3:OK
    334/30  tailcalls/tailcall_bpf2bpf_hierarchy_freplace_4:OK
    334/31  tailcalls/tailcall_bpf2bpf_hierarchy_freplace_5:OK
    334     tailcalls:OK
    Summary: 1/31 PASSED, 0 SKIPPED, 0 FAILED
    
    on arm64:
    
    cd tools/testing/selftests/bpf; ./test_progs -t tailcalls
    334/26  tailcalls/tailcall_bpf2bpf_freplace:OK
    334/27  tailcalls/tailcall_bpf2bpf_hierarchy_freplace_1:OK
    334/28  tailcalls/tailcall_bpf2bpf_hierarchy_freplace_2:OK
    334/29  tailcalls/tailcall_bpf2bpf_hierarchy_freplace_3:OK
    334/30  tailcalls/tailcall_bpf2bpf_hierarchy_freplace_4:OK
    334/31  tailcalls/tailcall_bpf2bpf_hierarchy_freplace_5:OK
    334     tailcalls:OK
    Summary: 1/31 PASSED, 0 SKIPPED, 0 FAILED
    
    Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

--- tools/testing/selftests/bpf/progs/tc_bpf2bpf.c
+++ tools/testing/selftests/bpf/progs/tc_bpf2bpf.c
@@ -4,11 +4,30 @@
 #include <bpf/bpf_helpers.h>
 #include "bpf_misc.h"
 
+struct {
+	__uint(type, BPF_MAP_TYPE_PROG_ARRAY);
+	__uint(max_entries, 1);
+	__uint(key_size, sizeof(__u32));
+	__uint(value_size, sizeof(__u32));
+} jmp_table SEC(".maps");
+
 __noinline
-int subprog(struct __sk_buff *skb)
+int subprog_tailcall_tc(struct __sk_buff *skb)
 {
 	int ret = 1;
 
+	bpf_tail_call_static(skb, &jmp_table, 0);
+	__sink(skb);
+	__sink(ret);
+	return ret;
+}
+
+__noinline
+int subprog_tc(struct __sk_buff *skb)
+{
+	int ret = 1;
+
+	__sink(skb);
 	__sink(ret);
 	return ret;
 }
@@ -16,7 +35,21 @@ int subprog(struct __sk_buff *skb)
 SEC("tc")
 int entry_tc(struct __sk_buff *skb)
 {
-	return subprog(skb);
+	subprog_tc(skb);
+	return subprog_tailcall_tc(skb);
+}
+
+SEC("tc")
+int entry_tc_2(struct __sk_buff *skb)
+{
+	int ret, i;
+
+	for (i = 0; i < 10; i++) {
+		ret = subprog_tailcall_tc(skb);
+		__sink(ret);
+	}
+
+	return ret;
 }
 
 char __license[] SEC("license") = "GPL";
