ba07d9b4b5c0974ba06bb1eecd1f67d91c877a32
Simon Sundberg <simon.sundberg@kau.se>
2024-09-30 19:17:07 +0200

    selftests/bpf: Provide and use a generic (un)load_module helper
    Modify the old (un)load_bpf_testmod() helpers (in testing_helpers.c)
    to the more generic (un)load_module(), which can load an arbitrary
    kernel module by name, and refactor (un)load_bpf_testmod() to use this
    new helper. Also use the new helper to more robustly (un)load the the
    modules in prog_tests/kfunc_module_order.c.
    
    NOTE: Not intended to be a separate commit, will squash into the
    commit adding the selftest if we think this is a good idea.
    
    Signed-off-by: Simon Sundberg <simon.sundberg@kau.se>

--- tools/testing/selftests/bpf/prog_tests/kfunc_module_order.c
+++ tools/testing/selftests/bpf/prog_tests/kfunc_module_order.c
@@ -4,53 +4,6 @@
 
 #include "kfunc_module_order.skel.h"
 
-static int load_test_modorder_modules(void)
-{
-	int fd, err;
-
-	fd = open("bpf_test_modorder_x.ko", O_RDONLY);
-	if (!ASSERT_GE(fd, 0, "open(bpf_test_modorder_x.ko)"))
-		return -errno;
-
-	err = finit_module(fd, "", 0);
-	if (!ASSERT_OK(err, "Load bpf_test_modorder_x.ko module"))
-		goto err_fd;
-	close(fd);
-
-	fd = open("bpf_test_modorder_y.ko", O_RDONLY);
-	if (!ASSERT_GE(fd, 0, "open(bpf_test_modorder_y.ko)")) {
-		err = -errno;
-		goto err_modx;
-	}
-
-	err = finit_module(fd, "", 0);
-	if (!ASSERT_OK(err, "Load bpf_test_modorder_y.ko"))
-		goto err_modx;
-	close(fd);
-
-	return 0;
-
-err_modx:
-	delete_module("bpf_test_modorder_x", 0);
-err_fd:
-	if (fd)
-		close(fd);
-	return err;
-}
-
-static int unload_test_modorder_modules(void)
-{
-	int err1, err2;
-
-	err1 = delete_module("bpf_test_modorder_x", 0);
-	ASSERT_OK(err1, "Unloading bpf_test_modorder_x");
-
-	err2 = delete_module("bpf_test_modorder_y", 0);
-	ASSERT_OK(err2, "Unloading bpf_test_modorder_y");
-
-	return err1 ?: err2;
-}
-
 static int test_run_prog(const struct bpf_program *prog,
 			 struct bpf_test_run_opts *opts, int expect_val)
 {
@@ -75,9 +28,13 @@ void test_kfunc_module_order(void)
 	DECLARE_LIBBPF_OPTS(bpf_test_run_opts, test_opts, .data_in = pkt_data,
 			    .data_size_in = sizeof(pkt_data));
 
-	err = load_test_modorder_modules();
-	if (!ASSERT_OK(err, "Load bpf_test_modorder modules"))
+	err = load_module("bpf_test_modorder_x.ko", false);
+	if (!ASSERT_OK(err, "load bpf_test_modorder_x.ko"))
 		return;
+
+	err = load_module("bpf_test_modorder_y.ko", false);
+	if (!ASSERT_OK(err, "load bpf_test_modorder_y.ko"))
+		goto exit_modx;
 
 	skel = kfunc_module_order__open_and_load();
 	if (!ASSERT_OK_PTR(skel, "kfunc_module_order__open_and_load()")) {
@@ -90,5 +47,7 @@ void test_kfunc_module_order(void)
 
 	kfunc_module_order__destroy(skel);
 exit_mods:
-	unload_test_modorder_modules();
+	unload_module("bpf_test_modorder_y", false);
+exit_modx:
+	unload_module("bpf_test_modorder_x", false);
 }
