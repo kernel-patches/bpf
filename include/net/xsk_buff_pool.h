a41b8a6c4a105acf8789332a5edb21f0cc24efda
Maciej Fijalkowski <maciej.fijalkowski@intel.com>
2024-10-02 17:54:39 +0200

    xsk: carry a copy of xdp_zc_max_segs within xsk_buff_pool
    This so we avoid dereferencing struct net_device within hot path.
    
    Signed-off-by: Maciej Fijalkowski <maciej.fijalkowski@intel.com>

--- include/net/xsk_buff_pool.h
+++ include/net/xsk_buff_pool.h
@@ -77,6 +77,7 @@ struct xsk_buff_pool {
 	u32 chunk_shift;
 	u32 frame_len;
 	u8 tx_metadata_len; /* inherited from umem */
+	u32 xdp_zc_max_segs;
 	u8 cached_need_wakeup;
 	bool uses_need_wakeup;
 	bool unaligned;
