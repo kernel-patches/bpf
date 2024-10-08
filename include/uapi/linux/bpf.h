9c1d283472c3aa535534beb569abdd7032d227c7
Hou Tao <houtao1@huawei.com>
2024-10-08 17:14:51 +0800

    bpf: Introduce bpf_dynptr_user
    For bpf map with dynptr key support, the userspace application will use
    bpf_dynptr_user to represent the bpf_dynptr in the map key and pass it
    to bpf syscall. The bpf syscall will copy from bpf_dynptr_user to
    construct a corresponding bpf_dynptr_kern object when the map key is an
    input argument, and copy to bpf_dynptr_user from a bpf_dynptr_kern
    object when the map key is an output argument.
    
    For now the size of bpf_dynptr_user must be the same as bpf_dynptr, but
    the last u32 field is not used, so make it a reserved field.
    
    Signed-off-by: Hou Tao <houtao1@huawei.com>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -7327,6 +7327,12 @@ struct bpf_dynptr {
 	__u64 __opaque[2];
 } __attribute__((aligned(8)));
 
+struct bpf_dynptr_user {
+	__u64 data;
+	__u32 size;
+	__u32 rsvd;
+} __attribute__((aligned(8)));
+
 struct bpf_list_head {
 	__u64 __opaque[2];
 } __attribute__((aligned(8)));
