064fe36745c735f6f653a5719b418b56925453e9
Marc Kleine-Budde <mkl@pengutronix.de>
2024-10-16 23:51:54 +0200

    net: fec: fec_restart(): make use of FEC_ECR_RESET
    Replace the magic number "1" by the already existing define
    FEC_ECR_RESET.
    
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Reviewed-by: Wei Fang <wei.fang@nxp.com>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Signed-off-by: NipaLocal <nipa@local>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -1089,7 +1089,7 @@ fec_restart(struct net_device *ndev)
 	    ((fep->quirks & FEC_QUIRK_NO_HARD_RESET) && fep->link)) {
 		writel(0, fep->hwp + FEC_ECNTRL);
 	} else {
-		writel(1, fep->hwp + FEC_ECNTRL);
+		writel(FEC_ECR_RESET, fep->hwp + FEC_ECNTRL);
 		udelay(10);
 	}
 
