9840c5ef5eca7ab2794d0b7cd98c1ad301596d9a
Juntong Deng <juntong.deng@outlook.com>
2024-11-06 19:38:47 +0000

    bpf/crib: Add struct file related CRIB kfuncs
    This patch adds struct file related CRIB kfuncs.
    
    bpf_fget_task() is used to get a pointer to the struct file
    corresponding to the task file descriptor. Note that this function
    acquires a reference to struct file.
    
    bpf_get_file_ops_type() is used to determine what exactly this file
    is based on the file operations, such as socket, eventfd, timerfd,
    pipe, etc, in order to perform different checkpoint/restore processing
    for different file types. This function currently has only one return
    value, FILE_OPS_UNKNOWN, but will increase with the file types that
    CRIB supports for checkpoint/restore.
    
    Since the struct file_operations of most file types are currently
    static (e.g., socket_file_ops) and cannot be directly accessed in
    crib/files.c, the future plan is to add functions like
    is_socket_file_fops() to the corresponding files (e.g., net/socket.c).
    
    Signed-off-by: Juntong Deng <juntong.deng@outlook.com>

---
+++ kernel/bpf/crib/crib.c
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Checkpoint/Restore In eBPF (CRIB)
+ */
+
+#include <linux/bpf.h>
+#include <linux/btf.h>
+#include <linux/btf_ids.h>
+
+BTF_KFUNCS_START(bpf_crib_kfuncs)
+
+BTF_ID_FLAGS(func, bpf_fget_task, KF_ACQUIRE | KF_TRUSTED_ARGS | KF_RET_NULL)
+BTF_ID_FLAGS(func, bpf_get_file_ops_type, KF_TRUSTED_ARGS)
+BTF_ID_FLAGS(func, bpf_put_file, KF_RELEASE)
+
+BTF_KFUNCS_END(bpf_crib_kfuncs)
+
+static const struct btf_kfunc_id_set bpf_crib_kfunc_set = {
+	.owner = THIS_MODULE,
+	.set   = &bpf_crib_kfuncs,
+};
+
+static int __init bpf_crib_init(void)
+{
+	return register_btf_kfunc_id_set(BPF_PROG_TYPE_SYSCALL, &bpf_crib_kfunc_set);
+}
+
+late_initcall(bpf_crib_init);
