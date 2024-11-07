cdd5522ace325c80c3ef723130d21424c0d32118
Leon Hwang <leon.hwang@linux.dev>
2024-11-07 21:45:28 +0800

    bpf, x64: Propagate tailcall info only for subprogs
    In x64 JIT, propagate tailcall info only for subprogs, not for helpers
    or kfuncs.
    
    Acked-by: Yonghong Song <yonghong.song@linux.dev>
    Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

--- arch/x86/net/bpf_jit_comp.c
+++ arch/x86/net/bpf_jit_comp.c
@@ -2124,10 +2124,11 @@ populate_extable:
 
 			/* call */
 		case BPF_JMP | BPF_CALL: {
+			bool pseudo_call = src_reg == BPF_PSEUDO_CALL;
 			u8 *ip = image + addrs[i - 1];
 
 			func = (u8 *) __bpf_call_base + imm32;
-			if (tail_call_reachable) {
+			if (pseudo_call && tail_call_reachable) {
 				LOAD_TAIL_CALL_CNT_PTR(bpf_prog->aux->stack_depth);
 				ip += 7;
 			}
