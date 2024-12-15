dc33143d017a768496e8163941fd214f30022f42
Jiri Olsa <jolsa@kernel.org>
2024-12-15 15:15:02 +0100

    selftests/bpf: Add uprobe/usdt syscall tests
    Adding tests for optimized uprobe/usdt probes.
    
    Checking that we get expected trampoline and attached bpf programs
    get executed properly.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/testing/selftests/bpf/progs/uprobe_syscall_executed.c
+++ tools/testing/selftests/bpf/progs/uprobe_syscall_executed.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/usdt.bpf.h>
 #include <string.h>
 
 struct pt_regs regs;
@@ -9,9 +11,37 @@ char _license[] SEC("license") = "GPL";
 
 int executed = 0;
 
-SEC("uretprobe.multi")
-int test(struct pt_regs *regs)
+SEC("uprobe")
+int BPF_UPROBE(test_uprobe)
 {
-	executed = 1;
+	executed++;
+	return 0;
+}
+
+SEC("uretprobe")
+int BPF_URETPROBE(test_uretprobe)
+{
+	executed++;
+	return 0;
+}
+
+SEC("uprobe.multi")
+int test_uprobe_multi(struct pt_regs *ctx)
+{
+	executed++;
+	return 0;
+}
+
+SEC("uretprobe.multi")
+int test_uretprobe_multi(struct pt_regs *ctx)
+{
+	executed++;
+	return 0;
+}
+
+SEC("usdt")
+int test_usdt(struct pt_regs *ctx)
+{
+	executed++;
 	return 0;
 }
