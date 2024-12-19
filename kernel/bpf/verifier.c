d3b334ebdf287eff30ae6c4d9dae8ab1a163e678
Thomas Weißschuh <linux@weissschuh.net>
2024-12-19 08:24:28 +0100

    bpf: fix configuration-dependent BTF function references
    These BTF functions are not available unconditionally,
    only reference them when they are available.
    
    Avoid the following build warnings:
    
      BTF     .tmp_vmlinux1.btf.o
    btf_encoder__tag_kfunc: failed to find kfunc 'bpf_send_signal_task' in BTF
    btf_encoder__tag_kfuncs: failed to tag kfunc 'bpf_send_signal_task'
      NM      .tmp_vmlinux1.syms
      KSYMS   .tmp_vmlinux1.kallsyms.S
      AS      .tmp_vmlinux1.kallsyms.o
      LD      .tmp_vmlinux2
      NM      .tmp_vmlinux2.syms
      KSYMS   .tmp_vmlinux2.kallsyms.S
      AS      .tmp_vmlinux2.kallsyms.o
      LD      vmlinux
      BTFIDS  vmlinux
    WARN: resolve_btfids: unresolved symbol prog_test_ref_kfunc
    WARN: resolve_btfids: unresolved symbol bpf_crypto_ctx
    WARN: resolve_btfids: unresolved symbol bpf_send_signal_task
    WARN: resolve_btfids: unresolved symbol bpf_modify_return_test_tp
    WARN: resolve_btfids: unresolved symbol bpf_dynptr_from_xdp
    WARN: resolve_btfids: unresolved symbol bpf_dynptr_from_skb
    
    Signed-off-by: Thomas Weißschuh <linux@weissschuh.net>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -5507,7 +5507,9 @@ static bool in_rcu_cs(struct bpf_verifier_env *env)
 
 /* Once GCC supports btf_type_tag the following mechanism will be replaced with tag check */
 BTF_SET_START(rcu_protected_types)
+#ifdef CONFIG_NET
 BTF_ID(struct, prog_test_ref_kfunc)
+#endif
 #ifdef CONFIG_CGROUPS
 BTF_ID(struct, cgroup)
 #endif
@@ -5515,7 +5517,9 @@ BTF_ID(struct, cgroup)
 BTF_ID(struct, bpf_cpumask)
 #endif
 BTF_ID(struct, task_struct)
+#ifdef CONFIG_CRYPTO
 BTF_ID(struct, bpf_crypto_ctx)
+#endif
 BTF_SET_END(rcu_protected_types)
 
 static bool rcu_protected_object(const struct btf *btf, u32 btf_id)
@@ -11486,8 +11490,10 @@ BTF_ID(func, bpf_rdonly_cast)
 BTF_ID(func, bpf_rbtree_remove)
 BTF_ID(func, bpf_rbtree_add_impl)
 BTF_ID(func, bpf_rbtree_first)
+#ifdef CONFIG_NET
 BTF_ID(func, bpf_dynptr_from_skb)
 BTF_ID(func, bpf_dynptr_from_xdp)
+#endif
 BTF_ID(func, bpf_dynptr_slice)
 BTF_ID(func, bpf_dynptr_slice_rdwr)
 BTF_ID(func, bpf_dynptr_clone)
@@ -11515,8 +11521,13 @@ BTF_ID(func, bpf_rcu_read_unlock)
 BTF_ID(func, bpf_rbtree_remove)
 BTF_ID(func, bpf_rbtree_add_impl)
 BTF_ID(func, bpf_rbtree_first)
+#ifdef CONFIG_NET
 BTF_ID(func, bpf_dynptr_from_skb)
 BTF_ID(func, bpf_dynptr_from_xdp)
+#else
+BTF_ID_UNUSED
+BTF_ID_UNUSED
+#endif
 BTF_ID(func, bpf_dynptr_slice)
 BTF_ID(func, bpf_dynptr_slice_rdwr)
 BTF_ID(func, bpf_dynptr_clone)
