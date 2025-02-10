5488b53ec0df537e00a48a36784c52b92544a759
Dimitar Kanaliev <dimitar.kanaliev@siteground.com>
2025-02-10 15:19:59 +0200

    Fix bounds

--- tools/testing/selftests/bpf/progs/verifier_movsx.c
+++ tools/testing/selftests/bpf/progs/verifier_movsx.c
@@ -408,28 +408,19 @@ __naked void test_tnum_scast_s8_refine(void)
 {
     asm volatile ("                                      \
         call %[bpf_get_prandom_u32];                    \
-                                                        \
         r1 = r0;                                        \
         r1 &= 0x7F;                                     \
-                                                        \
         call %[bpf_get_prandom_u32];                    \
         r2 = r0;                                        \
         r2 &= 1;                                        \
-                                                        \
         r1 |= r2 << 7;                                  \
-                                                        \
         r1 = (s8) r1;                                   \
-                                                        \
         if r1 s< 0 goto fail_%=;                        \
-                                                        \
-        if r1 s>= 128 goto fail_%=;                     \
-                                                        \
+        if r1 s>= 127 goto fail_%=;                     \
         r2 = r10;                                       \
         r2 += -128;                                     \
         r2 += r1;                                       \
-                                                        \
         *(u8 *)(r2 + 0) = 0xAA;                         \
-                                                        \
         r0 = 1;                                         \
         exit;                                           \
 fail_%=:                                                \
