15246ce479bb32548237952669a9cb9275e5fb0b
Arthur Fabre <afabre@cloudflare.com>
2025-03-05 15:32:06 +0100

    bnxt: Propagate trait presence to skb
    Call the common xdp_buff_update_skb() helper.
    
    Signed-off-by: Arthur Fabre <afabre@cloudflare.com>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.c
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.c
@@ -2273,6 +2273,10 @@ static int bnxt_rx_pkt(struct bnxt *bp, struct bnxt_cp_ring_info *cpr,
 			}
 		}
 	}
+
+	if (xdp_active)
+		xdp_buff_update_skb(&xdp, skb);
+
 	bnxt_deliver_skb(bp, bnapi, skb);
 	rc = 1;
 
