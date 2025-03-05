ab008e41c74453c776469320af080923e63a5fd1
Bastien Curutchet (eBPF Foundation) <bastien.curutchet@bootlin.com>
2025-03-05 15:00:37 +0100

    selftests/bpf: test_xsk: Don't exit immediately on allocation failures
    If any allocation in the pkt_stream_*() helpers fail, exit_on_error() is
    called. This terminates the program immediately and can lead to memory
    leaks.
    
    Return NULL in case of allocation failure.
    Return TEST_FAILURE when something goes wrong in the packet generation.
    Clean up the resources if a failure happens between two steps of a test.
    Move exit_with_error() definition to xskxceiver.c as it isn't used
    anymore by test_xsk.c
    
    Signed-off-by: Bastien Curutchet (eBPF Foundation) <bastien.curutchet@bootlin.com>

--- tools/testing/selftests/bpf/test_xsk.h
+++ tools/testing/selftests/bpf/test_xsk.h
@@ -5,7 +5,6 @@
 #include <linux/ethtool.h>
 #include <linux/if_xdp.h>
 
-#include "../kselftest.h"
 #include "xsk.h"
 
 #ifndef SO_PREFER_BUSY_POLL
@@ -40,13 +39,6 @@ void xsk_fail(const char *msg, ...);
 #define skip_reason(x...) xsk_skip(x)
 #define fail_reason(x...) xsk_fail(x)
 
-static void __exit_with_error(int error, const char *file, const char *func, int line)
-{
-	ksft_test_result_fail("[%s:%s:%i]: ERROR: %d/\"%s\"\n", file, func, line, error,
-			      strerror(error));
-	ksft_exit_xfail();
-}
-#define exit_with_error(error) __exit_with_error(error, __FILE__, __func__, __LINE__)
 
 static inline u32 ceil_u32(u32 a, u32 b)
 {
