1a9da6833ea89e0f720eae8b7d32918b82f64df5
Jiri Olsa <jolsa@kernel.org>
2025-03-12 15:35:01 +0100

    uprobes: Remove breakpoint in unapply_uprobe under mmap_write_lock
    Currently unapply_uprobe takes mmap_read_lock, but it might call
    remove_breakpoint which eventually changes user pages.
    
    Current code writes either breakpoint or original instruction,
    so it probably go away with that, but with the upcoming changes
    that use multiple instructions on the probed address we need to
    ensure that any update to mm's pages is exclusive.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- kernel/events/uprobes.c
+++ kernel/events/uprobes.c
@@ -1482,7 +1482,7 @@ static int unapply_uprobe(struct uprobe *uprobe, struct mm_struct *mm)
 	struct vm_area_struct *vma;
 	int err = 0;
 
-	mmap_read_lock(mm);
+	mmap_write_lock(mm);
 	for_each_vma(vmi, vma) {
 		unsigned long vaddr;
 		loff_t offset;
@@ -1499,7 +1499,7 @@ static int unapply_uprobe(struct uprobe *uprobe, struct mm_struct *mm)
 		vaddr = offset_to_vaddr(vma, uprobe->offset);
 		err |= remove_breakpoint(uprobe, vma, vaddr);
 	}
-	mmap_read_unlock(mm);
+	mmap_write_unlock(mm);
 
 	return err;
 }
