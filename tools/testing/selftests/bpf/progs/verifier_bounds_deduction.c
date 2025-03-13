cc7da67b7a0c172aba456d45b80771ceb33c4507
Luis Gerhorst <luis.gerhorst@fau.de>
2025-03-13 17:25:58 +0100

    bpf: Fall back to nospec for sanitization-failures
    ALU sanitization was introduced to ensure that a subsequent ptr access
    can never go OOB, even under speculation. This is required because we
    currently allow speculative scalar confusion. Spec. scalar confusion is
    possible because Spectre v4 sanitization only adds a nospec after
    critical stores (e.g., scalar overwritten with a pointer).
    
    If we add a nospec before the ALU op, none of the operands can be
    subject to scalar confusion. As an ADD/SUB can not introduce scalar
    confusion itself, the result will also not be subject to scalar
    confusion. Therefore, the subsequent ptr access is always safe.
    
    We directly fall back to nospec for the sanitization errors
    REASON_BOUNDS, _TYPE, _PATHS, and _LIMIT, even if we are not on a
    speculative path. For REASON_STACK, we return the error directly now.
    
    Decided to directly set cur_aux(env)->nospec to implement the fallback
    instead of (conceptually) making the nospec part of the ALU sanitization
    state and therefore potentially dragging it through info.aux before
    copying it over into cur_aux(env). This has the drawback that the usage
    of cur_aux(env) and aux in these functions might be confusing, but it
    has the upside that it does not needlessly complicate the dataflow for
    nospec. Also the presence of cur_aux() might make it more obvious that
    aux might not equal cur_aux() here. In the commit window,
    sanitize_ptr_alu() will bail out early because can_skip_alu_sanitation()
    checks cur_aux(env)->nospec.
    
    Regarding commit 97744b4971d81 ("bpf: Clarify sanitize_check_bounds()"),
    having the assertion trigger if alu_state is set on a non-speculative
    path makes the most sense, because the masking would truncate the bounds
    on that path when executed and sanitize_check_bounds() exists to ensure
    this trucation does not happen. Two cases are relevant:
    
    - First, if a case in sanitize_check_bounds() is omitted, it fails with
      EOPNOTSUPP but retrieve_ptr_limit() returns 0 (thereby potentially not
      setting cur_aux(env)->nospec instead of setting alu_state). With the
      old/new assertion, this is cought.
    
    - Second, if a case is omitted from retrieve_ptr_limit() but not from
      sanitize_check_bounds(), bounds_ret equals 0 or -EACCES. If it is 0
      and the default case in retrieve_ptr_limit() runs errorously, we mark
      the insn for nospec-sanitization. This is not really a security
      problem and it could also only be detected by adding a
      verifier_bug_if(bounds_ret != -EOPNOTSUPP) into the default case in
      retrieve_ptr_limit(). It is not cought by the old/new assertion.
    
    Because it remove the possibility for these errors altogether, this
    change also fixes some unwarranted test failures on architectures that
    have bpf_jit_bypass_spec_v1 set (e.g., LoongArch).
    
    Signed-off-by: Luis Gerhorst <luis.gerhorst@fau.de>
    Acked-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
    Acked-by: Henriette Herzog <henriette.herzog@rub.de>
    Cc: Hengqi Chen <hengqi.chen@gmail.com>
    ---
    
    Changes since v4 of series:
    - Rebase and resolve merge conflicts with 97744b4971d81 ("bpf: Clarify
      sanitize_check_bounds()").
    - Change update_alu_sanitation_state() and sanitize_val_alu() to return
      void as they now never fail.
    - Link to v4 of series: https://lore.kernel.org/all/20250603213232.339242-1-luis.gerhorst@fau.de/
    
    ...

--- tools/testing/selftests/bpf/progs/verifier_bounds_deduction.c
+++ tools/testing/selftests/bpf/progs/verifier_bounds_deduction.c
@@ -8,22 +8,26 @@
 SEC("socket")
 __description("check deducing bounds from const, 1")
 __failure __msg("R0 tried to subtract pointer from scalar")
-__msg_unpriv("R1 has pointer with unsupported alu operation")
+__failure_unpriv
 __naked void deducing_bounds_from_const_1(void)
 {
 	asm volatile ("					\
 	r0 = 1;						\
 	if r0 s>= 1 goto l0_%=;				\
-l0_%=:	r0 -= r1;					\
+l0_%=:	/* unpriv: nospec (inserted to prevent `R1 has pointer with unsupported alu operation`) */\
+	r0 -= r1;					\
 	exit;						\
 "	::: __clobber_all);
 }
 
 SEC("socket")
 __description("check deducing bounds from const, 2")
-__success __failure_unpriv
-__msg_unpriv("R1 has pointer with unsupported alu operation")
+__success __success_unpriv
 __retval(1)
+#ifdef SPEC_V1
+__xlated_unpriv("nospec") /* inserted to prevent `R1 has pointer with unsupported alu operation` */
+__xlated_unpriv("r1 -= r0")
+#endif
 __naked void deducing_bounds_from_const_2(void)
 {
 	asm volatile ("					\
@@ -40,22 +44,26 @@ l1_%=:	r1 -= r0;					\
 SEC("socket")
 __description("check deducing bounds from const, 3")
 __failure __msg("R0 tried to subtract pointer from scalar")
-__msg_unpriv("R1 has pointer with unsupported alu operation")
+__failure_unpriv
 __naked void deducing_bounds_from_const_3(void)
 {
 	asm volatile ("					\
 	r0 = 0;						\
 	if r0 s<= 0 goto l0_%=;				\
-l0_%=:	r0 -= r1;					\
+l0_%=:	/* unpriv: nospec (inserted to prevent `R1 has pointer with unsupported alu operation`) */\
+	r0 -= r1;					\
 	exit;						\
 "	::: __clobber_all);
 }
 
 SEC("socket")
 __description("check deducing bounds from const, 4")
-__success __failure_unpriv
-__msg_unpriv("R6 has pointer with unsupported alu operation")
+__success __success_unpriv
 __retval(0)
+#ifdef SPEC_V1
+__xlated_unpriv("nospec") /* inserted to prevent `R6 has pointer with unsupported alu operation` */
+__xlated_unpriv("r6 -= r0")
+#endif
 __naked void deducing_bounds_from_const_4(void)
 {
 	asm volatile ("					\
@@ -73,12 +81,13 @@ l1_%=:	r6 -= r0;					\
 SEC("socket")
 __description("check deducing bounds from const, 5")
 __failure __msg("R0 tried to subtract pointer from scalar")
-__msg_unpriv("R1 has pointer with unsupported alu operation")
+__failure_unpriv
 __naked void deducing_bounds_from_const_5(void)
 {
 	asm volatile ("					\
 	r0 = 0;						\
 	if r0 s>= 1 goto l0_%=;				\
+	/* unpriv: nospec (inserted to prevent `R1 has pointer with unsupported alu operation`) */\
 	r0 -= r1;					\
 l0_%=:	exit;						\
 "	::: __clobber_all);
@@ -87,14 +96,15 @@ l0_%=:	exit;						\
 SEC("socket")
 __description("check deducing bounds from const, 6")
 __failure __msg("R0 tried to subtract pointer from scalar")
-__msg_unpriv("R1 has pointer with unsupported alu operation")
+__failure_unpriv
 __naked void deducing_bounds_from_const_6(void)
 {
 	asm volatile ("					\
 	r0 = 0;						\
 	if r0 s>= 0 goto l0_%=;				\
 	exit;						\
-l0_%=:	r0 -= r1;					\
+l0_%=:	/* unpriv: nospec (inserted to prevent `R1 has pointer with unsupported alu operation`) */\
+	r0 -= r1;					\
 	exit;						\
 "	::: __clobber_all);
 }
@@ -102,14 +112,15 @@ l0_%=:	r0 -= r1;					\
 SEC("socket")
 __description("check deducing bounds from const, 7")
 __failure __msg("dereference of modified ctx ptr")
-__msg_unpriv("R1 has pointer with unsupported alu operation")
+__failure_unpriv
 __flag(BPF_F_ANY_ALIGNMENT)
 __naked void deducing_bounds_from_const_7(void)
 {
 	asm volatile ("					\
 	r0 = %[__imm_0];				\
 	if r0 s>= 0 goto l0_%=;				\
-l0_%=:	r1 -= r0;					\
+l0_%=:	/* unpriv: nospec (inserted to prevent `R1 has pointer with unsupported alu operation`) */\
+	r1 -= r0;					\
 	r0 = *(u32*)(r1 + %[__sk_buff_mark]);		\
 	exit;						\
 "	:
@@ -121,13 +132,14 @@ l0_%=:	r1 -= r0;					\
 SEC("socket")
 __description("check deducing bounds from const, 8")
 __failure __msg("negative offset ctx ptr R1 off=-1 disallowed")
-__msg_unpriv("R1 has pointer with unsupported alu operation")
+__failure_unpriv
 __flag(BPF_F_ANY_ALIGNMENT)
 __naked void deducing_bounds_from_const_8(void)
 {
 	asm volatile ("					\
 	r0 = %[__imm_0];				\
 	if r0 s>= 0 goto l0_%=;				\
+	/* unpriv: nospec (inserted to prevent `R1 has pointer with unsupported alu operation`) */\
 	r1 += r0;					\
 l0_%=:	r0 = *(u32*)(r1 + %[__sk_buff_mark]);		\
 	exit;						\
@@ -140,13 +152,14 @@ l0_%=:	r0 = *(u32*)(r1 + %[__sk_buff_mark]);		\
 SEC("socket")
 __description("check deducing bounds from const, 9")
 __failure __msg("R0 tried to subtract pointer from scalar")
-__msg_unpriv("R1 has pointer with unsupported alu operation")
+__failure_unpriv
 __naked void deducing_bounds_from_const_9(void)
 {
 	asm volatile ("					\
 	r0 = 0;						\
 	if r0 s>= 0 goto l0_%=;				\
-l0_%=:	r0 -= r1;					\
+l0_%=:	/* unpriv: nospec (inserted to prevent `R1 has pointer with unsupported alu operation`) */\
+	r0 -= r1;					\
 	exit;						\
 "	::: __clobber_all);
 }
