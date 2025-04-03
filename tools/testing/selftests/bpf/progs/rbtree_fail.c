9d541ef4294ca1aa681ea85db16679a1d56e0ad0
Martin KaFai Lau <martin.lau@kernel.org>
2025-04-03 16:20:47 -0700

    selftests/bpf: Adjust test that did not allow refcounted node in rbtree_remove
    rbtree_remove allows refcounted node now. The
    rbtree_api_remove_unadded_node test needs to be adjust.
    
    First change, it does not expect a verifier's error now.
    
    Second change, the test now expects bpf_rbtree_remove(&groot, &m->node)
    to return NULL. The test uses __retval(0) to ensure this NULL
    return value.
    
    Some of the "only take non-owning..." failure messages are changed also.
    
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>

--- tools/testing/selftests/bpf/progs/rbtree_fail.c
+++ tools/testing/selftests/bpf/progs/rbtree_fail.c
@@ -69,11 +69,11 @@ long rbtree_api_nolock_first(void *ctx)
 }
 
 SEC("?tc")
-__failure __msg("bpf_rbtree_remove can only take non-owning bpf_rb_node pointer")
+__retval(0)
 long rbtree_api_remove_unadded_node(void *ctx)
 {
 	struct node_data *n, *m;
-	struct bpf_rb_node *res;
+	struct bpf_rb_node *res_n, *res_m;
 
 	n = bpf_obj_new(typeof(*n));
 	if (!n)
@@ -89,18 +89,20 @@ long rbtree_api_remove_unadded_node(void *ctx)
 	bpf_rbtree_add(&groot, &n->node, less);
 
 	/* This remove should pass verifier */
-	res = bpf_rbtree_remove(&groot, &n->node);
-	n = container_of(res, struct node_data, node);
+	res_n = bpf_rbtree_remove(&groot, &n->node);
 
 	/* This remove shouldn't, m isn't in an rbtree */
-	res = bpf_rbtree_remove(&groot, &m->node);
-	m = container_of(res, struct node_data, node);
+	res_m = bpf_rbtree_remove(&groot, &m->node);
 	bpf_spin_unlock(&glock);
 
-	if (n)
-		bpf_obj_drop(n);
-	if (m)
-		bpf_obj_drop(m);
+	bpf_obj_drop(m);
+	if (res_n)
+		bpf_obj_drop(container_of(res_n, struct node_data, node));
+	if (res_m) {
+		bpf_obj_drop(container_of(res_m, struct node_data, node));
+		return 2;
+	}
+
 	return 0;
 }
 
@@ -178,7 +180,7 @@ err_out:
 }
 
 SEC("?tc")
-__failure __msg("bpf_rbtree_remove can only take non-owning bpf_rb_node pointer")
+__failure __msg("bpf_rbtree_remove can only take non-owning or refcounted bpf_rb_node pointer")
 long rbtree_api_add_release_unlock_escape(void *ctx)
 {
 	struct node_data *n;
@@ -202,7 +204,7 @@ long rbtree_api_add_release_unlock_escape(void *ctx)
 }
 
 SEC("?tc")
-__failure __msg("bpf_rbtree_remove can only take non-owning bpf_rb_node pointer")
+__failure __msg("bpf_rbtree_remove can only take non-owning or refcounted bpf_rb_node pointer")
 long rbtree_api_first_release_unlock_escape(void *ctx)
 {
 	struct bpf_rb_node *res;
