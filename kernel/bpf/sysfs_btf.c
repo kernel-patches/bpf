68acb6ed90476e3dc246ad006a4b3118c8fd3c6a
Lorenz Bauer <lmb@isovalent.com>
2025-05-02 11:20:05 +0100

    btf: allow mmap of vmlinux btf
    User space needs access to kernel BTF for many modern features of BPF.
    Right now each process needs to read the BTF blob either in pieces or
    as a whole. Allow mmaping the sysfs file so that processes can directly
    access the memory allocated for it in the kernel.
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

--- kernel/bpf/sysfs_btf.c
+++ kernel/bpf/sysfs_btf.c
@@ -7,17 +7,49 @@
 #include <linux/kobject.h>
 #include <linux/init.h>
 #include <linux/sysfs.h>
+#include <linux/mm.h>
+#include <linux/io.h>
 
 /* See scripts/link-vmlinux.sh, gen_btf() func for details */
 extern char __start_BTF[];
 extern char __stop_BTF[];
 
+struct kobject *btf_kobj;
+
+static int btf_vmlinux_mmap(struct file *filp, struct kobject *kobj,
+			    const struct bin_attribute *attr,
+			    struct vm_area_struct *vma)
+{
+	phys_addr_t start = virt_to_phys(__start_BTF);
+	size_t btf_size = __stop_BTF - __start_BTF;
+	size_t vm_size = vma->vm_end - vma->vm_start;
+	unsigned long pfn = start >> PAGE_SHIFT;
+	unsigned long pages = PAGE_ALIGN(btf_size) >> PAGE_SHIFT;
+
+	if (kobj != btf_kobj)
+		return -EINVAL;
+
+	if (vma->vm_pgoff)
+		return -EINVAL;
+
+	if (vma->vm_flags & (VM_WRITE|VM_EXEC|VM_MAYSHARE))
+		return -EACCES;
+
+	if (pfn + pages < pfn)
+		return -EINVAL;
+
+	if (vm_size >> PAGE_SHIFT > pages)
+		return -EINVAL;
+
+	vm_flags_mod(vma, VM_DONTDUMP, VM_MAYEXEC|VM_MAYWRITE);
+	return remap_pfn_range(vma, vma->vm_start, pfn, vm_size, vma->vm_page_prot);
+}
+
 static struct bin_attribute bin_attr_btf_vmlinux __ro_after_init = {
 	.attr = { .name = "vmlinux", .mode = 0444, },
 	.read_new = sysfs_bin_attr_simple_read,
+	.mmap = btf_vmlinux_mmap,
 };
-
-struct kobject *btf_kobj;
 
 static int __init btf_vmlinux_init(void)
 {
