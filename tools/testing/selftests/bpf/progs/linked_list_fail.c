5bdd6db32f4263d08182e21beb16353dbea4f705
Ilya Leoshkevich <iii@linux.ibm.com>
2025-05-08 13:37:53 +0200

    selftests/bpf: Fix "expression result unused" warnings
    clang-21 complains about unused expressions in a few progs.
    Fix by explicitly casting the respective expressions to void.
    
    Signed-off-by: Ilya Leoshkevich <iii@linux.ibm.com>

--- tools/testing/selftests/bpf/progs/linked_list_fail.c
+++ tools/testing/selftests/bpf/progs/linked_list_fail.c
@@ -219,15 +219,14 @@ int obj_type_id_oor(void *ctx)
 SEC("?tc")
 int obj_new_no_composite(void *ctx)
 {
-	bpf_obj_new_impl(bpf_core_type_id_local(int), (void *)42);
+	(void)bpf_obj_new_impl(bpf_core_type_id_local(int), (void *)42);
 	return 0;
 }
 
 SEC("?tc")
 int obj_new_no_struct(void *ctx)
 {
-
-	bpf_obj_new(union { int data; unsigned udata; });
+	(void)bpf_obj_new(union { int data; unsigned udata; });
 	return 0;
 }
 
@@ -252,7 +251,7 @@ int new_null_ret(void *ctx)
 SEC("?tc")
 int obj_new_acq(void *ctx)
 {
-	bpf_obj_new(struct foo);
+	(void)bpf_obj_new(struct foo);
 	return 0;
 }
 
