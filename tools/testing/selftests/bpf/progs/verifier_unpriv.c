4b2725665c840a9df8d3eec68173a63e98fd509d
Ihor Solodrai <isolodrai@meta.com>
2025-06-04 15:27:28 -0700

    selftests/bpf: add cmp_map_pointer_with_const test
    Add a test for CONST_PTR_TO_MAP comparison with a non-0 constant. A
    BPF program with this code must not pass verification in unpriv.
    
    Signed-off-by: Ihor Solodrai <isolodrai@meta.com>

--- tools/testing/selftests/bpf/progs/verifier_unpriv.c
+++ tools/testing/selftests/bpf/progs/verifier_unpriv.c
@@ -635,6 +635,23 @@ l0_%=:	r0 = 0;						\
 }
 
 SEC("socket")
+__description("unpriv: cmp map pointer with const")
+__success __failure_unpriv __msg_unpriv("R1 pointer comparison prohibited")
+__retval(0)
+__naked void cmp_map_pointer_with_const(void)
+{
+	asm volatile ("					\
+	r1 = 0;						\
+	r1 = %[map_hash_8b] ll;				\
+	if r1 == 0xdeadbeef goto l0_%=;		\
+l0_%=:	r0 = 0;						\
+	exit;						\
+"	:
+	: __imm_addr(map_hash_8b)
+	: __clobber_all);
+}
+
+SEC("socket")
 __description("unpriv: write into frame pointer")
 __failure __msg("frame pointer is read only")
 __failure_unpriv
