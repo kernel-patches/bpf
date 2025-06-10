19d45bcff4a909e9e8cdfbb3dd2820772cb53b2e
Jerome Marchand <jmarchan@redhat.com>
2025-06-10 11:19:33 +0200

    selftests/bpf: Convert test_sysctl to prog_tests
    Convert test_sysctl test to prog_tests with minimal change to the
    tests themselves.
    
    Signed-off-by: Jerome Marchand <jmarchan@redhat.com>

--- tools/testing/selftests/bpf/prog_tests/test_sysctl.c
+++ tools/testing/selftests/bpf/prog_tests/test_sysctl.c
@@ -1,22 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0
 // Copyright (c) 2019 Facebook
 
-#include <fcntl.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-#include <linux/filter.h>
-
-#include <bpf/bpf.h>
-#include <bpf/libbpf.h>
-
-#include <bpf/bpf_endian.h>
-#include "bpf_util.h"
+#include "test_progs.h"
 #include "cgroup_helpers.h"
-#include "testing_helpers.h"
 
 #define CG_PATH			"/foo"
 #define MAX_INSNS		512
@@ -1608,26 +1594,22 @@ static int run_tests(int cgfd)
 	return fails ? -1 : 0;
 }
 
-int main(int argc, char **argv)
+void test_sysctl(void)
 {
 	int cgfd = -1;
-	int err = 0;
 
 	cgfd = cgroup_setup_and_join(CG_PATH);
-	if (cgfd < 0)
-		goto err;
+	if (CHECK_FAIL(cgfd < 0))
+		goto out;
 
 	/* Use libbpf 1.0 API mode */
 	libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
 
-	if (run_tests(cgfd))
-		goto err;
+	if (CHECK_FAIL(run_tests(cgfd)))
+		goto out;
 
-	goto out;
-err:
-	err = -1;
 out:
 	close(cgfd);
 	cleanup_cgroup_environment();
-	return err;
+	return;
 }
