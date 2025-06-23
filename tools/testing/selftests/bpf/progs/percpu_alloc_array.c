d786a1939ee51a4336a349498ed17c2a1b0453e1
Leon Hwang <leon.hwang@linux.dev>
2025-06-24 01:01:35 +0800

    selftests/bpf: Add case to test BPF_F_CPU
    This patch adds test coverage for the new BPF_F_CPU flag support in
    percpu_array maps. The following APIs are exercised:
    
    * bpf_map_update_batch()
    * bpf_map_lookup_batch()
    * bpf_map_update_elem()
    * bpf_map__update_elem()
    * bpf_map_lookup_elem_flags()
    * bpf_map__lookup_elem()
    
    cd tools/testing/selftests/bpf/
    ./test_progs -t percpu_alloc
    253/13  percpu_alloc/cpu_flag_tests:OK
    253     percpu_alloc:OK
    Summary: 1/13 PASSED, 0 SKIPPED, 0 FAILED
    
    Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

--- tools/testing/selftests/bpf/progs/percpu_alloc_array.c
+++ tools/testing/selftests/bpf/progs/percpu_alloc_array.c
@@ -187,4 +187,21 @@ out:
 	return 0;
 }
 
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(max_entries, 2);
+	__type(key, int);
+	__type(value, u64);
+} percpu SEC(".maps");
+
+SEC("?fentry/bpf_fentry_test1")
+int BPF_PROG(test_percpu_array, int x)
+{
+	u64 value = 0xDEADC0DE;
+	int key = 0;
+
+	bpf_map_update_elem(&percpu, &key, &value, BPF_ANY);
+	return 0;
+}
+
 char _license[] SEC("license") = "GPL";
