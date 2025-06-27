abad5903eb77258c39886e1e3406b32d50aa4852
Luis Gerhorst <luis.gerhorst@fau.de>
2025-06-27 22:25:54 +0200

    fixup to see warning in selftest 2

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -19978,7 +19978,9 @@ static int do_check(struct bpf_verifier_env *env)
 			 * to document this in case nospec_result is used
 			 * elsewhere in the future.
 			 */
-			WARN_ON_ONCE(env->insn_idx != prev_insn_idx + 1);
+			if (verifier_bug_if(env->insn_idx != prev_insn_idx + 1,
+					    env, "nospec_result"))
+				return -EFAULT;
 process_bpf_exit:
 			mark_verifier_state_scratched(env);
 			err = update_branch_counts(env, env->cur_state);
