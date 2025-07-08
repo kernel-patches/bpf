3f6988181f9264810d3cc48cd6d4d7131cb8f419
Josh Poimboeuf <jpoimboe@kernel.org>
2025-07-07 22:00:09 -0400

    perf: Support deferred user callchains
    Use the new unwind_deferred_trace() interface (if available) to defer
    unwinds to task context.  This will allow the use of .sframe (when it
    becomes available) and also prevents duplicate userspace unwinds.
    
    Suggested-by: Peter Zijlstra <peterz@infradead.org>
    Co-developed-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- kernel/events/callchain.c
+++ kernel/events/callchain.c
@@ -218,7 +218,7 @@ static void fixup_uretprobe_trampoline_entries(struct perf_callchain_entry *entr
 
 struct perf_callchain_entry *
 get_perf_callchain(struct pt_regs *regs, bool kernel, bool user,
-		   u32 max_stack, bool crosstask, bool add_mark)
+		   u32 max_stack, bool crosstask, bool add_mark, bool defer_user)
 {
 	struct perf_callchain_entry *entry;
 	struct perf_callchain_entry_ctx ctx;
@@ -249,6 +249,15 @@ get_perf_callchain(struct pt_regs *regs, bool kernel, bool user,
 			if (current->flags & (PF_KTHREAD | PF_USER_WORKER))
 				goto exit_put;
 			regs = task_pt_regs(current);
+		}
+
+		if (defer_user) {
+			/*
+			 * Foretell the coming of PERF_RECORD_CALLCHAIN_DEFERRED
+			 * which can be stitched to this one.
+			 */
+			perf_callchain_store_context(&ctx, PERF_CONTEXT_USER_DEFERRED);
+			goto exit_put;
 		}
 
 		if (add_mark)
