99426e45a2ad496bd33de1489479889b471cc134
KP Singh <kpsingh@kernel.org>
2025-07-12 03:29:05 +0200

    libbpf: Support exclusive map creation
    Implement setters and getters that allow map to be registers as
    exclusive to the specified program. The registration should be done
    before the exclusive program is loaded.

--- tools/lib/bpf/libbpf.h
+++ tools/lib/bpf/libbpf.h
@@ -1264,7 +1264,25 @@ LIBBPF_API int bpf_map__lookup_and_delete_elem(const struct bpf_map *map,
  */
 LIBBPF_API int bpf_map__get_next_key(const struct bpf_map *map,
 				     const void *cur_key, void *next_key, size_t key_sz);
+/**
+ * @brief **bpf_map__set_exclusive_program()** sets map to be exclusive to the
+ * to the specified program. The program must not be loaded yet.
+ * @param map BPF map to make exclusive.
+ * @param prog BPF program to be the exclusive user of the map.
+ * @return 0 on success; a negative error code otherwise.
+ *
+ * Once a map is made exclusive, only the specified program can access its
+ * contents.
+ */
+LIBBPF_API int bpf_map__set_exclusive_program(struct bpf_map *map, struct bpf_program *prog);
 
+/**
+ * @brief **bpf_map__get_exclusive_program()** sets map returns the exclusive program
+ * that registered with the map (if any)
+ * @param map BPF map to which the exclusive program is registered.
+ * @return registered program.
+ */
+LIBBPF_API struct bpf_program *bpf_map__get_exclusive_program(struct bpf_map *map);
 struct bpf_xdp_set_link_opts {
 	size_t sz;
 	int old_fd;
