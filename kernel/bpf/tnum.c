efed7578a226da3304b8dbe2131a7d59cd23f395
Dimitar Kanaliev <dimitar.kanaliev@siteground.com>
2025-08-05 11:19:56 +0300

    Retry with original helper

--- kernel/bpf/tnum.c
+++ kernel/bpf/tnum.c
@@ -155,47 +155,47 @@ struct tnum tnum_intersect(struct tnum a, struct tnum b)
 	return TNUM(v & ~mu, mu);
 }
 
-//struct tnum tnum_scast(struct tnum a, u8 size)
-//{
-//	u64 s = size * 8 - 1;
-//	u64 sign_mask;
-//	u64 value_mask;
-//	u64 new_value, new_mask;
-//	u64 sign_bit_unknown, sign_bit_value;
-//	u64 mask;
-//
-//	if (size >= 8) {
-//		return a;
-//	}
-//
-//	sign_mask = 1ULL << s;
-//	value_mask = (1ULL << (s + 1)) - 1;
-//
-//	new_value = a.value & value_mask;
-//	new_mask = a.mask & value_mask;
-//
-//	sign_bit_unknown = (a.mask >> s) & 1;
-//	sign_bit_value = (a.value >> s) & 1;
-//
-//	mask = ~value_mask;
-//	new_mask |= mask & (0 - sign_bit_unknown);
-//	new_value |= mask & (0 - ((sign_bit_unknown ^ 1) & sign_bit_value));
-//
-//	return TNUM(new_value, new_mask);
-//}
-
 struct tnum tnum_scast(struct tnum a, u8 size)
 {
-	u8 s = 64 - size * 8;
-	u64 value, mask;
+	u64 s = size * 8 - 1;
+	u64 sign_mask;
+	u64 value_mask;
+	u64 new_value, new_mask;
+	u64 sign_bit_unknown, sign_bit_value;
+	u64 mask;
 
-	if (size >= 8)
+	if (size >= 8) {
 		return a;
+	}
 
-	value = ((s64)a.value << s) >> s;
-	mask = ((s64)a.mask << s) >> s;
-	return TNUM(value, mask);
+	sign_mask = 1ULL << s;
+	value_mask = (1ULL << (s + 1)) - 1;
+
+	new_value = a.value & value_mask;
+	new_mask = a.mask & value_mask;
+
+	sign_bit_unknown = (a.mask >> s) & 1;
+	sign_bit_value = (a.value >> s) & 1;
+
+	mask = ~value_mask;
+	new_mask |= mask & (0 - sign_bit_unknown);
+	new_value |= mask & (0 - ((sign_bit_unknown ^ 1) & sign_bit_value));
+
+	return TNUM(new_value, new_mask);
 }
+
+//struct tnum tnum_scast(struct tnum a, u8 size)
+//{
+//	u8 s = 64 - size * 8;
+//	u64 value, mask;
+//
+//	if (size >= 8)
+//		return a;
+//
+//	value = ((s64)a.value << s) >> s;
+//	mask = ((s64)a.mask << s) >> s;
+//	return TNUM(value, mask);
+//}
 
 struct tnum tnum_cast(struct tnum a, u8 size)
 {
