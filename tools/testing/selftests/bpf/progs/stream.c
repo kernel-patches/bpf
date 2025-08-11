a85c322cc9da0ca0d35c8aad3604747c61e6db1c
Puranjay Mohan <puranjay@kernel.org>
2025-08-11 11:18:24 +0000

    selftests/bpf: Add tests for arena fault reporting
    Add selftests for testing the reporting of arena page faults through BPF
    streams. Two new bpf programs are added that read and write to an
    unmapped arena address and the fault reporting is verified in the
    userspace through streams.
    
    Signed-off-by: Puranjay Mohan <puranjay@kernel.org>

--- tools/testing/selftests/bpf/progs/stream.c
+++ tools/testing/selftests/bpf/progs/stream.c
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: GPL-2.0
 /* Copyright (c) 2025 Meta Platforms, Inc. and affiliates. */
+#define BPF_NO_KFUNC_PROTOTYPES
 #include <vmlinux.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_helpers.h>
 #include "bpf_misc.h"
 #include "bpf_experimental.h"
+#include "bpf_arena_common.h"
+
+extern int bpf_res_spin_lock(struct bpf_res_spin_lock *lock) __weak __ksym;
+extern void bpf_res_spin_unlock(struct bpf_res_spin_lock *lock) __weak __ksym;
 
 struct arr_elem {
 	struct bpf_res_spin_lock lock;
@@ -16,6 +21,12 @@ struct {
 	__type(key, int);
 	__type(value, struct arr_elem);
 } arrmap SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_ARENA);
+	__uint(map_flags, BPF_F_MMAPABLE);
+	__uint(max_entries, 1); /* number of pages */
+} arena SEC(".maps");
 
 #define ENOSPC 28
 #define _STR "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
@@ -74,6 +85,32 @@ int stream_syscall(void *ctx)
 {
 	bpf_stream_printk(BPF_STDOUT, "foo");
 	return 0;
+}
+
+SEC("syscall")
+__success __retval(0)
+int stream_arena_write_fault(void *ctx)
+{
+	unsigned char __arena *page;
+
+	page = bpf_arena_alloc_pages(&arena, NULL, 1, NUMA_NO_NODE, 0);
+	bpf_arena_free_pages(&arena, page, 1);
+
+	*(page + 0xbeef) = 1;
+
+	return 0;
+}
+
+SEC("syscall")
+__success __retval(0)
+int stream_arena_read_fault(void *ctx)
+{
+	unsigned char __arena *page;
+
+	page = bpf_arena_alloc_pages(&arena, NULL, 1, NUMA_NO_NODE, 0);
+	bpf_arena_free_pages(&arena, page, 1);
+
+	return *(page + 0xbeef);
 }
 
 char _license[] SEC("license") = "GPL";
