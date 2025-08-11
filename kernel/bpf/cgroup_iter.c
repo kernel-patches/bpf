2af61a7d9df3c17d35c30a525b9d24f853c3231a
Kumar Kartikeya Dwivedi <memxor@gmail.com>
2025-08-11 10:50:44 -0700

    bpf: Do not limit bpf_cgroup_from_id to current's namespace
    The bpf_cgroup_from_id kfunc relies on cgroup_get_from_id to obtain the
    cgroup corresponding to a given cgroup ID. This helper can be called in
    a lot of contexts where the current thread can be random. A recent
    example was its use in sched_ext's ops.tick(), to obtain the root cgroup
    pointer. Since the current task can be whatever random user space task
    preempted by the timer tick, this makes the behavior of the helper
    unreliable.
    
    Resolve this by refactoring cgroup_get_from_id to take a parameter to
    elide the cgroup_is_descendant check when root_cgns parameter is set to
    true.
    
    There is no compatibility breakage here, since changing the namespace
    against which the lookup is being done to the root cgroup namespace only
    permits a wider set of lookups to succeed now. The cgroup IDs across
    namespaces are globally unique, and thus don't need to be retranslated.
    
    Reported-by: Dan Schatzberg <dschatzberg@meta.com>
    Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
    Acked-by: Tejun Heo <tj@kernel.org>

--- kernel/bpf/cgroup_iter.c
+++ kernel/bpf/cgroup_iter.c
@@ -212,7 +212,7 @@ static int bpf_iter_attach_cgroup(struct bpf_prog *prog,
 	if (fd)
 		cgrp = cgroup_v1v2_get_from_fd(fd);
 	else if (id)
-		cgrp = cgroup_get_from_id(id);
+		cgrp = cgroup_get_from_id(id, false);
 	else /* walk the entire hierarchy by default. */
 		cgrp = cgroup_get_from_path("/");
 
