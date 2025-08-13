0c63a97566664f1137c88907f880045f6016fd40
KP Singh <kpsingh@kernel.org>
2025-08-13 22:55:16 +0200

    bpf: Implement exclusive map creation
    Exclusive maps allow maps to only be accessed by program with a
    program with a matching hash which is specified in the excl_prog_hash
    attr.
    
    For the signing use-case, this allows the trusted loader program
    to load the map and verify the integrity
    
    Signed-off-by: KP Singh <kpsingh@kernel.org>

--- tools/include/uapi/linux/bpf.h
+++ tools/include/uapi/linux/bpf.h
@@ -1522,6 +1522,8 @@ union bpf_attr {
 		 * If provided, map_flags should have BPF_F_TOKEN_FD flag set.
 		 */
 		__s32	map_token_fd;
+		__u32 excl_prog_hash_size;
+		__aligned_u64 excl_prog_hash;
 	};
 
 	struct { /* anonymous struct used by BPF_MAP_*_ELEM and BPF_MAP_FREEZE commands */
