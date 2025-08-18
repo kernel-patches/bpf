2a2ce786d35d879d0198cdfe51438ed5c1cd0788
Yafang Shao <laoar.shao@gmail.com>
2025-08-18 03:35:33 +0000

    bpf: mark vma->vm_mm as trusted
    Every VMA must have an associated mm_struct, and it is safe to access
    outside of RCU. Thus, we can mark it as trusted. With this change, BPF
    helpers can safely access vma->vm_mm to retrieve the associated task
    from the VMA.
    
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -7159,6 +7159,10 @@ BTF_TYPE_SAFE_TRUSTED(struct file) {
 	struct inode *f_inode;
 };
 
+BTF_TYPE_SAFE_TRUSTED(struct vm_area_struct) {
+	struct mm_struct *vm_mm;
+};
+
 BTF_TYPE_SAFE_TRUSTED_OR_NULL(struct dentry) {
 	struct inode *d_inode;
 };
@@ -7198,6 +7202,7 @@ static bool type_is_trusted(struct bpf_verifier_env *env,
 	BTF_TYPE_EMIT(BTF_TYPE_SAFE_TRUSTED(struct bpf_iter__task));
 	BTF_TYPE_EMIT(BTF_TYPE_SAFE_TRUSTED(struct linux_binprm));
 	BTF_TYPE_EMIT(BTF_TYPE_SAFE_TRUSTED(struct file));
+	BTF_TYPE_EMIT(BTF_TYPE_SAFE_TRUSTED(struct vm_area_struct));
 
 	return btf_nested_type_is_trusted(&env->log, reg, field_name, btf_id, "__safe_trusted");
 }
