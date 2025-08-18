93617c9ba987b6334c66c168befd4f617365aae7
Jiawei Zhao <phoenix500526@163.com>
2025-08-18 07:46:31 +0000

    selftests/bpf: Add an usdt_o1 test case in selftests to cover SIB handling logic
    When using GCC on x86-64 to compile an usdt prog with -O1 or higher
    optimization, the compiler will generate SIB addressing mode for global
    array and PC-relative addressing mode for global variable,
    e.g. "1@-96(%rbp,%rax,8)" and "-1@4+t1(%rip)".
    
    In this patch:
    - add usdt_o1 test case to cover SIB addressing usdt argument spec
      handling logic
    
    Signed-off-by: Jiawei Zhao <phoenix500526@163.com>

---
+++ tools/testing/selftests/bpf/prog_tests/usdt_o1.c
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (c) 2025 Jiawei Zhao <phoenix500526@163.com>. */
+#include <test_progs.h>
+
+#include "../sdt.h"
+#include "test_usdt_o1.skel.h"
+
+#if (defined(__GNUC__) && !defined(__clang__))
+#pragma GCC optimize("O1")
+#else
+#pragma message("non-gcc compiler: the correct probes might not be installed")
+#endif
+
+
+#define test_value 0xFEDCBA9876543210ULL
+#define SEC(name) __attribute__((section(name), used))
+
+int lets_test_this(int);
+static volatile __u64 array[1] = {test_value};
+
+static __always_inline void trigger_func(void)
+{
+	/* Base address + offset + (index * scale) */
+	for (volatile int i = 0; i <= 0; i++)
+		STAP_PROBE1(test, usdt1, array[i]);
+}
+
+static void basic_sib_usdt(void)
+{
+	LIBBPF_OPTS(bpf_usdt_opts, opts);
+	struct test_usdt_o1 *skel;
+	struct test_usdt_o1__bss *bss;
+	int err;
+
+	skel = test_usdt_o1__open_and_load();
+	if (!ASSERT_OK_PTR(skel, "skel_open"))
+		return;
+
+	bss = skel->bss;
+	bss->my_pid = getpid();
+
+	err = test_usdt_o1__attach(skel);
+	if (!ASSERT_OK(err, "skel_attach"))
+		goto cleanup;
+
+	/* usdt1 won't be auto-attached */
+	opts.usdt_cookie = 0xcafedeadbeeffeed;
+	skel->links.usdt1 = bpf_program__attach_usdt(skel->progs.usdt1,
+						     0 /*self*/, "/proc/self/exe",
+						     "test", "usdt1", &opts);
+	if (!ASSERT_OK_PTR(skel->links.usdt1, "usdt1_link"))
+		goto cleanup;
+
+	trigger_func();
+
+	ASSERT_EQ(bss->usdt1_called, 1, "usdt1_called");
+	ASSERT_EQ(bss->usdt1_cookie, 0xcafedeadbeeffeed, "usdt1_cookie");
+	ASSERT_EQ(bss->usdt1_arg_cnt, 1, "usdt1_arg_cnt");
+	ASSERT_EQ(bss->usdt1_arg, test_value, "usdt1_arg");
+	ASSERT_EQ(bss->usdt1_arg_ret, 0, "usdt1_arg_ret");
+	ASSERT_EQ(bss->usdt1_arg_size, sizeof(array[0]), "usdt1_arg_size");
+
+cleanup:
+	test_usdt_o1__destroy(skel);
+}
+
+void test_usdt_o1(void)
+{
+	basic_sib_usdt();
+}
