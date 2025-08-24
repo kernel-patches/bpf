63c0bee3b93fac72de1759ac9d9a1dbc52167188
Jordan Rife <jordan@jrife.io>
2025-08-24 09:55:45 -0700

    selftests/bpf: Socket map + sockops insert and destroy
    Use a sockops program to automatically insert sockets into a socket map
    and socket hash and use BPF iterators with key prefix bucketing and
    filtering to destroy the set of sockets connected to the same remote
    port regardless of protocol. This test wraps things up by demonstrating
    the desired end to end flow and showing how all the pieces are meant to
    fit together.
    
    Signed-off-by: Jordan Rife <jordan@jrife.io>

--- tools/testing/selftests/bpf/progs/test_sockmap_update.c
+++ tools/testing/selftests/bpf/progs/test_sockmap_update.c
@@ -45,4 +45,47 @@ int copy_sock_map(void *ctx)
 	return failed ? SK_DROP : SK_PASS;
 }
 
+__u32 count = 0;
+
+struct sock_hash_key {
+	__u32 bucket_key;
+	__u64 cookie;
+} __attribute__((__packed__));
+
+struct {
+	__uint(type, BPF_MAP_TYPE_SOCKHASH);
+	__uint(max_entries, 16);
+	__ulong(map_extra, offsetof(struct sock_hash_key, cookie));
+	__type(key, struct sock_hash_key);
+	__type(value, __u64);
+} sock_hash SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_SOCKMAP);
+	__uint(max_entries, 16);
+	__type(key, __u32);
+	__type(value, __u64);
+} sock_map SEC(".maps");
+
+SEC("sockops")
+int insert_sock(struct bpf_sock_ops *skops)
+{
+	struct sock_hash_key key = {
+		.bucket_key = skops->remote_port,
+		.cookie     = bpf_get_socket_cookie(skops),
+	};
+
+	switch (skops->op) {
+	case BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB:
+		bpf_sock_hash_update(skops, &sock_hash, &key, BPF_NOEXIST);
+		bpf_sock_map_update(skops, &sock_map, &count, BPF_NOEXIST);
+		count++;
+		break;
+	default:
+		break;
+	}
+
+	return 0;
+}
+
 char _license[] SEC("license") = "GPL";
