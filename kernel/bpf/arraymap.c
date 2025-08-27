8cb4be322d351d5f5b76c0e1c013e046280340df
Leon Hwang <leon.hwang@linux.dev>
2025-08-28 00:45:05 +0800

    bpf: Introduce BPF_F_CPU and BPF_F_ALL_CPUS flags for percpu_array maps
    Introduce support for the BPF_F_ALL_CPUS flag in percpu_array maps to
    allow updating values for all CPUs with a single value for both
    update_elem and update_batch APIs.
    
    Introduce support for the BPF_F_CPU flag in percpu_array maps to allow:
    
    * update value for specified CPU for both update_elem and update_batch
      APIs.
    * lookup value for specified CPU for both lookup_elem and lookup_batch
      APIs.
    
    The BPF_F_CPU flag is passed via:
    
    * map_flags of lookup_elem and update_elem APIs along with embedded cpu
      info.
    * elem_flags of lookup_batch and update_batch APIs along with embedded
      cpu info.
    
    Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

--- kernel/bpf/arraymap.c
+++ kernel/bpf/arraymap.c
@@ -295,16 +295,20 @@ static void *percpu_array_map_lookup_percpu_elem(struct bpf_map *map, void *key,
 	return per_cpu_ptr(array->pptrs[index & array->index_mask], cpu);
 }
 
-int bpf_percpu_array_copy(struct bpf_map *map, void *key, void *value)
+int bpf_percpu_array_copy(struct bpf_map *map, void *key, void *value, u64 flags)
 {
 	struct bpf_array *array = container_of(map, struct bpf_array, map);
 	u32 index = *(u32 *)key;
 	void __percpu *pptr;
-	int cpu, off = 0;
 	u32 size;
+	int err;
 
 	if (unlikely(index >= array->map.max_entries))
 		return -ENOENT;
+
+	err = bpf_map_check_cpu_flags(flags, false);
+	if (unlikely(err))
+		return err;
 
 	/* per_cpu areas are zero-filled and bpf programs can only
 	 * access 'value_size' of them, so copying rounded areas
@@ -313,11 +317,7 @@ int bpf_percpu_array_copy(struct bpf_map *map, void *key, void *value)
 	size = array->elem_size;
 	rcu_read_lock();
 	pptr = array->pptrs[index & array->index_mask];
-	for_each_possible_cpu(cpu) {
-		copy_map_value_long(map, value + off, per_cpu_ptr(pptr, cpu));
-		check_and_init_map_value(map, value + off);
-		off += size;
-	}
+	bpf_percpu_copy_to_user(map, pptr, value, size, flags);
 	rcu_read_unlock();
 	return 0;
 }
@@ -387,12 +387,12 @@ int bpf_percpu_array_update(struct bpf_map *map, void *key, void *value,
 	struct bpf_array *array = container_of(map, struct bpf_array, map);
 	u32 index = *(u32 *)key;
 	void __percpu *pptr;
-	int cpu, off = 0;
 	u32 size;
+	int err;
 
-	if (unlikely(map_flags > BPF_EXIST))
-		/* unknown flags */
-		return -EINVAL;
+	err = bpf_map_check_cpu_flags(map_flags, true);
+	if (unlikely(err))
+		return err;
 
 	if (unlikely(index >= array->map.max_entries))
 		/* all elements were pre-allocated, cannot insert a new one */
@@ -411,11 +411,7 @@ int bpf_percpu_array_update(struct bpf_map *map, void *key, void *value,
 	size = array->elem_size;
 	rcu_read_lock();
 	pptr = array->pptrs[index & array->index_mask];
-	for_each_possible_cpu(cpu) {
-		copy_map_value_long(map, per_cpu_ptr(pptr, cpu), value + off);
-		bpf_obj_free_fields(array->map.record, per_cpu_ptr(pptr, cpu));
-		off += size;
-	}
+	bpf_percpu_copy_from_user(map, pptr, value, size, map_flags);
 	rcu_read_unlock();
 	return 0;
 }
