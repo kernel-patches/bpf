d739a436d46b7ecdc1fb04db143ed62cd3effcbb
Yafang Shao <laoar.shao@gmail.com>
2025-09-08 02:13:59 +0000

    mm: thp: enable THP allocation exclusively through khugepaged
    Currently, THP allocation cannot be restricted to khugepaged alone while
    being disabled in the page fault path. This limitation exists because
    disabling THP allocation during page faults also prevents the execution of
    khugepaged_enter_vma() in that path.
    
    With the introduction of BPF, we can now implement THP policies based on
    different TVA types. This patch adjusts the logic to support this new
    capability.
    
    While we could also extend prtcl() to utilize this new policy, such a
    change would require a uAPI modification.
    
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -1325,7 +1325,6 @@ vm_fault_t do_huge_pmd_anonymous_page(struct vm_fault *vmf)
 	ret = vmf_anon_prepare(vmf);
 	if (ret)
 		return ret;
-	khugepaged_enter_vma(vma, vma->vm_flags);
 
 	if (!(vmf->flags & FAULT_FLAG_WRITE) &&
 			!mm_forbids_zeropage(vma->vm_mm) &&
