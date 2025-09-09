7896868ee1bfaf746ce7812ab7b2ae75475307ab
Leon Hwang <leon.hwang@linux.dev>
2025-09-09 13:59:34 +0800

    bpf: Add BPF_F_CPU and BPF_F_ALL_CPUS flags support for percpu_hash and lru_percpu_hash maps
    Introduce BPF_F_ALL_CPUS flag support for percpu_hash and lru_percpu_hash
    maps to allow updating values for all CPUs with a single value for both
    update_elem and update_batch APIs.
    
    Introduce BPF_F_CPU flag support for percpu_hash and lru_percpu_hash
    maps to allow:
    
    * update value for specified CPU for both update_elem and update_batch
    APIs.
    * lookup value for specified CPU for both lookup_elem and lookup_batch
    APIs.
    
    The BPF_F_CPU flag is passed via:
    
    * map_flags along with embedded cpu info.
    * elem_flags along with embedded cpu info.
    
    Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -135,6 +135,8 @@ static bool bpf_map_supports_cpu_flags(enum bpf_map_type map_type)
 {
 	switch (map_type) {
 	case BPF_MAP_TYPE_PERCPU_ARRAY:
+	case BPF_MAP_TYPE_PERCPU_HASH:
+	case BPF_MAP_TYPE_LRU_PERCPU_HASH:
 		return true;
 	default:
 		return false;
@@ -341,7 +343,7 @@ static int bpf_map_copy_value(struct bpf_map *map, void *key, void *value,
 	bpf_disable_instrumentation();
 	if (map->map_type == BPF_MAP_TYPE_PERCPU_HASH ||
 	    map->map_type == BPF_MAP_TYPE_LRU_PERCPU_HASH) {
-		err = bpf_percpu_hash_copy(map, key, value);
+		err = bpf_percpu_hash_copy(map, key, value, flags);
 	} else if (map->map_type == BPF_MAP_TYPE_PERCPU_ARRAY) {
 		err = bpf_percpu_array_copy(map, key, value, flags);
 	} else if (map->map_type == BPF_MAP_TYPE_PERCPU_CGROUP_STORAGE) {
