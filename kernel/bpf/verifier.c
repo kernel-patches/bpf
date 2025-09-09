166fb3bf06ae859abf33a2ea7269d254d311a6c5
Kumar Kartikeya Dwivedi <memxor@gmail.com>
2025-09-09 20:33:54 +0000

    bpf: Enforce RCU protection for KF_RCU_PROTECTED
    Currently, KF_RCU_PROTECTED only applies to iterator APIs and that too
    in a convoluted fashion: the presence of this flag on the kfunc is used
    to set MEM_RCU in iterator type, and the lack of RCU protection results
    in an error only later, once next() or destroy() methods are invoked on
    the iterator. While there is no bug, this is certainly a bit
    unintuitive, and makes the enforcement of the flag iterator specific.
    
    In the interest of making this flag useful for other upcoming kfuncs,
    e.g. scx_bpf_cpu_curr() [0][1], add enforcement for invoking the kfunc
    in an RCU critical section in general.
    
    This would also mean that iterator APIs using KF_RCU_PROTECTED will
    error out earlier, instead of throwing an error for lack of RCU CS
    protection when next() or destroy() methods are invoked.
    
      [0]: https://lore.kernel.org/all/20250903212311.369697-3-christian.loehle@arm.com
      [1]: https://lore.kernel.org/all/20250909195709.92669-1-arighi@nvidia.com
    
    Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -13916,6 +13916,11 @@ static int check_kfunc_call(struct bpf_verifier_env *env, struct bpf_insn *insn,
 		return -EACCES;
 	}
 
+	if (is_kfunc_rcu_protected(&meta) && !in_rcu_cs(env)) {
+		verbose(env, "kernel func %s requires RCU critical section protection\n", func_name);
+		return -EACCES;
+	}
+
 	/* In case of release function, we get register number of refcounted
 	 * PTR_TO_BTF_ID in bpf_kfunc_arg_meta, do the release now.
 	 */
