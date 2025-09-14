59d419babb244877f2f927325c01e6bdbe639a07
Yafang Shao <laoar.shao@gmail.com>
2025-09-14 12:46:46 +0000

    mm: thp: remove vm_flags parameter from khugepaged_enter_vma()
    The khugepaged_enter_vma() function requires handling in two specific
    scenarios:
    1. New VMA creation
      When a new VMA is created (for anon vma, it is deferred to pagefault), if
      vma->vm_mm is not present in khugepaged_mm_slot, it must be added. In
      this case, khugepaged_enter_vma() is called after vma->vm_flags have been
      set, allowing direct use of the VMA's flags.
    2. VMA flag modification
      When vma->vm_flags are modified (particularly when VM_HUGEPAGE is set),
      the system must recheck whether to add vma->vm_mm to khugepaged_mm_slot.
      Currently, khugepaged_enter_vma() is called before the flag update, so
      the call must be relocated to occur after vma->vm_flags have been set.
    
    In the VMA merging path, khugepaged_enter_vma() is also called. For this
    case, since VMA merging only occurs when the vm_flags of both VMAs are
    identical (excluding special flags like VM_SOFTDIRTY), we can safely use
    target->vm_flags instead. (It is worth noting that khugepaged_enter_vma()
    can be removed from the VMA merging path because the VMA has already been
    added in the two aforementioned cases. We will address this cleanup in a
    separate patch.)
    
    After this change, we can further remove vm_flags parameter from
    thp_vma_allowable_order(). That will be handled in a followup patch.
    
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>
    Cc: Yang Shi <shy828301@gmail.com>
    Cc: Usama Arif <usamaarif642@gmail.com>

--- include/linux/khugepaged.h
+++ include/linux/khugepaged.h
@@ -13,8 +13,8 @@ extern void khugepaged_destroy(void);
 extern int start_stop_khugepaged(void);
 extern void __khugepaged_enter(struct mm_struct *mm);
 extern void __khugepaged_exit(struct mm_struct *mm);
-extern void khugepaged_enter_vma(struct vm_area_struct *vma,
-				 vm_flags_t vm_flags);
+extern void khugepaged_enter_vma(struct vm_area_struct *vma);
+extern void khugepaged_enter_mm(struct mm_struct *mm);
 extern void khugepaged_min_free_kbytes_update(void);
 extern bool current_is_khugepaged(void);
 extern int collapse_pte_mapped_thp(struct mm_struct *mm, unsigned long addr,
@@ -38,8 +38,10 @@ static inline void khugepaged_fork(struct mm_struct *mm, struct mm_struct *oldmm
 static inline void khugepaged_exit(struct mm_struct *mm)
 {
 }
-static inline void khugepaged_enter_vma(struct vm_area_struct *vma,
-					vm_flags_t vm_flags)
+static inline void khugepaged_enter_vma(struct vm_area_struct *vma)
+{
+}
+static inline void khugepaged_enter_mm(struct mm_struct *mm)
 {
 }
 static inline int collapse_pte_mapped_thp(struct mm_struct *mm,
