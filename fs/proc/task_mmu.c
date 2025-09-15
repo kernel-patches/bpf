716b061075c3a2453995e2349193f4adc6ca9971
Yafang Shao <laoar.shao@gmail.com>
2025-09-15 02:37:50 +0000

    mm: thp: remove vm_flags parameter from thp_vma_allowable_order()
    Because all calls to thp_vma_allowable_order() pass vma->vm_flags as the
    vma_flags argument, we can remove the parameter and have the function
    access vma->vm_flags directly.
    
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>
    Acked-by: Usama Arif <usamaarif642@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1369,8 +1369,7 @@ static int show_smap(struct seq_file *m, void *v)
 	__show_smap(m, &mss, false);
 
 	seq_printf(m, "THPeligible:    %8u\n",
-		   !!thp_vma_allowable_orders(vma, vma->vm_flags, TVA_SMAPS,
-					      THP_ORDERS_ALL));
+		   !!thp_vma_allowable_orders(vma, TVA_SMAPS, THP_ORDERS_ALL));
 
 	if (arch_pkeys_enabled())
 		seq_printf(m, "ProtectionKey:  %8u\n", vma_pkey(vma));
