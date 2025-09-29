5287890610e5f4ab42e54af36fa1aa1990e911d5
Sidharth Seela <sidharthseela@gmail.com>
2025-09-29 17:13:57 +0530

    selftest:net: Fix uninit pointers and return values
    Fix uninitialized character pointers, and functions that return
    undefined values. These issues were caught by running clang using LLVM=1
    option; and are as follows:
    
    Signed-off-by: NipaLocal <nipa@local>

--- tools/testing/selftests/net/ovpn/ovpn-cli.c
+++ tools/testing/selftests/net/ovpn/ovpn-cli.c
@@ -1581,7 +1581,7 @@ static int ovpn_listen_mcast(void)
 {
 	struct nl_sock *sock;
 	struct nl_cb *cb;
-	int mcid, ret;
+	int mcid, ret = -1;
 
 	sock = nl_socket_alloc();
 	if (!sock) {
@@ -1936,7 +1936,7 @@ static int ovpn_run_cmd(struct ovpn_ctx *ovpn)
 {
 	char peer_id[10], vpnip[INET6_ADDRSTRLEN], laddr[128], lport[10];
 	char raddr[128], rport[10];
-	int n, ret;
+	int n, ret = -1;
 	FILE *fp;
 
 	switch (ovpn->cmd) {
