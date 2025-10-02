20636f3d418ff64e40efe58474132dea6f52e901
Amery Hung <ameryhung@gmail.com>
2025-10-02 15:53:42 -0700

    bpf: Convert bpf_selem_link_map to failable
    To prepare for changing bpf_local_storage_map_bucket::lock to rqspinlock,
    convert bpf_selem_link_map() to failable. It still always succeeds and
    returns 0 until the change happens. No functional change.
    
    __must_check is added to the function declaration locally to make sure
    all the callers are accounted for during the conversion.
    
    Signed-off-by: Amery Hung <ameryhung@gmail.com>

--- kernel/bpf/bpf_local_storage.c
+++ kernel/bpf/bpf_local_storage.c
@@ -438,8 +438,8 @@ static void bpf_selem_unlink_map_nolock(struct bpf_local_storage_elem *selem)
 		hlist_del_init_rcu(&selem->map_node);
 }
 
-void bpf_selem_link_map(struct bpf_local_storage_map *smap,
-			struct bpf_local_storage_elem *selem)
+int bpf_selem_link_map(struct bpf_local_storage_map *smap,
+		       struct bpf_local_storage_elem *selem)
 {
 	struct bpf_local_storage *local_storage;
 	struct bpf_local_storage_map_bucket *b;
@@ -452,6 +452,8 @@ void bpf_selem_link_map(struct bpf_local_storage_map *smap,
 	RCU_INIT_POINTER(SDATA(selem)->smap, smap);
 	hlist_add_head_rcu(&selem->map_node, &b->list);
 	raw_spin_unlock_irqrestore(&b->lock, flags);
+
+	return 0;
 }
 
 static void bpf_selem_link_map_nolock(struct bpf_local_storage_map *smap,
