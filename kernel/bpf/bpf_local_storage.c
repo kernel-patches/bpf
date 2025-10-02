3357cfc21cb1ba3fed928a584e360c407cd9355d
Amery Hung <ameryhung@gmail.com>
2025-10-02 15:53:40 -0700

    bpf: Select bpf_local_storage_map_bucket based on bpf_local_storage
    A later bpf_local_storage refactor will acquire all locks before
    performing any update. To simplified the number of locks needed to take
    in bpf_local_storage_map_update(), determine the bucket based on the
    local_storage an selem belongs to instead of the selem pointer.
    
    Currently, when a new selem needs to be created to replace the old selem
    in bpf_local_storage_map_update(), locks of both buckets need to be
    acquired to prevent racing. This can be simplified if the two selem
    belongs to the same bucket so that only one bucket needs to be locked.
    Therefore, instead of hashing selem, hashing the local_storage pointer
    the selem belongs.
    
    This is safe since a selem is always linked to local_storage before
    linked to map and unlinked from local_storage after unlinked from map.
    Performance wise, this is slightly better as update now requires locking
    one bucket. It should not change the level of contention on one bucket
    as the pointers to local storages of selems in a map are just as unique
    as pointers to selems.
    
    Signed-off-by: Amery Hung <ameryhung@gmail.com>

--- kernel/bpf/bpf_local_storage.c
+++ kernel/bpf/bpf_local_storage.c
@@ -19,9 +19,9 @@
 
 static struct bpf_local_storage_map_bucket *
 select_bucket(struct bpf_local_storage_map *smap,
-	      struct bpf_local_storage_elem *selem)
+	      struct bpf_local_storage *local_storage)
 {
-	return &smap->buckets[hash_ptr(selem, smap->bucket_log)];
+	return &smap->buckets[hash_ptr(local_storage, smap->bucket_log)];
 }
 
 static int mem_charge(struct bpf_local_storage_map *smap, void *owner, u32 size)
@@ -411,6 +411,7 @@ void bpf_selem_link_storage_nolock(struct bpf_local_storage *local_storage,
 
 static void bpf_selem_unlink_map(struct bpf_local_storage_elem *selem)
 {
+	struct bpf_local_storage *local_storage;
 	struct bpf_local_storage_map *smap;
 	struct bpf_local_storage_map_bucket *b;
 	unsigned long flags;
@@ -419,8 +420,10 @@ static void bpf_selem_unlink_map(struct bpf_local_storage_elem *selem)
 		/* selem has already be unlinked from smap */
 		return;
 
+	local_storage = rcu_dereference_check(selem->local_storage,
+					      bpf_rcu_lock_held());
 	smap = rcu_dereference_check(SDATA(selem)->smap, bpf_rcu_lock_held());
-	b = select_bucket(smap, selem);
+	b = select_bucket(smap, local_storage);
 	raw_spin_lock_irqsave(&b->lock, flags);
 	if (likely(selem_linked_to_map(selem)))
 		hlist_del_init_rcu(&selem->map_node);
@@ -430,9 +433,13 @@ static void bpf_selem_unlink_map(struct bpf_local_storage_elem *selem)
 void bpf_selem_link_map(struct bpf_local_storage_map *smap,
 			struct bpf_local_storage_elem *selem)
 {
-	struct bpf_local_storage_map_bucket *b = select_bucket(smap, selem);
+	struct bpf_local_storage *local_storage;
+	struct bpf_local_storage_map_bucket *b;
 	unsigned long flags;
 
+	local_storage = rcu_dereference_check(selem->local_storage,
+					      bpf_rcu_lock_held());
+	b = select_bucket(smap, local_storage);
 	raw_spin_lock_irqsave(&b->lock, flags);
 	RCU_INIT_POINTER(SDATA(selem)->smap, smap);
 	hlist_add_head_rcu(&selem->map_node, &b->list);
