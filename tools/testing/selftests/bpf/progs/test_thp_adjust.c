b033a875e712abf9f8ab95bc47cab77fe0cf00da
Yafang Shao <laoar.shao@gmail.com>
2025-10-15 22:17:15 +0800

    selftests/bpf: add a simple BPF based THP policy
    This test case implements a basic THP policy that sets THPeligible to 1 for
    a specific task and to 0 for all others. I selected THPeligible for
    verification because its straightforward nature makes it ideal for
    validating the BPF THP policy functionality.
    
    Below configs must be enabled for this test:
    
      CONFIG_BPF_THP=y
      CONFIG_MEMCG=y
      CONFIG_TRANSPARENT_HUGEPAGE=y
    
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>

---
+++ tools/testing/selftests/bpf/progs/test_thp_adjust.c
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+char _license[] SEC("license") = "GPL";
+
+int pmd_order;
+
+SEC("struct_ops/thp_get_order")
+int BPF_PROG(thp_eligible, struct vm_area_struct *vma, enum tva_type type,
+	     unsigned long orders)
+{
+	if (type != TVA_SMAPS)
+		return 0;
+	return pmd_order;
+}
+
+SEC(".struct_ops.link")
+struct bpf_thp_ops thp_eligible_ops = {
+	.thp_get_order = (void *)thp_eligible,
+};
