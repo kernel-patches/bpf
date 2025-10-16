b19963c65562b68280e296ebb5117839499a6574
Leon Hwang <leon.hwang@linux.dev>
2025-10-16 22:58:01 +0800

    selftests/bpf: Add test to verify no memleak when updating hash maps
    Add two tests to verify that updating hash maps does not leak memory
    when BPF_KPTR_REF objects are involved.
    
    The test performs the following steps:
    
    1. Call update_elem() to insert an initial value.
    2. Use bpf_refcount_acquire() to increment the refcount.
    3. Store the node pointer in the map value.
    4. Add the node to a linked list.
    5. Probe-read the refcount and verify it is *2*.
    6. Call update_elem() again to trigger refcount decrement.
    7. Verify that the field has been reset.
    
    Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

--- tools/testing/selftests/bpf/prog_tests/refcounted_kptr.c
+++ tools/testing/selftests/bpf/prog_tests/refcounted_kptr.c
@@ -44,3 +44,96 @@ void test_refcounted_kptr_wrong_owner(void)
 	ASSERT_OK(opts.retval, "rbtree_wrong_owner_remove_fail_a2 retval");
 	refcounted_kptr__destroy(skel);
 }
+
+static void test_refcnt_leak(void *values, size_t values_sz, u64 flags, bool lock_hash)
+{
+	struct refcounted_kptr *skel;
+	int ret, fd, key = 0;
+	struct bpf_map *map;
+	LIBBPF_OPTS(bpf_test_run_opts, opts,
+		    .data_in = &pkt_v4,
+		    .data_size_in = sizeof(pkt_v4),
+		    .repeat = 1,
+	);
+
+	skel = refcounted_kptr__open_and_load();
+	if (!ASSERT_OK_PTR(skel, "refcounted_kptr__open_and_load"))
+		return;
+
+	map = skel->maps.pcpu_hash;
+	if (lock_hash)
+		map = skel->maps.lock_hash;
+
+	ret = bpf_map__update_elem(map, &key, sizeof(key), values, values_sz, flags);
+	if (!ASSERT_OK(ret, "bpf_map__update_elem first"))
+		goto out;
+
+	fd = bpf_program__fd(skel->progs.pcpu_hash_refcount_leak);
+	if (lock_hash)
+		fd = bpf_program__fd(skel->progs.hash_lock_refcount_leak);
+
+	ret = bpf_prog_test_run_opts(fd, &opts);
+	if (!ASSERT_OK(ret, "test_run_opts"))
+		goto out;
+	if (!ASSERT_EQ(opts.retval, 2, "retval refcount"))
+		goto out;
+
+	ret = bpf_map__update_elem(map, &key, sizeof(key), values, values_sz, flags);
+	if (!ASSERT_OK(ret, "bpf_map__update_elem second"))
+		goto out;
+
+	fd = bpf_program__fd(skel->progs.check_pcpu_hash_refcount);
+	if (lock_hash)
+		fd = bpf_program__fd(skel->progs.check_hash_lock_refcount);
+
+	ret = bpf_prog_test_run_opts(fd, &opts);
+	if (!ASSERT_OK(ret, "test_run_opts"))
+		goto out;
+	if (!ASSERT_EQ(opts.retval, 1, "retval"))
+		goto out;
+
+out:
+	refcounted_kptr__destroy(skel);
+}
+
+static void test_percpu_hash_refcount_leak(void)
+{
+	size_t values_sz;
+	u64 *values;
+	int cpu_nr;
+
+	cpu_nr = libbpf_num_possible_cpus();
+	if (!ASSERT_GT(cpu_nr, 0, "libbpf_num_possible_cpus"))
+		return;
+
+	values = calloc(cpu_nr, sizeof(u64));
+	if (!ASSERT_OK_PTR(values, "calloc values"))
+		return;
+
+	values_sz = cpu_nr * sizeof(u64);
+	memset(values, 0, values_sz);
+
+	test_refcnt_leak(values, values_sz, 0, false);
+
+	free(values);
+}
+
+struct hash_lock_value {
+	struct bpf_spin_lock lock;
+	u64 node;
+};
+
+static void test_hash_lock_refcount_leak(void)
+{
+	struct hash_lock_value value = {};
+
+	test_refcnt_leak(&value, sizeof(value), BPF_F_LOCK, true);
+}
+
+void test_refcount_leak(void)
+{
+	if (test__start_subtest("percpu_hash_refcount_leak"))
+		test_percpu_hash_refcount_leak();
+	if (test__start_subtest("hash_lock_refcount_leak"))
+		test_hash_lock_refcount_leak();
+}
