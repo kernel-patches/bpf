f7c56b8aa5c0b8340dc4e7ee157bb034f1a60fdd
Menglong Dong <dongml2@chinatelecom.cn>
2025-10-18 20:49:15 +0800

    bpf: add two kfunc for TRACE_SESSION
    If TRACE_SESSION exists, we will use extra 8-bytes in the stack of the
    trampoline to store the flags that we needed, and the 8-bytes lie after
    the return value, which means ctx[nr_args + 1]. And we will store the
    flag "is_exit" to the first bit of it.
    
    Introduce the kfunc bpf_tracing_is_exit(), which is used to tell if it
    is fexit currently. Meanwhile, inline it in the verifier.
    
    Add the kfunc bpf_fsession_cookie(), which is similar to
    bpf_session_cookie() and return the address of the session cookie. The
    address of the session cookie is stored after session flags, which means
    ctx[nr_args + 2]. Inline this kfunc in the verifier too.
    
    Signed-off-by: Menglong Dong <dongml2@chinatelecom.cn>
    Co-developed-by: Leon Hwang <leon.hwang@linux.dev>
    Signed-off-by: Leon Hwang <leon.hwang@linux.dev>
    ---
    v3:
    - merge the bpf_tracing_is_exit and bpf_fsession_cookie into a single
      patch
    
    v2:
    - store the session flags after return value, instead of before nr_args
    - inline the bpf_tracing_is_exit, as Jiri suggested

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -3356,12 +3356,65 @@ static const struct btf_kfunc_id_set bpf_kprobe_multi_kfunc_set = {
 	.filter = bpf_kprobe_multi_filter,
 };
 
-static int __init bpf_kprobe_multi_kfuncs_init(void)
+__bpf_kfunc_start_defs();
+
+__bpf_kfunc bool bpf_tracing_is_exit(void *ctx)
 {
-	return register_btf_kfunc_id_set(BPF_PROG_TYPE_KPROBE, &bpf_kprobe_multi_kfunc_set);
+	/* This helper call is inlined by verifier. */
+	u64 nr_args = ((u64 *)ctx)[-1];
+
+	/*
+	 * ctx[nr_args + 1] is the session flags, and the last bit is
+	 * is_exit.
+	 */
+	return ((u64 *)ctx)[nr_args + 1] & 1;
 }
 
-late_initcall(bpf_kprobe_multi_kfuncs_init);
+__bpf_kfunc u64 *bpf_fsession_cookie(void *ctx)
+{
+	/* This helper call is inlined by verifier. */
+	u64 nr_args = ((u64 *)ctx)[-1];
+
+	/* ctx[nr_args + 2] is the session cookie address */
+	return (u64 *)((u64 *)ctx)[nr_args + 2];
+}
+
+__bpf_kfunc_end_defs();
+
+BTF_KFUNCS_START(tracing_kfunc_set_ids)
+BTF_ID_FLAGS(func, bpf_tracing_is_exit, KF_FASTCALL)
+BTF_ID_FLAGS(func, bpf_fsession_cookie, KF_FASTCALL)
+BTF_KFUNCS_END(tracing_kfunc_set_ids)
+
+static int bpf_tracing_filter(const struct bpf_prog *prog, u32 kfunc_id)
+{
+	if (!btf_id_set8_contains(&tracing_kfunc_set_ids, kfunc_id))
+		return 0;
+
+	if (prog->type != BPF_PROG_TYPE_TRACING ||
+	    prog->expected_attach_type != BPF_TRACE_SESSION)
+		return -EINVAL;
+
+	return 0;
+}
+
+static const struct btf_kfunc_id_set bpf_tracing_kfunc_set = {
+	.owner = THIS_MODULE,
+	.set = &tracing_kfunc_set_ids,
+	.filter = bpf_tracing_filter,
+};
+
+static int __init bpf_trace_kfuncs_init(void)
+{
+	int err = 0;
+
+	err = err ?: register_btf_kfunc_id_set(BPF_PROG_TYPE_KPROBE, &bpf_kprobe_multi_kfunc_set);
+	err = err ?: register_btf_kfunc_id_set(BPF_PROG_TYPE_TRACING, &bpf_tracing_kfunc_set);
+
+	return err;
+}
+
+late_initcall(bpf_trace_kfuncs_init);
 
 typedef int (*copy_fn_t)(void *dst, const void *src, u32 size, struct task_struct *tsk);
 
