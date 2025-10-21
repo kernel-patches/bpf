33556957d17c86ff596cd502fd737b70f63b677f
Song Liu <song@kernel.org>
2025-10-21 11:02:16 -0700

    ftrace: Fix BPF fexit with livepatch
    When livepatch is attached to the same function as bpf trampoline with
    a fexit program, bpf trampoline code calls register_ftrace_direct()
    twice. The first time will fail with -EAGAIN, and the second time it
    will succeed. This requires register_ftrace_direct() to unregister
    the address on the first attempt. Otherwise, the bpf trampoline cannot
    attach. Here is an easy way to reproduce this issue:
    
      insmod samples/livepatch/livepatch-sample.ko
      bpftrace -e 'fexit:cmdline_proc_show {}'
      ERROR: Unable to attach probe: fexit:vmlinux:cmdline_proc_show...
    
    Fix this by cleaning up the hash when register_ftrace_function_nolock hits
    errors.
    
    Fixes: d05cb470663a ("ftrace: Fix modification of direct_function hash while in use")
    Cc: stable@vger.kernel.org # v6.6+
    Reported-by: Andrey Grodzovsky <andrey.grodzovsky@crowdstrike.com>
    Closes: https://lore.kernel.org/live-patching/c5058315a39d4615b333e485893345be@crowdstrike.com/
    Cc: Steven Rostedt (Google) <rostedt@goodmis.org>
    Cc: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    Acked-and-tested-by: Andrey Grodzovsky <andrey.grodzovsky@crowdstrike.com>
    Signed-off-by: Song Liu <song@kernel.org>

--- kernel/trace/ftrace.c
+++ kernel/trace/ftrace.c
@@ -6048,6 +6048,8 @@ int register_ftrace_direct(struct ftrace_ops *ops, unsigned long addr)
 	ops->direct_call = addr;
 
 	err = register_ftrace_function_nolock(ops);
+	if (err)
+		remove_direct_functions_hash(hash, addr);
 
  out_unlock:
 	mutex_unlock(&direct_mutex);
