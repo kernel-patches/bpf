2d09e4165482ec75a55f2a381d5d9de4756c6712
Amery Hung <ameryhung@gmail.com>
2025-10-24 14:29:10 -0700

    bpf: Support associating BPF program with struct_ops
    Add a new BPF command BPF_PROG_ASSOC_STRUCT_OPS to allow associating
    a BPF program with a struct_ops map. This command takes a file
    descriptor of a struct_ops map and a BPF program and set
    prog->aux->st_ops_assoc to the kdata of the struct_ops map.
    
    The command does not accept a struct_ops program nor a non-struct_ops
    map. Programs of a struct_ops map is automatically associated with the
    map during map update. If a program is shared between two struct_ops
    maps, prog->aux->st_ops_assoc will be poisoned to indicate that the
    associated struct_ops is ambiguous. The pointer, once poisoned, cannot
    be reset since we have lost track of associated struct_ops. For other
    program types, the associated struct_ops map, once set, cannot be
    changed later. This restriction may be lifted in the future if there is
    a use case.
    
    A kernel helper bpf_prog_get_assoc_struct_ops() can be used to retrieve
    the associated struct_ops pointer. The returned pointer, if not NULL, is
    guaranteed to be valid and point to a fully updated struct_ops struct.
    For struct_ops program reused in multiple struct_ops map, the return
    will be NULL. The call must be paired with bpf_struct_ops_put() once the
    caller is done with the struct_ops.
    
    To make sure the returned pointer to be valid, the command increases the
    refcount of the map for every associated non-struct_ops programs. For
    struct_ops programs, since they do not increase the refcount of
    struct_ops map, bpf_prog_get_assoc_struct_ops() has to bump the refcount
    of the map to prevent a map from being freed while the program runs.
    This can happen if a struct_ops program schedules a time callback that
    runs after the struct_ops map is freed.
    
    struct_ops implementers should note that the struct_ops returned may or
    may not be attached. The struct_ops implementer will be responsible for
    tracking and checking the state of the associated struct_ops map if the
    use case requires an attached struct_ops.
    
    Signed-off-by: Amery Hung <ameryhung@gmail.com>

--- kernel/bpf/bpf_struct_ops.c
+++ kernel/bpf/bpf_struct_ops.c
@@ -533,6 +533,17 @@ static void bpf_struct_ops_map_put_progs(struct bpf_struct_ops_map *st_map)
 	}
 }
 
+static void bpf_struct_ops_map_dissoc_progs(struct bpf_struct_ops_map *st_map)
+{
+	u32 i;
+
+	for (i = 0; i < st_map->funcs_cnt; i++) {
+		if (!st_map->links[i])
+			break;
+		bpf_prog_disassoc_struct_ops(st_map->links[i]->prog);
+	}
+}
+
 static void bpf_struct_ops_map_free_image(struct bpf_struct_ops_map *st_map)
 {
 	int i;
@@ -801,6 +812,12 @@ static long bpf_struct_ops_map_update_elem(struct bpf_map *map, void *key,
 			goto reset_unlock;
 		}
 
+		err = bpf_prog_assoc_struct_ops(prog, &st_map->map);
+		if (err) {
+			bpf_prog_put(prog);
+			goto reset_unlock;
+		}
+
 		link = kzalloc(sizeof(*link), GFP_USER);
 		if (!link) {
 			bpf_prog_put(prog);
@@ -979,6 +996,8 @@ static void bpf_struct_ops_map_free(struct bpf_map *map)
 	 */
 	if (btf_is_module(st_map->btf))
 		module_put(st_map->st_ops_desc->st_ops->owner);
+
+	bpf_struct_ops_map_dissoc_progs(st_map);
 
 	bpf_struct_ops_map_del_ksyms(st_map);
 
@@ -1173,6 +1192,7 @@ void bpf_struct_ops_put(const void *kdata)
 
 	bpf_map_put(&st_map->map);
 }
+EXPORT_SYMBOL_GPL(bpf_struct_ops_put);
 
 u32 bpf_struct_ops_id(const void *kdata)
 {
@@ -1393,6 +1413,84 @@ err_out:
 	kfree(link);
 	return err;
 }
+
+int bpf_prog_assoc_struct_ops(struct bpf_prog *prog, struct bpf_map *map)
+{
+	struct bpf_map *st_ops_assoc;
+
+	guard(mutex)(&prog->aux->st_ops_assoc_mutex);
+
+	st_ops_assoc = rcu_access_pointer(prog->aux->st_ops_assoc);
+
+	if (st_ops_assoc && st_ops_assoc == map)
+		return 0;
+
+	if (st_ops_assoc) {
+		if (prog->type != BPF_PROG_TYPE_STRUCT_OPS)
+			return -EBUSY;
+
+		rcu_assign_pointer(prog->aux->st_ops_assoc, BPF_PTR_POISON);
+	} else {
+		if (prog->type != BPF_PROG_TYPE_STRUCT_OPS)
+			bpf_map_inc(map);
+
+		rcu_assign_pointer(prog->aux->st_ops_assoc, map);
+	}
+
+	return 0;
+}
+
+void bpf_prog_disassoc_struct_ops(struct bpf_prog *prog)
+{
+	struct bpf_map *st_ops_assoc;
+
+	guard(mutex)(&prog->aux->st_ops_assoc_mutex);
+
+	st_ops_assoc = rcu_access_pointer(prog->aux->st_ops_assoc);
+
+	if (!st_ops_assoc || st_ops_assoc == BPF_PTR_POISON)
+		return;
+
+	if (prog->type != BPF_PROG_TYPE_STRUCT_OPS)
+		bpf_map_put(st_ops_assoc);
+
+	RCU_INIT_POINTER(prog->aux->st_ops_assoc, NULL);
+}
+
+/*
+ * Get a reference to the struct_ops struct (i.e., kdata) associated with a
+ * program. Must be paired with bpf_struct_ops_put().
+ *
+ * If the returned pointer is not NULL, it must points to a valid and
+ * initialized struct_ops. The struct_ops may or may not be attached.
+ * Kernel struct_ops implementers are responsible for tracking and checking
+ * the state of the struct_ops if the use case requires an attached struct_ops.
+ */
+void *bpf_prog_get_assoc_struct_ops(const struct bpf_prog_aux *aux)
+{
+	struct bpf_struct_ops_map *st_map;
+	struct bpf_map *map;
+
+	scoped_guard(rcu) {
+		map = rcu_dereference(aux->st_ops_assoc);
+		if (!map || map == BPF_PTR_POISON)
+			return NULL;
+
+		map = bpf_map_inc_not_zero(map);
+		if (IS_ERR(map))
+			return NULL;
+	}
+
+	st_map = (struct bpf_struct_ops_map *)map;
+
+	if (smp_load_acquire(&st_map->kvalue.common.state) == BPF_STRUCT_OPS_STATE_INIT) {
+		bpf_map_put(map);
+		return NULL;
+	}
+
+	return &st_map->kvalue.data;
+}
+EXPORT_SYMBOL_GPL(bpf_prog_get_assoc_struct_ops);
 
 void bpf_map_struct_ops_info_fill(struct bpf_map_info *info, struct bpf_map *map)
 {
