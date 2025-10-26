5da4269ad01ab2fa0ce7b7db1b660d2bf1fac3bd
Menglong Dong <menglong8.dong@gmail.com>
2025-10-26 11:01:38 +0800

    bpf: add two kfunc for TRACE_SESSION
    If TRACE_SESSION exists, we will use extra 8-bytes in the stack of the
    trampoline to store the flags that we needed, and the 8-bytes lie after
    the return value, which means ctx[nr_args + 1]. And we will store the
    flag "is_exit" to the first bit of it.
    
    Introduce the kfunc bpf_tracing_is_exit(), which is used to tell if it
    is fexit currently. Meanwhile, inline it in the verifier.
    
    Add the kfunc bpf_fsession_cookie(), which is similar to
    bpf_session_cookie() and return the address of the session cookie. The
    address of the session cookie is stored after session flags, which means
    ctx[nr_args + 2]. Inline this kfunc in the verifier too.
    
    Signed-off-by: Menglong Dong <dongml2@chinatelecom.cn>
    Co-developed-by: Leon Hwang <leon.hwang@linux.dev>
    Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -1744,6 +1744,7 @@ struct bpf_prog {
 				enforce_expected_attach_type:1, /* Enforce expected_attach_type checking at attach time */
 				call_get_stack:1, /* Do we call bpf_get_stack() or bpf_get_stackid() */
 				call_get_func_ip:1, /* Do we call get_func_ip() */
+				call_session_cookie:1, /* Do we call bpf_fsession_cookie() */
 				tstamp_type_access:1, /* Accessed __sk_buff->tstamp_type */
 				sleepable:1;	/* BPF program is sleepable */
 	enum bpf_prog_type	type;		/* Type of BPF program */
