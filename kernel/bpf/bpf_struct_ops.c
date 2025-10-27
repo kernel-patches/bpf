bd5c880ee9d8e9665979a0df26945ad466b14d34
Roman Gushchin <roman.gushchin@linux.dev>
2025-10-27 16:17:04 -0700

    bpf: move bpf_struct_ops_link into bpf.h
    Move struct bpf_struct_ops_link's definition into bpf.h,
    where other custom bpf links definitions are.
    
    It's necessary to access its members from outside of generic
    bpf_struct_ops implementation, which will be done by following
    patches in the series.
    
    Signed-off-by: Roman Gushchin <roman.gushchin@linux.dev>

--- kernel/bpf/bpf_struct_ops.c
+++ kernel/bpf/bpf_struct_ops.c
@@ -55,12 +55,6 @@ struct bpf_struct_ops_map {
 	struct bpf_struct_ops_value kvalue;
 };
 
-struct bpf_struct_ops_link {
-	struct bpf_link link;
-	struct bpf_map __rcu *map;
-	wait_queue_head_t wait_hup;
-};
-
 static DEFINE_MUTEX(update_mutex);
 
 #define VALUE_PREFIX "bpf_struct_ops_"
