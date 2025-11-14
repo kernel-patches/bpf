00ed2cf40b3acd833785bdae836a6887025b5973
Puranjay Mohan <puranjay@kernel.org>
2025-11-14 11:16:59 +0000

    selftests: bpf: test non-sleepable arena allocations
    As arena kfuncs can now be called from non-sleepable contexts, test this
    by adding non-sleepable copies of tests in verifier_arena, this is done
    by using a socket program instead of syscall.
    
    Add a new test case in verifier_arena_large to check that the
    bpf_arena_alloc_pages() works for more than 1024 pages.
    1024 * sizeof(struct page *) is the upper limit of kmalloc_nolock() but
    bpf_arena_alloc_pages() should still succeed because it re-uses this
    array in a loop.
    
    Augment the arena_list selftest to also run in non-sleepable context by
    taking rcu_read_lock.
    
    Signed-off-by: Puranjay Mohan <puranjay@kernel.org>

--- tools/testing/selftests/bpf/progs/verifier_arena_large.c
+++ tools/testing/selftests/bpf/progs/verifier_arena_large.c
@@ -270,5 +270,29 @@ int big_alloc2(void *ctx)
 		return 9;
 	return 0;
 }
+
+SEC("socket")
+__success __retval(0)
+int big_alloc3(void *ctx)
+{
+#if defined(__BPF_FEATURE_ADDR_SPACE_CAST)
+	char __arena *pages;
+	u64 i;
+
+	/* Allocate 2051 pages (more than 1024) at once to test the limit of kmalloc_nolock() */
+	pages = bpf_arena_alloc_pages(&arena, NULL, 2051, NUMA_NO_NODE, 0);
+	if (!pages)
+		return -1;
+
+	bpf_for(i, 0, 2051)
+		        pages[i * PAGE_SIZE] = 123;
+	bpf_for(i, 0, 2051)
+		        if (pages[i * PAGE_SIZE] != 123)
+				return i;
+
+	bpf_arena_free_pages(&arena, pages, 1025);
+#endif
+	return 0;
+}
 #endif
 char _license[] SEC("license") = "GPL";
