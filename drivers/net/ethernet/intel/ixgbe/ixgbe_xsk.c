f8d93953fefe149f0ae166c9adaf061aff06dae8
Jakub Sitnicki <jakub@cloudflare.com>
2025-11-24 17:28:41 +0100

    ixgbe: Call skb_metadata_set when skb->data points at metadata end
    Prepare to track skb metadata location independently of MAC header offset.
    
    Following changes will make skb_metadata_set() record where metadata ends
    relative to skb->head. Hence the helper must be called when skb->data
    already points past the metadata area.
    
    Adjust the driver to pull from skb->data before calling skb_metadata_set().
    
    Signed-off-by: Jakub Sitnicki <jakub@cloudflare.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_xsk.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_xsk.c
@@ -228,8 +228,8 @@ static struct sk_buff *ixgbe_construct_skb_zc(struct ixgbe_ring *rx_ring,
 	       ALIGN(totalsize, sizeof(long)));
 
 	if (metasize) {
-		skb_metadata_set(skb, metasize);
 		__skb_pull(skb, metasize);
+		skb_metadata_set(skb, metasize);
 	}
 
 	return skb;
