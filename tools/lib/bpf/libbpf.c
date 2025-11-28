a80d7624b3c38cf4145f629d8ac1968ee2480f5a
Mikhail Gavrilov <mikhail.v.gavrilov@gmail.com>
2025-11-28 19:59:45 +0500

    tools/lib/bpf: fix -Wdiscarded-qualifiers under C23
    glibc ≥ 2.42 (GCC 15) defaults to -std=gnu23, which promotes
    -Wdiscarded-qualifiers to an error in the default hardening flags
    of Fedora Rawhide, Arch Linux, openSUSE Tumbleweed, Gentoo, etc.
    
    In C23, strstr() and strchr() return "const char *" in most cases,
    making previous implicit casts invalid.
    
    This breaks the build of tools/bpf/resolve_btfids on pristine
    upstream kernel when using GCC 15 + glibc 2.42+.
    
    Fix the three remaining instances with explicit casts.
    
    No functional changes.
    
    Link: https://bugzilla.redhat.com/show_bug.cgi?id=2417601
    Suggested-by: Florian Weimer <fweimer@redhat.com>
    Signed-off-by: Mikhail Gavrilov <mikhail.v.gavrilov@gmail.com>

--- tools/lib/bpf/libbpf.c
+++ tools/lib/bpf/libbpf.c
@@ -8484,7 +8484,7 @@ static int kallsyms_cb(unsigned long long sym_addr, char sym_type,
 	struct bpf_object *obj = ctx;
 	const struct btf_type *t;
 	struct extern_desc *ext;
-	char *res;
+	const char *res;
 
 	res = strstr(sym_name, ".llvm.");
 	if (sym_type == 'd' && res)
@@ -11820,7 +11820,7 @@ static int avail_kallsyms_cb(unsigned long long sym_addr, char sym_type,
 		 */
 		char sym_trim[256], *psym_trim = sym_trim, *sym_sfx;
 
-		if (!(sym_sfx = strstr(sym_name, ".llvm.")))
+		if (!(sym_sfx = (char *)strstr(sym_name, ".llvm.")))  /* needs mutation */
 			return 0;
 
 		/* psym_trim vs sym_trim dance is done to avoid pointer vs array
@@ -12406,7 +12406,7 @@ static int resolve_full_path(const char *file, char *result, size_t result_sz)
 
 			if (s[0] == ':')
 				s++;
-			next_path = strchr(s, ':');
+			next_path = (char *)strchr(s, ':');   /* declared as char * above */
 			seg_len = next_path ? next_path - s : strlen(s);
 			if (!seg_len)
 				continue;
