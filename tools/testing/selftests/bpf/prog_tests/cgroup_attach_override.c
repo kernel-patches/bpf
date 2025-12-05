9b1a37eb875ccb0e69e67b5a39c670bd6b926020
Eduard Zingerman <eddyz87@gmail.com>
2025-12-05 12:25:16 -0800

    selftests/bpf: replace CHECK macro usage in cgroup_attach_override.c
    Signed-off-by: Eduard Zingerman <eddyz87@gmail.com>

--- tools/testing/selftests/bpf/prog_tests/cgroup_attach_override.c
+++ tools/testing/selftests/bpf/prog_tests/cgroup_attach_override.c
@@ -26,118 +26,102 @@ static int prog_load(int verdict)
 void serial_test_cgroup_attach_override(void)
 {
 	int drop_prog = -1, allow_prog = -1, foo = -1, bar = -1;
-	__u32 duration = 0;
 
 	allow_prog = prog_load(1);
-	if (CHECK(allow_prog < 0, "prog_load_allow",
-		  "verifier output:\n%s\n-------\n", bpf_log_buf))
+	if (!ASSERT_OK_FD(allow_prog, "prog_load_allow")) {
+		printf("verifier output:\n%s\n-------\n", bpf_log_buf);
 		goto err;
+	}
 
 	drop_prog = prog_load(0);
-	if (CHECK(drop_prog < 0, "prog_load_drop",
-		  "verifier output:\n%s\n-------\n", bpf_log_buf))
+	if (!ASSERT_OK_FD(drop_prog, "prog_load_drop")) {
+		printf("verifier output:\n%s\n-------\n", bpf_log_buf);
 		goto err;
+	}
 
 	foo = test__join_cgroup(FOO);
-	if (CHECK(foo < 0, "cgroup_join_foo", "cgroup setup failed\n"))
+	if (!ASSERT_OK_FD(foo, "cgroup_join_foo"))
 		goto err;
 
-	if (CHECK(bpf_prog_attach(drop_prog, foo, BPF_CGROUP_INET_EGRESS,
-				  BPF_F_ALLOW_OVERRIDE),
-		  "prog_attach_drop_foo_override",
-		  "attach prog to %s failed, errno=%d\n", FOO, errno))
+	if (!ASSERT_OK(bpf_prog_attach(drop_prog, foo, BPF_CGROUP_INET_EGRESS,
+				       BPF_F_ALLOW_OVERRIDE),
+		       "prog_attach_drop_foo_override"))
 		goto err;
 
-	if (CHECK(!system(PING_CMD), "ping_fail",
-		  "ping unexpectedly succeeded\n"))
+	if (!ASSERT_NEQ(system(PING_CMD), 0, "ping expcted to fail"))
 		goto err;
 
 	bar = test__join_cgroup(BAR);
-	if (CHECK(bar < 0, "cgroup_join_bar", "cgroup setup failed\n"))
+	if (!ASSERT_OK_FD(bar, "cgroup_join_bar"))
 		goto err;
 
-	if (CHECK(!system(PING_CMD), "ping_fail",
-		  "ping unexpectedly succeeded\n"))
+	if (!ASSERT_NEQ(system(PING_CMD), 0, "ping expected to fail"))
 		goto err;
 
-	if (CHECK(bpf_prog_attach(allow_prog, bar, BPF_CGROUP_INET_EGRESS,
-				  BPF_F_ALLOW_OVERRIDE),
-		  "prog_attach_allow_bar_override",
-		  "attach prog to %s failed, errno=%d\n", BAR, errno))
+	if (!ASSERT_OK(bpf_prog_attach(allow_prog, bar, BPF_CGROUP_INET_EGRESS,
+				       BPF_F_ALLOW_OVERRIDE),
+		       "prog_attach_allow_bar_override"))
 		goto err;
 
-	if (CHECK(system(PING_CMD), "ping_ok", "ping failed\n"))
+	if (!ASSERT_OK(system(PING_CMD), "ping_ok"))
 		goto err;
 
-	if (CHECK(bpf_prog_detach(bar, BPF_CGROUP_INET_EGRESS),
-		  "prog_detach_bar",
-		  "detach prog from %s failed, errno=%d\n", BAR, errno))
+	if (!ASSERT_OK(bpf_prog_detach(bar, BPF_CGROUP_INET_EGRESS),
+		       "prog_detach_bar"))
 		goto err;
 
-	if (CHECK(!system(PING_CMD), "ping_fail",
-		  "ping unexpectedly succeeded\n"))
+	if (!ASSERT_NEQ(system(PING_CMD), 0, "ping expcted to fail"))
 		goto err;
 
-	if (CHECK(bpf_prog_attach(allow_prog, bar, BPF_CGROUP_INET_EGRESS,
-				  BPF_F_ALLOW_OVERRIDE),
-		  "prog_attach_allow_bar_override",
-		  "attach prog to %s failed, errno=%d\n", BAR, errno))
+	if (!ASSERT_OK(bpf_prog_attach(allow_prog, bar, BPF_CGROUP_INET_EGRESS,
+				       BPF_F_ALLOW_OVERRIDE),
+		       "prog_attach_allow_bar_override"))
 		goto err;
 
-	if (CHECK(bpf_prog_detach(foo, BPF_CGROUP_INET_EGRESS),
-		  "prog_detach_foo",
-		  "detach prog from %s failed, errno=%d\n", FOO, errno))
+	if (!ASSERT_OK(bpf_prog_detach(foo, BPF_CGROUP_INET_EGRESS),
+		       "prog_detach_foo"))
 		goto err;
 
-	if (CHECK(system(PING_CMD), "ping_ok", "ping failed\n"))
+	if (!ASSERT_OK(system(PING_CMD), "ping_ok"))
 		goto err;
 
-	if (CHECK(bpf_prog_attach(allow_prog, bar, BPF_CGROUP_INET_EGRESS,
-				  BPF_F_ALLOW_OVERRIDE),
-		  "prog_attach_allow_bar_override",
-		  "attach prog to %s failed, errno=%d\n", BAR, errno))
+	if (!ASSERT_OK(bpf_prog_attach(allow_prog, bar, BPF_CGROUP_INET_EGRESS,
+				       BPF_F_ALLOW_OVERRIDE),
+		       "prog_attach_allow_bar_override"))
 		goto err;
 
-	if (CHECK(!bpf_prog_attach(allow_prog, bar, BPF_CGROUP_INET_EGRESS, 0),
-		  "fail_prog_attach_allow_bar_none",
-		  "attach prog to %s unexpectedly succeeded\n", BAR))
+	if (!ASSERT_ERR(bpf_prog_attach(allow_prog, bar, BPF_CGROUP_INET_EGRESS, 0),
+			"fail_prog_attach_allow_bar_none"))
 		goto err;
 
-	if (CHECK(bpf_prog_detach(bar, BPF_CGROUP_INET_EGRESS),
-		  "prog_detach_bar",
-		  "detach prog from %s failed, errno=%d\n", BAR, errno))
+	if (!ASSERT_OK(bpf_prog_detach(bar, BPF_CGROUP_INET_EGRESS),
+		       "prog_detach_bar"))
 		goto err;
 
-	if (CHECK(!bpf_prog_detach(foo, BPF_CGROUP_INET_EGRESS),
-		  "fail_prog_detach_foo",
-		  "double detach from %s unexpectedly succeeded\n", FOO))
+	if (!ASSERT_ERR(bpf_prog_detach(foo, BPF_CGROUP_INET_EGRESS),
+			"fail_prog_detach_foo"))
 		goto err;
 
-	if (CHECK(bpf_prog_attach(allow_prog, foo, BPF_CGROUP_INET_EGRESS, 0),
-		  "prog_attach_allow_foo_none",
-		  "attach prog to %s failed, errno=%d\n", FOO, errno))
+	if (!ASSERT_OK(bpf_prog_attach(allow_prog, foo, BPF_CGROUP_INET_EGRESS, 0),
+		       "prog_attach_allow_foo_none"))
 		goto err;
 
-	if (CHECK(!bpf_prog_attach(allow_prog, bar, BPF_CGROUP_INET_EGRESS, 0),
-		  "fail_prog_attach_allow_bar_none",
-		  "attach prog to %s unexpectedly succeeded\n", BAR))
+	if (!ASSERT_ERR(bpf_prog_attach(allow_prog, bar, BPF_CGROUP_INET_EGRESS, 0),
+			"fail_prog_attach_allow_bar_none"))
 		goto err;
 
-	if (CHECK(!bpf_prog_attach(allow_prog, bar, BPF_CGROUP_INET_EGRESS,
-				   BPF_F_ALLOW_OVERRIDE),
-		  "fail_prog_attach_allow_bar_override",
-		  "attach prog to %s unexpectedly succeeded\n", BAR))
+	if (!ASSERT_ERR(bpf_prog_attach(allow_prog, bar, BPF_CGROUP_INET_EGRESS,
+					BPF_F_ALLOW_OVERRIDE),
+			"fail_prog_attach_allow_bar_override"))
 		goto err;
 
-	if (CHECK(!bpf_prog_attach(allow_prog, foo, BPF_CGROUP_INET_EGRESS,
-				   BPF_F_ALLOW_OVERRIDE),
-		  "fail_prog_attach_allow_foo_override",
-		  "attach prog to %s unexpectedly succeeded\n", FOO))
+	if (!ASSERT_ERR(bpf_prog_attach(allow_prog, foo, BPF_CGROUP_INET_EGRESS,
+					BPF_F_ALLOW_OVERRIDE),
+			"fail_prog_attach_allow_foo_override"))
 		goto err;
 
-	if (CHECK(bpf_prog_attach(drop_prog, foo, BPF_CGROUP_INET_EGRESS, 0),
-		  "prog_attach_drop_foo_none",
-		  "attach prog to %s failed, errno=%d\n", FOO, errno))
+	if (!ASSERT_OK(bpf_prog_attach(drop_prog, foo, BPF_CGROUP_INET_EGRESS, 0),
+		       "prog_attach_drop_foo_none"))
 		goto err;
 
 err:
