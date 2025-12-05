67a3019c8b3c51b1f7779f31ecd5dbfd6e39f877
Eduard Zingerman <eddyz87@gmail.com>
2025-12-05 12:08:59 -0800

    selftests/bpf: replace CHECK macro usage in skeleton.c
    Signed-off-by: Eduard Zingerman <eddyz87@gmail.com>

--- tools/testing/selftests/bpf/prog_tests/skeleton.c
+++ tools/testing/selftests/bpf/prog_tests/skeleton.c
@@ -13,7 +13,6 @@ struct s {
 
 void test_skeleton(void)
 {
-	int duration = 0, err;
 	struct test_skeleton* skel;
 	struct test_skeleton__bss *bss;
 	struct test_skeleton__data *data;
@@ -23,14 +22,14 @@ void test_skeleton(void)
 	struct test_skeleton__kconfig *kcfg;
 	const void *elf_bytes;
 	size_t elf_bytes_sz = 0;
+	int i, fd, err;
 	void *m;
-	int i, fd;
 
 	skel = test_skeleton__open();
-	if (CHECK(!skel, "skel_open", "failed to open skeleton\n"))
+	if (!ASSERT_OK_PTR(skel, "skel_open"))
 		return;
 
-	if (CHECK(skel->kconfig, "skel_kconfig", "kconfig is mmaped()!\n"))
+	if (!ASSERT_NULL(skel->kconfig, "skel_kconfig not mmaped"))
 		goto cleanup;
 
 	bss = skel->bss;
@@ -43,18 +42,18 @@ void test_skeleton(void)
 	ASSERT_STREQ(bpf_map__name(skel->maps.data_dyn), ".data.dyn", "data_dyn_name");
 
 	/* validate values are pre-initialized correctly */
-	CHECK(data->in1 != -1, "in1", "got %d != exp %d\n", data->in1, -1);
-	CHECK(data->out1 != -1, "out1", "got %d != exp %d\n", data->out1, -1);
-	CHECK(data->in2 != -1, "in2", "got %lld != exp %lld\n", data->in2, -1LL);
-	CHECK(data->out2 != -1, "out2", "got %lld != exp %lld\n", data->out2, -1LL);
+	ASSERT_EQ(data->in1, -1, "in1");
+	ASSERT_EQ(data->out1, -1, "out1");
+	ASSERT_EQ(data->in2, -1, "in2");
+	ASSERT_EQ(data->out2, -1, "out2");
 
-	CHECK(bss->in3 != 0, "in3", "got %d != exp %d\n", bss->in3, 0);
-	CHECK(bss->out3 != 0, "out3", "got %d != exp %d\n", bss->out3, 0);
-	CHECK(bss->in4 != 0, "in4", "got %lld != exp %lld\n", bss->in4, 0LL);
-	CHECK(bss->out4 != 0, "out4", "got %lld != exp %lld\n", bss->out4, 0LL);
+	ASSERT_EQ(bss->in3, 0, "in3");
+	ASSERT_EQ(bss->out3, 0, "out3");
+	ASSERT_EQ(bss->in4, 0, "in4");
+	ASSERT_EQ(bss->out4, 0, "out4");
 
-	CHECK(rodata->in.in6 != 0, "in6", "got %d != exp %d\n", rodata->in.in6, 0);
-	CHECK(bss->out6 != 0, "out6", "got %d != exp %d\n", bss->out6, 0);
+	ASSERT_EQ(rodata->in.in6, 0, "in6");
+	ASSERT_EQ(bss->out6, 0, "out6");
 
 	ASSERT_EQ(rodata_dyn->in_dynarr_sz, 0, "in_dynarr_sz");
 	for (i = 0; i < 4; i++)
@@ -74,15 +73,15 @@ void test_skeleton(void)
 		rodata_dyn->in_dynarr[i] = i + 10;
 
 	err = test_skeleton__load(skel);
-	if (CHECK(err, "skel_load", "failed to load skeleton: %d\n", err))
+	if (!ASSERT_OK(err, "skel_load"))
 		goto cleanup;
 
 	/* validate pre-setup values are still there */
-	CHECK(data->in1 != 10, "in1", "got %d != exp %d\n", data->in1, 10);
-	CHECK(data->in2 != 11, "in2", "got %lld != exp %lld\n", data->in2, 11LL);
-	CHECK(bss->in3 != 12, "in3", "got %d != exp %d\n", bss->in3, 12);
-	CHECK(bss->in4 != 13, "in4", "got %lld != exp %lld\n", bss->in4, 13LL);
-	CHECK(rodata->in.in6 != 14, "in6", "got %d != exp %d\n", rodata->in.in6, 14);
+	ASSERT_EQ(data->in1, 10, "in1");
+	ASSERT_EQ(data->in2, 11, "in2");
+	ASSERT_EQ(bss->in3, 12, "in3");
+	ASSERT_EQ(bss->in4, 13, "in4");
+	ASSERT_EQ(rodata->in.in6, 14, "in6");
 
 	ASSERT_EQ(rodata_dyn->in_dynarr_sz, 4, "in_dynarr_sz");
 	for (i = 0; i < 4; i++)
@@ -100,24 +99,22 @@ void test_skeleton(void)
 	skel->data_read_mostly->read_mostly_var = 123;
 
 	err = test_skeleton__attach(skel);
-	if (CHECK(err, "skel_attach", "skeleton attach failed: %d\n", err))
+	if (!ASSERT_OK(err, "skel_attach"))
 		goto cleanup;
 
 	/* trigger tracepoint */
 	usleep(1);
 
-	CHECK(data->out1 != 1, "res1", "got %d != exp %d\n", data->out1, 1);
-	CHECK(data->out2 != 2, "res2", "got %lld != exp %d\n", data->out2, 2);
-	CHECK(bss->out3 != 3, "res3", "got %d != exp %d\n", (int)bss->out3, 3);
-	CHECK(bss->out4 != 4, "res4", "got %lld != exp %d\n", bss->out4, 4);
-	CHECK(bss->out5.a != 5, "res5", "got %d != exp %d\n", bss->out5.a, 5);
-	CHECK(bss->out5.b != 6, "res6", "got %lld != exp %d\n", bss->out5.b, 6);
-	CHECK(bss->out6 != 14, "res7", "got %d != exp %d\n", bss->out6, 14);
+	ASSERT_EQ(data->out1, 1, "res1");
+	ASSERT_EQ(data->out2, 2, "res2");
+	ASSERT_EQ(bss->out3, 3, "res3");
+	ASSERT_EQ(bss->out4, 4, "res4");
+	ASSERT_EQ(bss->out5.a, 5, "res5");
+	ASSERT_EQ(bss->out5.b, 6, "res6");
+	ASSERT_EQ(bss->out6, 14, "res7");
 
-	CHECK(bss->bpf_syscall != kcfg->CONFIG_BPF_SYSCALL, "ext1",
-	      "got %d != exp %d\n", bss->bpf_syscall, kcfg->CONFIG_BPF_SYSCALL);
-	CHECK(bss->kern_ver != kcfg->LINUX_KERNEL_VERSION, "ext2",
-	      "got %d != exp %d\n", bss->kern_ver, kcfg->LINUX_KERNEL_VERSION);
+	ASSERT_EQ(bss->bpf_syscall, kcfg->CONFIG_BPF_SYSCALL, "ext1");
+	ASSERT_EQ(bss->kern_ver, kcfg->LINUX_KERNEL_VERSION, "ext2");
 
 	for (i = 0; i < 4; i++)
 		ASSERT_EQ(data_dyn->out_dynarr[i], i + 10, "out_dynarr");
