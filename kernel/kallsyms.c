40051691522f2d6c4de8b3f82477dccab6c90443
Maurice Hieronymus <mhi@mailbox.org>
2025-12-10 17:28:15 +0100

    kallsyms: Always initialize modbuildid
    modbuildid is never set when kallsyms_lookup_buildid is returning via
    successful bpf_address_lookup or ftrace_mod_address_lookup.
    
    This leads to an uninitialized pointer dereference on x86 when
    CONFIG_STACKTRACE_BUILD_ID=y inside __sprint_symbol.
    
    Prevent this by always initializing modbuildid.
    
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=220717
    Signed-off-by: Maurice Hieronymus <mhi@mailbox.org>

--- kernel/kallsyms.c
+++ kernel/kallsyms.c
@@ -378,11 +378,11 @@ static int kallsyms_lookup_buildid(unsigned long addr,
 				    modname, modbuildid, namebuf);
 	if (!ret)
 		ret = bpf_address_lookup(addr, symbolsize,
-					 offset, modname, namebuf);
+					 offset, modname, modbuildid, namebuf);
 
 	if (!ret)
 		ret = ftrace_mod_address_lookup(addr, symbolsize,
-						offset, modname, namebuf);
+						offset, modname, modbuildid, namebuf);
 
 	return ret;
 }
