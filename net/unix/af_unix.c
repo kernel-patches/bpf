222425f3951d6090720b59531fad68aaade8445c
Jens Axboe <axboe@kernel.dk>
2025-12-18 07:59:13 -0700

    af_unix: don't post cmsg for SO_INQ unless explicitly asked for
    A previous commit added SO_INQ support for AF_UNIX (SOCK_STREAM), but
    it posts a SCM_INQ cmsg even if just msg->msg_get_inq is set. This is
    incorrect, as ->msg_get_inq is just the caller asking for the remainder
    to be passed back in msg->msg_inq, it has nothing to do with cmsg. The
    original commit states that this is done to make sockets
    io_uring-friendly", but it's actually incorrect as io_uring doesn't
    use cmsg headers internally at all, and it's actively wrong as this
    means that cmsg's are always posted if someone does recvmsg via
    io_uring.
    
    Fix that up by only posting cmsg if u->recvmsg_inq is set.
    
    Cc: stable@vger.kernel.org
    Fixes: df30285b3670 ("af_unix: Introduce SO_INQ.")
    Reported-by: Julian Orth <ju.orth@gmail.com>
    Link: https://github.com/axboe/liburing/issues/1509
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: NipaLocal <nipa@local>

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -3086,12 +3086,16 @@ unlock:
 
 	mutex_unlock(&u->iolock);
 	if (msg) {
+		bool do_cmsg;
+
 		scm_recv_unix(sock, msg, &scm, flags);
 
-		if (READ_ONCE(u->recvmsg_inq) || msg->msg_get_inq) {
+		do_cmsg = READ_ONCE(u->recvmsg_inq);
+		if (do_cmsg || msg->msg_get_inq) {
 			msg->msg_inq = READ_ONCE(u->inq_len);
-			put_cmsg(msg, SOL_SOCKET, SCM_INQ,
-				 sizeof(msg->msg_inq), &msg->msg_inq);
+			if (do_cmsg)
+				put_cmsg(msg, SOL_SOCKET, SCM_INQ,
+					 sizeof(msg->msg_inq), &msg->msg_inq);
 		}
 	} else {
 		scm_destroy(&scm);
