5ab95ced97c53fef605dccc4a94b2ae5238e19a3
Vimlesh Kumar <vimleshk@marvell.com>
2025-12-19 10:07:49 +0000

    octeon_ep_vf: ensure dbell BADDR updation
    Make sure the OUT DBELL base address reflects the
    latest values written to it.
    
    Fix:
    Add a wait until the OUT DBELL base address register
    is updated with the DMA ring descriptor address,
    and modify the setup_oq function to properly
    handle failures.
    
    Fixes: 2c0c32c72be29 ("octeon_ep_vf: add hardware configuration APIs")
    Signed-off-by: Sathesh Edara <sedara@marvell.com>
    Signed-off-by: Shinas Rasheed <srasheed@marvell.com>
    Signed-off-by: Vimlesh Kumar <vimleshk@marvell.com>
    Signed-off-by: NipaLocal <nipa@local>

--- drivers/net/ethernet/marvell/octeon_ep_vf/octep_vf_rx.c
+++ drivers/net/ethernet/marvell/octeon_ep_vf/octep_vf_rx.c
@@ -171,7 +171,9 @@ static int octep_vf_setup_oq(struct octep_vf_device *oct, int q_no)
 		goto oq_fill_buff_err;
 
 	octep_vf_oq_reset_indices(oq);
-	oct->hw_ops.setup_oq_regs(oct, q_no);
+	if (oct->hw_ops.setup_oq_regs(oct, q_no))
+		goto oq_fill_buff_err;
+
 	oct->num_oqs++;
 
 	return 0;
