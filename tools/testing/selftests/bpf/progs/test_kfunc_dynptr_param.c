bb2a409e76f53a385d0d369f7e6411713912c1a3
Puranjay Mohan <puranjay@kernel.org>
2025-12-24 11:24:35 -0800

    selftests: bpf: fix test_kfunc_dynptr_param
    As verifier now assumes that all kfuncs only takes trusted pointer
    arguments, passing 0 (NULL) to a kfunc that doesn't mark the argument as
    __nullable or __opt will be rejected with a failure message of: Possibly
    NULL pointer passed to trusted arg<n>
    
    Pass a non-null value to the kfunc to test the expected failure mode.
    
    Signed-off-by: Puranjay Mohan <puranjay@kernel.org>

--- tools/testing/selftests/bpf/progs/test_kfunc_dynptr_param.c
+++ tools/testing/selftests/bpf/progs/test_kfunc_dynptr_param.c
@@ -48,7 +48,7 @@ SEC("?lsm.s/bpf")
 __failure __msg("arg#0 expected pointer to stack or const struct bpf_dynptr")
 int BPF_PROG(not_ptr_to_stack, int cmd, union bpf_attr *attr, unsigned int size, bool kernel)
 {
-	unsigned long val = 0;
+	unsigned long val = 1;
 
 	return bpf_verify_pkcs7_signature((struct bpf_dynptr *)val,
 					  (struct bpf_dynptr *)val, NULL);
