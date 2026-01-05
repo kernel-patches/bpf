3388ad21bd3404806d6086c8f3ea83a1d08958b8
Jakub Sitnicki <jakub@cloudflare.com>
2026-01-05 13:14:26 +0100

    bnxt_en: Call skb_metadata_set when skb->data points at metadata end
    Prepare to track skb metadata location independently of MAC header offset.
    
    Following changes will make skb_metadata_set() record where metadata ends
    relative to skb->head. Hence the helper must be called when skb->data
    already points past the metadata area.
    
    Adjust the driver to pull from skb->data before calling skb_metadata_set().
    
    Signed-off-by: Jakub Sitnicki <jakub@cloudflare.com>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.c
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.c
@@ -1440,8 +1440,8 @@ static struct sk_buff *bnxt_copy_xdp(struct bnxt_napi *bnapi,
 		return skb;
 
 	if (metasize) {
-		skb_metadata_set(skb, metasize);
 		__skb_pull(skb, metasize);
+		skb_metadata_set(skb, metasize);
 	}
 
 	return skb;
