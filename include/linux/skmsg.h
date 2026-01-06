21a02427809c9ae500f3b64388464ca97d58a3b2
Jiayuan Chen <jiayuan.chen@linux.dev>
2026-01-06 13:14:28 +0800

    bpf, sockmap: Fix FIONREAD for sockmap
    A socket using sockmap has its own independent receive queue: ingress_msg.
    This queue may contain data from its own protocol stack or from other
    sockets.
    
    Therefore, for sockmap, relying solely on copied_seq and rcv_nxt to
    calculate FIONREAD is not enough.
    
    This patch adds a new ingress_size field in the psock structure to record
    the data length in ingress_msg. Additionally, we implement new ioctl
    interfaces for TCP and UDP to intercept FIONREAD operations. While Unix
    and VSOCK also support sockmap and have similar FIONREAD calculation
    issues, fixing them would require more extensive changes
    (please let me know if modifications are needed). I believe it's not
    appropriate to include those changes under this fix patch.
    
    Previous work by John Fastabend made some efforts towards FIONREAD support:
    commit e5c6de5fa025 ("bpf, sockmap: Incorrectly handling copied_seq")
    Although the current patch is based on the previous work by John Fastabend,
    it is acceptable for our Fixes tag to point to the same commit.
    
                                                         FD1:read()
                                                         --  FD1->copied_seq++
                                                             |  [read data]
                                                             |
                                    [enqueue data]           v
                      [sockmap]     -> ingress to self ->  ingress_msg queue
    FD1 native stack  ------>                                 ^
    -- FD1->rcv_nxt++               -> redirect to other      | [enqueue data]
                                           |                  |
                                           |             ingress to FD1
                                           v                  ^
                                          ...                 |  [sockmap]
                                                         FD2 native stack
    
    Fixes: 04919bed948dc ("tcp: Introduce tcp_read_skb()")
    Signed-off-by: Jiayuan Chen <jiayuan.chen@linux.dev>

--- include/linux/skmsg.h
+++ include/linux/skmsg.h
@@ -97,6 +97,7 @@ struct sk_psock {
 	struct sk_buff_head		ingress_skb;
 	struct list_head		ingress_msg;
 	spinlock_t			ingress_lock;
+	ssize_t				ingress_size;
 	unsigned long			state;
 	struct list_head		link;
 	spinlock_t			link_lock;
@@ -321,6 +322,27 @@ static inline void sock_drop(struct sock *sk, struct sk_buff *skb)
 	kfree_skb(skb);
 }
 
+static inline ssize_t sk_psock_get_msg_size_nolock(struct sk_psock *psock)
+{
+	/* Used by ioctl to read ingress_size only; lock-free for performance */
+	return READ_ONCE(psock->ingress_size);
+}
+
+static inline void sk_psock_inc_msg_size_locked(struct sk_psock *psock, ssize_t diff)
+{
+	/* Use WRITE_ONCE to ensure correct read in sk_psock_get_msg_size_nolock().
+	 * ingress_lock should be held to prevent concurrent updates to ingress_size
+	 */
+	WRITE_ONCE(psock->ingress_size, psock->ingress_size + diff);
+}
+
+static inline void sk_psock_inc_msg_size(struct sk_psock *psock, ssize_t diff)
+{
+	spin_lock_bh(&psock->ingress_lock);
+	sk_psock_inc_msg_size_locked(psock, diff);
+	spin_unlock_bh(&psock->ingress_lock);
+}
+
 static inline bool sk_psock_queue_msg(struct sk_psock *psock,
 				      struct sk_msg *msg)
 {
@@ -329,6 +351,7 @@ static inline bool sk_psock_queue_msg(struct sk_psock *psock,
 	spin_lock_bh(&psock->ingress_lock);
 	if (sk_psock_test_state(psock, SK_PSOCK_TX_ENABLED)) {
 		list_add_tail(&msg->list, &psock->ingress_msg);
+		sk_psock_inc_msg_size_locked(psock, msg->sg.size);
 		ret = true;
 	} else {
 		sk_msg_free(psock->sk, msg);
@@ -345,10 +368,17 @@ static inline struct sk_msg *sk_psock_dequeue_msg(struct sk_psock *psock)
 
 	spin_lock_bh(&psock->ingress_lock);
 	msg = list_first_entry_or_null(&psock->ingress_msg, struct sk_msg, list);
-	if (msg)
+	if (msg) {
 		list_del(&msg->list);
+		sk_psock_inc_msg_size_locked(psock, -msg->sg.size);
+	}
 	spin_unlock_bh(&psock->ingress_lock);
 	return msg;
+}
+
+static inline struct sk_msg *sk_psock_peek_msg_locked(struct sk_psock *psock)
+{
+	return list_first_entry_or_null(&psock->ingress_msg, struct sk_msg, list);
 }
 
 static inline struct sk_msg *sk_psock_peek_msg(struct sk_psock *psock)
@@ -356,7 +386,7 @@ static inline struct sk_msg *sk_psock_peek_msg(struct sk_psock *psock)
 	struct sk_msg *msg;
 
 	spin_lock_bh(&psock->ingress_lock);
-	msg = list_first_entry_or_null(&psock->ingress_msg, struct sk_msg, list);
+	msg = sk_psock_peek_msg_locked(psock);
 	spin_unlock_bh(&psock->ingress_lock);
 	return msg;
 }
@@ -521,6 +551,39 @@ static inline bool sk_psock_strp_enabled(struct sk_psock *psock)
 	if (!psock)
 		return false;
 	return !!psock->saved_data_ready;
+}
+
+/* for tcp only, sk is locked */
+static inline ssize_t sk_psock_msg_inq(struct sock *sk)
+{
+	struct sk_psock *psock;
+	ssize_t inq = 0;
+
+	psock = sk_psock_get(sk);
+	if (likely(psock)) {
+		inq = sk_psock_get_msg_size_nolock(psock);
+		sk_psock_put(sk, psock);
+	}
+	return inq;
+}
+
+/* for udp only, sk is not locked */
+static inline ssize_t sk_msg_first_length(struct sock *sk)
+{
+	struct sk_psock *psock;
+	struct sk_msg *msg;
+	ssize_t inq = 0;
+
+	psock = sk_psock_get(sk);
+	if (likely(psock)) {
+		spin_lock_bh(&psock->ingress_lock);
+		msg = sk_psock_peek_msg_locked(psock);
+		if (msg)
+			inq = msg->sg.size;
+		spin_unlock_bh(&psock->ingress_lock);
+		sk_psock_put(sk, psock);
+	}
+	return inq;
 }
 
 #if IS_ENABLED(CONFIG_NET_SOCK_MSG)
