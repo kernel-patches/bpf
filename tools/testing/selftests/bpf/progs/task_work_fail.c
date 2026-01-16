afc95ec6afc2ff860085e23e904f70eab7bf8428
Ihor Solodrai <ihor.solodrai@linux.dev>
2026-01-16 12:16:56 -0800

    bpf: Migrate bpf_task_work_schedule_* kfuncs to KF_IMPLICIT_ARGS
    Implement bpf_task_work_schedule_* with an implicit bpf_prog_aux
    argument, and remove corresponding _impl funcs from the kernel.
    
    Update special kfunc checks in the verifier accordingly.
    
    Update the selftests to use the new API with implicit argument.
    
    Reviewed-by: Eduard Zingerman <eddyz87@gmail.com>
    Signed-off-by: Ihor Solodrai <ihor.solodrai@linux.dev>

--- tools/testing/selftests/bpf/progs/task_work_fail.c
+++ tools/testing/selftests/bpf/progs/task_work_fail.c
@@ -53,7 +53,9 @@ int mismatch_map(struct pt_regs *args)
 	work = bpf_map_lookup_elem(&arrmap, &key);
 	if (!work)
 		return 0;
-	bpf_task_work_schedule_resume_impl(task, &work->tw, &hmap, process_work, NULL);
+
+	bpf_task_work_schedule_resume(task, &work->tw, &hmap, process_work);
+
 	return 0;
 }
 
@@ -65,7 +67,9 @@ int no_map_task_work(struct pt_regs *args)
 	struct bpf_task_work tw;
 
 	task = bpf_get_current_task_btf();
-	bpf_task_work_schedule_resume_impl(task, &tw, &hmap, process_work, NULL);
+
+	bpf_task_work_schedule_resume(task, &tw, &hmap, process_work);
+
 	return 0;
 }
 
@@ -76,7 +80,9 @@ int task_work_null(struct pt_regs *args)
 	struct task_struct *task;
 
 	task = bpf_get_current_task_btf();
-	bpf_task_work_schedule_resume_impl(task, NULL, &hmap, process_work, NULL);
+
+	bpf_task_work_schedule_resume(task, NULL, &hmap, process_work);
+
 	return 0;
 }
 
@@ -91,6 +97,8 @@ int map_null(struct pt_regs *args)
 	work = bpf_map_lookup_elem(&arrmap, &key);
 	if (!work)
 		return 0;
-	bpf_task_work_schedule_resume_impl(task, &work->tw, NULL, process_work, NULL);
+
+	bpf_task_work_schedule_resume(task, &work->tw, NULL, process_work);
+
 	return 0;
 }
