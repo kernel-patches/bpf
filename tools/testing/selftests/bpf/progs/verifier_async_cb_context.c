7d69d0d9984d2af73dd4aedba3fb19386a032c37
Ihor Solodrai <ihor.solodrai@linux.dev>
2026-01-20 14:26:34 -0800

    bpf: Migrate bpf_task_work_schedule_* kfuncs to KF_IMPLICIT_ARGS
    Implement bpf_task_work_schedule_* with an implicit bpf_prog_aux
    argument, and remove corresponding _impl funcs from the kernel.
    
    Update special kfunc checks in the verifier accordingly.
    
    Update the selftests to use the new API with implicit argument.
    
    Reviewed-by: Eduard Zingerman <eddyz87@gmail.com>
    Signed-off-by: Ihor Solodrai <ihor.solodrai@linux.dev>

--- tools/testing/selftests/bpf/progs/verifier_async_cb_context.c
+++ tools/testing/selftests/bpf/progs/verifier_async_cb_context.c
@@ -156,7 +156,8 @@ int task_work_non_sleepable_prog(void *ctx)
 	if (!task)
 		return 0;
 
-	bpf_task_work_schedule_resume_impl(task, &val->tw, &task_work_map, task_work_cb, NULL);
+	bpf_task_work_schedule_resume(task, &val->tw, &task_work_map, task_work_cb);
+
 	return 0;
 }
 
@@ -176,6 +177,7 @@ int task_work_sleepable_prog(void *ctx)
 	if (!task)
 		return 0;
 
-	bpf_task_work_schedule_resume_impl(task, &val->tw, &task_work_map, task_work_cb, NULL);
+	bpf_task_work_schedule_resume(task, &val->tw, &task_work_map, task_work_cb);
+
 	return 0;
 }
