beee2124c3fd8964c4c16abaee8d4cd862713db4
Jiri Olsa <jolsa@kernel.org>
2026-02-03 10:38:13 +0100

    bpf: Add bpf_trampoline_multi_attach/detach functions
    Adding bpf_trampoline_multi_attach/detach functions that allows
    to attach/detach multi tracing trampoline.
    
    The attachment is defined with bpf_program and array of BTF ids
    of functions to attach the bpf program to.
    
    The attachment will allocate or use currently existing trampoline
    for function to attach and link it with the bpf program.
    
    The attach works as follows:
    - we get all the needed trampolines
    - lock them and add the bpf program to each (__bpf_trampoline_link_prog)
    - the trampoline_multi_ops passed in __bpf_trampoline_link_prog gather needed
      ftrace_hash ip->trampoline data
    - we call update_ftrace_direct_add/mod to update needed locations
    - we unlock all the trampolines
    
    The detach works as follows:
    - we lock all the needed trampolines
    - remove the program from each (__bpf_trampoline_unlink_prog)
    - the trampoline_multi_ops passed in __bpf_trampoline_link_prog gather needed
      ftrace_hash ip->trampoline data
    - we call update_ftrace_direct_del/mod to update needed locations
    - we unlock and put all the trampolines
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -1466,6 +1466,12 @@ struct bpf_trampoline *bpf_trampoline_get(u64 key,
 void bpf_trampoline_put(struct bpf_trampoline *tr);
 int arch_prepare_bpf_dispatcher(void *image, void *buf, s64 *funcs, int num_funcs);
 
+struct bpf_tracing_multi_link;
+int bpf_trampoline_multi_attach(struct bpf_prog *prog, u32 *ids,
+				struct bpf_tracing_multi_link *link);
+int bpf_trampoline_multi_detach(struct bpf_prog *prog,
+				struct bpf_tracing_multi_link *link);
+
 /*
  * When the architecture supports STATIC_CALL replace the bpf_dispatcher_fn
  * indirection with a direct call to the bpf program. If the architecture does
@@ -1896,6 +1902,18 @@ struct bpf_tracing_link {
 struct bpf_fsession_link {
 	struct bpf_tracing_link link;
 	struct bpf_tramp_link fexit;
+};
+
+struct bpf_tracing_multi_node {
+	struct bpf_tramp_node node;
+	struct bpf_trampoline *trampoline;
+};
+
+struct bpf_tracing_multi_link {
+	struct bpf_link link;
+	enum bpf_attach_type attach_type;
+	int nodes_cnt;
+	struct bpf_tracing_multi_node nodes[] __counted_by(nodes_cnt);
 };
 
 struct bpf_raw_tp_link {
