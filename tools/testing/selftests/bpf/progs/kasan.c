a3c2c48fa7392818d9496ea1207bcf8698e64e8c
Alexis Lothoré (eBPF Foundation) <alexis.lothore@bootlin.com>
2026-02-17 11:12:15 +0100

    selftests/bpf: add tests to validate KASAN on JIT programs
    WIP: add a single test triggering a UAF access in an eBPF program, due
    to a kfunc behaving badly. This test is far from enough:
    - it does not detect the expected kasan splat programatically
    - there are other cases to test, eg OoB accesses
    - tests should stress better the code from emit_kasan_check (eg: more
      registers pressure to validate push/pop strategy)
    
    Signed-off-by: Alexis Lothoré (eBPF Foundation) <alexis.lothore@bootlin.com>

---
+++ tools/testing/selftests/bpf/progs/kasan.c
@@ -0,0 +1,19 @@
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+extern u8 *bpf_kfunc_kasan_uaf(void) __ksym;
+
+int kasan_counter;
+
+SEC("tcx/ingress")
+int bpf_kasan_uaf(struct __sk_buff *skb)
+{
+	volatile u8 *result = bpf_kfunc_kasan_uaf();
+
+	return result[0] ? 1 : 0;
+
+
+}
+
+char LICENSE[] SEC("license") = "GPL";
