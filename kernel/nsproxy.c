ee90bd3a3cf0011496897b86ca2b5d782e29c6be
Christian Brauner <brauner@kernel.org>
2026-02-19 10:19:03 +0100

    ns: add bpf hooks
    Add the three namespace lifecycle hooks and make them available to bpf
    lsm program types. This allows bpf to supervise namespace creation. I'm
    in the process of adding various "universal truth" bpf programs to
    systemd that will make use of this. This e.g., allows to lock in a
    program into a given set of namespaces.
    
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- kernel/nsproxy.c
+++ kernel/nsproxy.c
@@ -9,6 +9,7 @@
  *             Pavel Emelianov <xemul@openvz.org>
  */
 
+#include <linux/bpf_lsm.h>
 #include <linux/slab.h>
 #include <linux/export.h>
 #include <linux/nsproxy.h>
@@ -379,7 +380,13 @@ out:
 
 static inline int validate_ns(struct nsset *nsset, struct ns_common *ns)
 {
-	return ns->ops->install(nsset, ns);
+	int ret;
+
+	ret = ns->ops->install(nsset, ns);
+	if (ret)
+		return ret;
+
+	return bpf_lsm_namespace_install(nsset, ns);
 }
 
 /*
