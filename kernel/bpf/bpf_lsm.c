c8107f28acd241e07c0fd40425e7e351c790163e
Christian Brauner <brauner@kernel.org>
2026-02-20 14:49:10 +0100

    bpf: classify block device hooks appropriately
    A bunch of new hooks for managing block devices were added a while ago
    but they weren't actually appropriately classified.
    
    * bpf_lsm_bdev_alloc() is called when the inode for the block
      device is allocated. This happens from a sleepable context so mark the
      function as sleepable. When this function is called the memory for the
      block device storage embedded into the inode is zeroed. That block
      device cannot be meaningfully reference or interacted with at this
      point. So mark it as untrusted for now.
    
    * bpf_lsm_bdev_free() is called when the inode for the block
      device is freed. A bunch of memory associated with the block device
      has already been freed and there's dangling pointers in there. So mark
      it as untrusted. It cannot be meaningfully referenced or interacted
      with anymore. It is also called from sb->s_op->free_inode:: which
      means it runs in rcu context (most of the times). So leave it as
      non-sleepable.
    
    * bpf_lsm_bdev_setintegrity() is called when a dm-verity device
      is instantiated (glossing over details for simplicity of the commit
      message). The block device is very much alive so it remains a trusted
      hook. It's also called with device mapper's suspend lock held and so
      the hook is able to sleep so mark it sleepable.
    
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- kernel/bpf/bpf_lsm.c
+++ kernel/bpf/bpf_lsm.c
@@ -419,6 +419,8 @@ BTF_ID(func, bpf_lsm_userns_create)
 BTF_ID(func, bpf_lsm_namespace_alloc)
 BTF_ID(func, bpf_lsm_namespace_install)
 BTF_ID(func, bpf_lsm_cgroup_attach)
+BTF_ID(func, bpf_lsm_bdev_alloc)
+BTF_ID(func, bpf_lsm_bdev_setintegrity)
 BTF_SET_END(sleepable_lsm_hooks)
 
 BTF_SET_START(untrusted_lsm_hooks)
@@ -432,6 +434,8 @@ BTF_ID(func, bpf_lsm_sk_free_security)
 #endif /* CONFIG_SECURITY_NETWORK */
 BTF_ID(func, bpf_lsm_task_free)
 BTF_ID(func, bpf_lsm_namespace_free)
+BTF_ID(func, bpf_lsm_bdev_alloc)
+BTF_ID(func, bpf_lsm_bdev_free)
 BTF_SET_END(untrusted_lsm_hooks)
 
 bool bpf_lsm_is_sleepable_hook(u32 btf_id)
