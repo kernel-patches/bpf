4d336e5b71c44a0d605062d506e5e0f1a3db71df
Ihor Solodrai <ihor.solodrai@linux.dev>
2026-02-20 14:26:03 -0800

    selftests/bpf: Check BPFTOOL env var in detect_bpftool_path()
    The bpftool_maps_access and bpftool_metadata tests may fail on BPF CI
    with "command not found", depending on a workflow.
    This happens because detect_bpftool_path() only checks two hardcoded
    relative paths:
      - ./tools/sbin/bpftool
      - ../tools/sbin/bpftool
    
    Add support for a BPFTOOL environment variable that allows specifying
    the exact path to the bpftool binary.
    
    Acked-by: Mykyta Yatsenko <yatsenko@meta.com>
    Signed-off-by: Ihor Solodrai <ihor.solodrai@linux.dev>

--- tools/testing/selftests/bpf/bpftool_helpers.c
+++ tools/testing/selftests/bpf/bpftool_helpers.c
@@ -12,6 +12,17 @@
 static int detect_bpftool_path(char *buffer)
 {
 	char tmp[BPFTOOL_PATH_MAX_LEN];
+	const char *env_path;
+
+	/* First, check if BPFTOOL environment variable is set */
+	env_path = getenv("BPFTOOL");
+	if (env_path && access(env_path, X_OK) == 0) {
+		strscpy(buffer, env_path, size);
+		return 0;
+	} else if (env_path) {
+		fprintf(stderr, "bpftool '%s' doesn't exist or is not executable\n", env_path);
+		return 1;
+	}
 
 	/* Check default bpftool location (will work if we are running the
 	 * default flavor of test_progs)
@@ -31,7 +42,7 @@ static int detect_bpftool_path(char *buffer)
 		return 0;
 	}
 
-	/* Failed to find bpftool binary */
+	fprintf(stderr, "Failed to detect bpftool path, use BPFTOOL env var to override\n");
 	return 1;
 }
 
