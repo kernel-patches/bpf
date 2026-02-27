3617f9ed80c88e1ed3a233d42f4bc729bffd4417
Sun Jian <sun.jian.kdev@gmail.com>
2026-02-28 00:40:37 +0800

    selftests/bpf: bpf_cookie: make perf_event subtest trigger reliably
    The perf_event subtest relies on SW_CPU_CLOCK sampling to trigger the BPF
    program, but the current CPU burn loop can be too short on slower systems
    and may fail to generate any overflow sample. This leaves pe_res unchanged
    and makes the test flaky.
    
    Make burn_cpu() take a loop count and use a longer burn only for the
    perf_event subtest. Also scope perf_event_open() to the current task to
    avoid wasting samples on unrelated activity.
    
    Tested:
      ./test_progs -t bpf_cookie/perf_event -vv (30 runs): 0 failures
    
    Signed-off-by: Sun Jian <sun.jian.kdev@gmail.com>

--- tools/testing/selftests/bpf/prog_tests/bpf_cookie.c
+++ tools/testing/selftests/bpf/prog_tests/bpf_cookie.c
@@ -6,6 +6,7 @@
 #include <sys/syscall.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <linux/compiler.h>
 #include <test_progs.h>
 #include <network_helpers.h>
 #include <bpf/btf.h>
@@ -431,11 +432,12 @@ cleanup:
 	bpf_link__destroy(link3);
 }
 
-static void burn_cpu(void)
+static void burn_cpu(long loops)
 {
-	volatile int j = 0;
+	long j = 0;
 	cpu_set_t cpu_set;
-	int i, err;
+	long i;
+	int err;
 
 	/* generate some branches on cpu 0 */
 	CPU_ZERO(&cpu_set);
@@ -443,9 +445,10 @@ static void burn_cpu(void)
 	err = pthread_setaffinity_np(pthread_self(), sizeof(cpu_set), &cpu_set);
 	ASSERT_OK(err, "set_thread_affinity");
 
-	/* spin the loop for a while (random high number) */
-	for (i = 0; i < 1000000; ++i)
+	for (i = 0; i < loops; ++i) {
 		++j;
+		barrier();
+	}
 }
 
 static void pe_subtest(struct test_bpf_cookie *skel)
@@ -470,7 +473,7 @@ static void pe_subtest(struct test_bpf_cookie *skel)
 	if (!ASSERT_OK_PTR(link, "link1"))
 		goto cleanup;
 
-	burn_cpu(); /* trigger BPF prog */
+	burn_cpu(100000000L); /* trigger BPF prog */
 
 	ASSERT_EQ(skel->bss->pe_res, 0x100000, "pe_res1");
 
@@ -489,7 +492,7 @@ static void pe_subtest(struct test_bpf_cookie *skel)
 	if (!ASSERT_OK_PTR(link, "link2"))
 		goto cleanup;
 
-	burn_cpu(); /* trigger BPF prog */
+	burn_cpu(100000000L); /* trigger BPF prog */
 
 	ASSERT_EQ(skel->bss->pe_res, 0x200000, "pe_res2");
 
