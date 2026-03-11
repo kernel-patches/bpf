ebcc5e2f3a146378a00865074021d80e3aaf644c
Mykyta Yatsenko <yatsenko@meta.com>
2026-03-11 11:22:52 -0700

    selftests/bpf: Add tests for sleepable raw tracepoint programs
    Add tests for sleepable raw tracepoint programs:
    - success: Attach a sleepable BPF program to the faultable sys_enter
      tracepoint (tp_btf.s/sys_enter). Verify the program is triggered by
      a syscall.
    - reject_non_faultable: Verify that loading a sleepable BPF program
      targeting a non-faultable tracepoint (tp_btf.s/sched_switch) is
      rejected by the verifier.
    
    Acked-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
    Signed-off-by: Mykyta Yatsenko <yatsenko@meta.com>
    Reviewed-by: Emil Tsalapatis <emil@etsalapatis.com>
    Acked-by: Puranjay Mohan <puranjay@kernel.org>

---
+++ tools/testing/selftests/bpf/prog_tests/sleepable_raw_tp.c
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (c) 2025 Meta Platforms, Inc. and affiliates. */
+
+#include <test_progs.h>
+#include <time.h>
+#include "test_sleepable_raw_tp.skel.h"
+#include "test_sleepable_raw_tp_fail.skel.h"
+
+static void test_sleepable_raw_tp_success(void)
+{
+	struct test_sleepable_raw_tp *skel;
+	int err;
+
+	skel = test_sleepable_raw_tp__open_and_load();
+	if (!ASSERT_OK_PTR(skel, "skel_open_load"))
+		return;
+
+	skel->bss->target_pid = getpid();
+
+	err = test_sleepable_raw_tp__attach(skel);
+	if (!ASSERT_OK(err, "skel_attach"))
+		goto cleanup;
+
+	syscall(__NR_nanosleep, &(struct timespec){ .tv_nsec = 555 }, NULL);
+
+	ASSERT_EQ(skel->bss->triggered, 1, "triggered");
+	ASSERT_EQ(skel->bss->err, 0, "err");
+	ASSERT_EQ(skel->bss->copied_tv_nsec, 555, "copied_tv_nsec");
+
+cleanup:
+	test_sleepable_raw_tp__destroy(skel);
+}
+
+void test_sleepable_raw_tp(void)
+{
+	if (test__start_subtest("success"))
+		test_sleepable_raw_tp_success();
+
+	RUN_TESTS(test_sleepable_raw_tp_fail);
+}
