931bc0d012f5a7acb7be67690193ff275141726f
Harishankar Vishwanathan <harishankar.vishwanathan@gmail.com>
2026-03-13 23:54:43 +0100

    bpf: Simulate branches to prune based on range violations
    This patch fixes the invariant violation warnings. These warnings
    happen after we refine ranges & tnum based on an incorrectly-detected
    branch condition. For example, the branch is always true, but we miss
    it in is_branch_taken; we then refine based on the branch being false
    and end up with incoherent ranges (e.g. umax < umin).
    
    To avoid this, we can simulate the refinement on both branches. More
    specifically, this patch simulates both branches taken using
    regs_refine_cond_op and reg_bounds_sync. If the resulting register
    states are ill-formed on one of the branches, is_branch_taken can mark
    that branch as "never taken".
    
    On a more formal note, we can deduce a branch is not taken when
    regs_refine_cond_op or reg_bounds_syncreturns an ill-formed state
    because the branch operators are sound (formally verified). Soundness
    means that the verifier is guaranteed to produce sound outputs on the
    taken branches. On the non-taken branch (explored because of
    imprecision in the bounds), the verifier is free to produce any output.
    We use ill-formedness as a signal that the branch is dead and prune
    that branch.
    
    We have had five patches fixing specific cases of invariant violations
    in the past, all added with selftests:
    - commit fbc7aef517d8 ("bpf: Fix u32/s32 bounds when ranges cross
      min/max boundary")
    - commit efc11a667878 ("bpf: Improve bounds when tnum has a single
      possible value")
    - commit f41345f47fb2 ("bpf: Use tnums for JEQ/JNE is_branch_taken
      logic")
    - commit 00bf8d0c6c9b ("bpf: Improve bounds when s64 crosses sign
      boundary")
    - commit 6279846b9b25 ("bpf: Forget ranges when refining tnum after
      JSET")
    
    To confirm that this patch addresses all invariant violations, we have
    also reverted those five commits and verified that their related
    selftests don't cause any invariant violation warnings anymore. Those
    selftests still fail but only because of mis-detected branches or
    less-precise bounds than expected. This demonstrates that the current
    patch is enough to avoid the invariant violation warning AND that the
    previous five patches are still useful to improve branch detection.
    
    In addition to the selftests, this change was also tested with the
    Cilium complexity test suite: all programs were successfully loaded and
    it didn't change the number of processed instructions.
    
    Fixes: 5f99f312bd3b ("bpf: add register bounds sanity checks and sanitization")
    Reported-by: syzbot+c950cc277150935cc0b5@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=c950cc277150935cc0b5
    Co-developed-by: Paul Chaignon <paul.chaignon@gmail.com>
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>
    Signed-off-by: Harishankar Vishwanathan <harishankar.vishwanathan@gmail.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -16712,10 +16712,62 @@ static void find_good_pkt_pointers(struct bpf_verifier_state *vstate,
 	}));
 }
 
+static void regs_refine_cond_op(struct bpf_reg_state *reg1, struct bpf_reg_state *reg2,
+				u8 opcode, bool is_jmp32);
+static u8 rev_opcode(u8 opcode);
+
+/* Learn more information about live branches by simulating both branches being taken using
+ * regs_refine_cond_op. Because regs_refine_cond_op is sound when the branch is taken, if it
+ * produces ill-formed register bounds, it must mean that the branch is dead.
+ */
+static int simulate_both_branches_taken(struct bpf_verifier_env* env,
+				    struct bpf_reg_state *false_reg1,
+					struct bpf_reg_state *false_reg2, 
+					u8 opcode, bool is_jmp32)
+{
+	/* Create copies of reg states to simulate both branches because
+	 * regs_refine_cond_op will modify the reg states passed in. 
+	 */
+	copy_register_state(&env->false_reg1, false_reg1);
+	copy_register_state(&env->false_reg2, false_reg2);
+	copy_register_state(&env->true_reg1, false_reg1);
+	copy_register_state(&env->true_reg2, false_reg2);
+
+	/* Fallthrough (FALSE) branch */
+	regs_refine_cond_op(&env->false_reg1, &env->false_reg2, rev_opcode(opcode), is_jmp32);
+	/* If there is a range bounds violation in *any* of the abstract values in either
+	 * reg_states in the FALSE branch (i.e. false_reg1, false_reg2), the FALSE branch must be
+	 * dead. Only TRUE branch will be taken.
+	 */
+	if (range_bounds_violation(&env->false_reg1) || range_bounds_violation(&env->false_reg2))
+		return 1;
+	reg_bounds_sync(&env->false_reg1);
+	reg_bounds_sync(&env->false_reg2);
+	if (range_bounds_violation(&env->false_reg1) || range_bounds_violation(&env->false_reg2))
+		return 1;
+
+	/* Jump (TRUE) branch */
+	regs_refine_cond_op(&env->true_reg1, &env->true_reg2, opcode, is_jmp32);
+	/* If there is a range bounds violation in *any* of the abstract values in either
+	 * reg_states in the TRUE branch (i.e. true_reg1, true_reg2), the TRUE branch must be dead.
+	 * Only FALSE branch will be taken.
+	 */
+	if (range_bounds_violation(&env->true_reg1) || range_bounds_violation(&env->true_reg2))
+		return 0;
+	reg_bounds_sync(&env->true_reg1);
+	reg_bounds_sync(&env->true_reg2);
+	if (range_bounds_violation(&env->true_reg1) || range_bounds_violation(&env->true_reg2))
+		return 0;
+
+	/* Both branches are possible, we can't determine which one will be taken. */
+	return -1;
+}
+
 /*
  * <reg1> <op> <reg2>, currently assuming reg2 is a constant
  */
-static int is_scalar_branch_taken(struct bpf_reg_state *reg1, struct bpf_reg_state *reg2,
+static int is_scalar_branch_taken(struct bpf_verifier_env * env, 
+				  struct bpf_reg_state *reg1, struct bpf_reg_state *reg2,
 				  u8 opcode, bool is_jmp32)
 {
 	struct tnum t1 = is_jmp32 ? tnum_subreg(reg1->var_off) : reg1->var_off;
@@ -16868,7 +16920,7 @@ static int is_scalar_branch_taken(struct bpf_reg_state *reg1, struct bpf_reg_sta
 		break;
 	}
 
-	return -1;
+	return simulate_both_branches_taken(env, reg1, reg2, opcode, is_jmp32);
 }
 
 static int flip_opcode(u32 opcode)
@@ -16939,7 +16991,8 @@ static int is_pkt_ptr_branch_taken(struct bpf_reg_state *dst_reg,
  * -1 - unknown. Example: "if (reg1 < 5)" is unknown when register value
  *      range [0,10]
  */
-static int is_branch_taken(struct bpf_reg_state *reg1, struct bpf_reg_state *reg2,
+static int is_branch_taken(struct bpf_verifier_env* env,
+			   struct bpf_reg_state *reg1, struct bpf_reg_state *reg2,
 			   u8 opcode, bool is_jmp32)
 {
 	if (reg_is_pkt_pointer_any(reg1) && reg_is_pkt_pointer_any(reg2) && !is_jmp32)
@@ -16978,7 +17031,7 @@ static int is_branch_taken(struct bpf_reg_state *reg1, struct bpf_reg_state *reg
 	}
 
 	/* now deal with two scalars, but not necessarily constants */
-	return is_scalar_branch_taken(reg1, reg2, opcode, is_jmp32);
+	return is_scalar_branch_taken(env, reg1, reg2, opcode, is_jmp32);
 }
 
 /* Opcode that corresponds to a *false* branch condition.
@@ -17597,7 +17650,7 @@ static int check_cond_jmp_op(struct bpf_verifier_env *env,
 	}
 
 	is_jmp32 = BPF_CLASS(insn->code) == BPF_JMP32;
-	pred = is_branch_taken(dst_reg, src_reg, opcode, is_jmp32);
+	pred = is_branch_taken(env, dst_reg, src_reg, opcode, is_jmp32);
 	if (pred >= 0) {
 		/* If we get here with a dst_reg pointer type it is because
 		 * above is_branch_taken() special cased the 0 comparison.
