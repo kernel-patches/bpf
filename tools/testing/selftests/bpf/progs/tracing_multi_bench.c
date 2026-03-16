b79423dc29e00be07866c4c7ec0bc72fd0c61228
Jiri Olsa <jolsa@kernel.org>
2026-03-16 08:51:37 +0100

    selftests/bpf: Add tracing multi attach benchmark test
    Adding benchmark test that attaches to (almost) all allowed tracing
    functions and display attach/detach times.
    
      # ./test_progs -t tracing_multi_bench_attach -v
      bpf_testmod.ko is already unloaded.
      Loading bpf_testmod.ko...
      Successfully loaded bpf_testmod.ko.
      serial_test_tracing_multi_bench_attach:PASS:btf__load_vmlinux_btf 0 nsec
      serial_test_tracing_multi_bench_attach:PASS:tracing_multi_bench__open_and_load 0 nsec
      serial_test_tracing_multi_bench_attach:PASS:get_syms 0 nsec
      serial_test_tracing_multi_bench_attach:PASS:bpf_program__attach_tracing_multi 0 nsec
      serial_test_tracing_multi_bench_attach: found 51186 functions
      serial_test_tracing_multi_bench_attach: attached in   1.295s
      serial_test_tracing_multi_bench_attach: detached in   0.243s
      #507     tracing_multi_bench_attach:OK
      Summary: 1/0 PASSED, 0 SKIPPED, 0 FAILED
      Successfully unloaded bpf_testmod.ko.
    
    Exporting skip_entry as is_unsafe_function and usign it in the test.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

---
+++ tools/testing/selftests/bpf/progs/tracing_multi_bench.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <stdbool.h>
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+char _license[] SEC("license") = "GPL";
+
+SEC("fentry.multi")
+int BPF_PROG(bench)
+{
+	return 0;
+}
