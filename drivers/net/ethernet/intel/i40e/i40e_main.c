8b1db02b771f0de1600766f94884817caccabfd0
Kohei Enju <kohei@enjuk.jp>
2026-03-19 17:16:42 +0000

    i40e: prepare for XDP metadata ops support
    Prepare 'struct i40e_xdp_buff' that contains an xdp_buff and a pointer
    to i40e_rx_desc in order to pass the RX descriptor to the XDP kfuncs.
    Also in ZC path, use XSK_CHECK_PRIV_TYPE() to ensure i40e_xdp_buff
    doesn't exceed the offset of cb in xdp_buff_xsk.
    
    No functional changes.
    
    Signed-off-by: Kohei Enju <kohei@enjuk.jp>
    Reviewed-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Tested-by: Patryk Holda <patryk.holda@intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e_main.c
+++ drivers/net/ethernet/intel/i40e/i40e_main.c
@@ -3622,7 +3622,7 @@ static int i40e_configure_rx_ring(struct i40e_ring *ring)
 	}
 
 skip:
-	xdp_init_buff(&ring->xdp, xdp_frame_sz, &ring->xdp_rxq);
+	xdp_init_buff(&ring->xdp_ctx.xdp, xdp_frame_sz, &ring->xdp_rxq);
 
 	rx_ctx.dbuff = DIV_ROUND_UP(ring->rx_buf_len,
 				    BIT_ULL(I40E_RXQ_CTX_DBUFF_SHIFT));
