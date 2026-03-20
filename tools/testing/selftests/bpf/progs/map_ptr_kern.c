2e8a0c20db61d912bd68920b87ecbbf492dc483c
Menglong Dong <menglong8.dong@gmail.com>
2026-03-20 16:41:55 +0800

    selftests/bpf: test access to ringbuf position with map pointer
    Add the testing to access the bpf_ringbuf with the map pointer.
    "consumer_pos" and "producer_pos" is accessed in this testing. We reserve
    128 bytes in the ringbuf to test the producer_pos, which should be
    "128 + 8", and the "8" is BPF_RINGBUF_HDR_SZ.
    
    It will be helpful if we want to evaluate the usage of the ringbuf in bpf
    prog with the consumer and producer position.
    
    Signed-off-by: Menglong Dong <dongml2@chinatelecom.cn>

--- tools/testing/selftests/bpf/progs/map_ptr_kern.c
+++ tools/testing/selftests/bpf/progs/map_ptr_kern.c
@@ -647,20 +647,38 @@ static inline int check_devmap_hash(void)
 	return 1;
 }
 
+struct bpf_ringbuf {
+	unsigned long consumer_pos;
+	unsigned long producer_pos;
+} __attribute__((preserve_access_index));
+
 struct bpf_ringbuf_map {
 	struct bpf_map map;
+	struct bpf_ringbuf *rb;
 } __attribute__((preserve_access_index));
 
 struct {
 	__uint(type, BPF_MAP_TYPE_RINGBUF);
+	__uint(max_entries, 1 << 10);
 } m_ringbuf SEC(".maps");
 
 static inline int check_ringbuf(void)
 {
 	struct bpf_ringbuf_map *ringbuf = (struct bpf_ringbuf_map *)&m_ringbuf;
 	struct bpf_map *map = (struct bpf_map *)&m_ringbuf;
+	struct bpf_ringbuf *rb;
+	void *ptr;
 
 	VERIFY(check(&ringbuf->map, map, 0, 0, page_size));
+
+	ptr = bpf_ringbuf_reserve(&m_ringbuf, 128, 0);
+	VERIFY(ptr);
+
+	bpf_ringbuf_discard(ptr, 0);
+	rb = ringbuf->rb;
+	VERIFY(rb);
+	VERIFY(rb->consumer_pos == 0);
+	VERIFY(rb->producer_pos == 128 + 8);
 
 	return 1;
 }
