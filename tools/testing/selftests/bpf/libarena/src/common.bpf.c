3b461e34dd46564b94c441d1a9e84af7a72e818e
Emil Tsalapatis <emil@etsalapatis.com>
2026-04-03 00:27:17 -0400

    selftests/bpf: Add arena ASAN runtime to libarena
    Add an address sanitizer (ASAN) runtime to the arena library. The
    ASAN runtime implements the functions injected into BPF binaries
    by LLVM sanitization when ASAN is enabled during compilation.
    
    The runtime also includes functions called explicitly by memory
    allocation code to mark memory as poisoned/unpoisoned to ASAN.
    This code is a no-op when sanitization is turned off.
    
    Signed-off-by: Emil Tsalapatis <emil@etsalapatis.com>

--- tools/testing/selftests/bpf/libarena/src/common.bpf.c
+++ tools/testing/selftests/bpf/libarena/src/common.bpf.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: LGPL-2.1 OR BSD-2-Clause
 /* Copyright (c) 2026 Meta Platforms, Inc. and affiliates. */
 #include <common.h>
+#include <asan.h>
 
 const volatile u32 zero = 0;
 
