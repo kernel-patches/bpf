f7755486a7f5734df84c93947cdfb94303cdcb77
Alexis Lothoré (eBPF Foundation) <alexis.lothore@bootlin.com>
2026-04-10 17:20:31 +0200

    bpf: mark instructions accessing program stack
    In order to prepare to emit KASAN checks in JITed programs, JIT
    compilers need to be aware about whether some load/store instructions
    are targeting the bpf program stack, as those should not be monitored
    (we already have guard pages for that, and it is difficult anyway to
    correctly monitor any kind of data passed on stack).
    
    To support this need, make the BPF verifier mark the instructions
    depending on whether they could access or not memory other than stack:
    - add a setter that allows the verifier to mark instructions accessing
      non-stack memory
    - add a getter that allows JIT compilers to check whether instructions
      being JITed are accessing the stack _and only_ the stack. If no env is
      provided (eg this is a cBPF program), do a best-effort check based on
      source and destination registers.
    
    Signed-off-by: Alexis Lothoré (eBPF Foundation) <alexis.lothore@bootlin.com>
    ---
    Changes in v2:
    - invert marking logic to cover possible different reg types when the
      verifier covers different states
    - add a best-effort processing for classical bpf programs, inspecting
      directly src and dst registers

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -1560,6 +1560,8 @@ void bpf_jit_uncharge_modmem(u32 size);
 bool bpf_prog_has_trampoline(const struct bpf_prog *prog);
 bool bpf_insn_is_indirect_target(const struct bpf_verifier_env *env, const struct bpf_prog *prog,
 				 int insn_idx);
+bool bpf_insn_accesses_stack_only(const struct bpf_verifier_env *env,
+				  const struct bpf_prog *prog, int insn_idx);
 u16 bpf_out_stack_arg_cnt(const struct bpf_verifier_env *env, const struct bpf_prog *prog);
 #else
 static inline int bpf_trampoline_link_prog(struct bpf_tramp_link *link,
