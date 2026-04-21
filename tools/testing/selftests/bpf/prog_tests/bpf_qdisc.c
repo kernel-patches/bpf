4cc32c72091fd483522ac5419aa42639864e81c6
Amery Hung <ameryhung@gmail.com>
2026-04-21 15:10:15 -0700

    selftests/bpf: Test using slice after invalidating dynptr clone
    The parent object of a cloned dynptr is skb not the original dynptr.
    Invalidate the original dynptr should not prevent the program from
    using the slice derived from the cloned dynptr.
    
    Signed-off-by: Amery Hung <ameryhung@gmail.com>

--- tools/testing/selftests/bpf/prog_tests/bpf_qdisc.c
+++ tools/testing/selftests/bpf/prog_tests/bpf_qdisc.c
@@ -11,6 +11,7 @@
 #include "bpf_qdisc_fail__invalid_dynptr.skel.h"
 #include "bpf_qdisc_fail__invalid_dynptr_slice.skel.h"
 #include "bpf_qdisc_fail__invalid_dynptr_cross_frame.skel.h"
+#include "bpf_qdisc_dynptr_use_after_invalidate_clone.skel.h"
 
 #define LO_IFINDEX 1
 
@@ -229,6 +230,7 @@ void test_ns_bpf_qdisc(void)
 	RUN_TESTS(bpf_qdisc_fail__invalid_dynptr);
 	RUN_TESTS(bpf_qdisc_fail__invalid_dynptr_cross_frame);
 	RUN_TESTS(bpf_qdisc_fail__invalid_dynptr_slice);
+	RUN_TESTS(bpf_qdisc_dynptr_use_after_invalidate_clone);
 }
 
 void serial_test_bpf_qdisc_default(void)
