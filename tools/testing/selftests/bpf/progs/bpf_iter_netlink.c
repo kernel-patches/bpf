7643e0f73e2fc4a2784b64d1b1f8e7dd54b1602b
Suchit Karunakaran <suchitkarunakaran@gmail.com>
2026-04-23 00:07:57 +0530

    selftests/bpf: replace bpf_probe_read_kernel with direct access
    Use direct field access instead of bpf_probe_read_kernel() for
    nlk->groups[0] and inode->i_ino, and clean up related code.
    
    Removes obsolete FIXME comments and unused variable.
    
    Signed-off-by: Suchit Karunakaran <suchitkarunakaran@gmail.com>

--- tools/testing/selftests/bpf/progs/bpf_iter_netlink.c
+++ tools/testing/selftests/bpf/progs/bpf_iter_netlink.c
@@ -17,7 +17,6 @@ int dump_netlink(struct bpf_iter__netlink *ctx)
 	struct seq_file *seq = ctx->meta->seq;
 	struct netlink_sock *nlk = ctx->sk;
 	unsigned long group, ino;
-	struct inode *inode;
 	struct socket *sk;
 	struct sock *s;
 
@@ -35,10 +34,7 @@ int dump_netlink(struct bpf_iter__netlink *ctx)
 	if (!nlk->groups)  {
 		group = 0;
 	} else {
-		/* FIXME: temporary use bpf_probe_read_kernel here, needs
-		 * verifier support to do direct access.
-		 */
-		bpf_probe_read_kernel(&group, sizeof(group), &nlk->groups[0]);
+		group = nlk->groups[0];
 	}
 	BPF_SEQ_PRINTF(seq, "%-10u %08x %-8d %-8d %-5d %-8d ",
 		       nlk->portid, (u32)group,
@@ -50,12 +46,7 @@ int dump_netlink(struct bpf_iter__netlink *ctx)
 	if (!sk) {
 		ino = 0;
 	} else {
-		/* FIXME: container_of inside SOCK_INODE has a forced
-		 * type conversion, and direct access cannot be used
-		 * with current verifier.
-		 */
-		inode = SOCK_INODE(sk);
-		bpf_probe_read_kernel(&ino, sizeof(ino), &inode->i_ino);
+		ino = SOCK_INODE(sk)->i_ino;
 	}
 	BPF_SEQ_PRINTF(seq, "%-8u %-8lu\n", s->sk_drops.counter, ino);
 
