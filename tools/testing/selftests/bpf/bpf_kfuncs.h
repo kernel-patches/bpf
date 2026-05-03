102d625627c55311919d93a52294f43576bf7ffb
David Windsor <dwindsor@gmail.com>
2026-05-03 17:18:31 -0400

    selftests/bpf: add tests for bpf_init_inode_xattr kfunc
    Test bpf atomic inode xattr labeling in inode_init_security.
    
    Signed-off-by: David Windsor <dwindsor@gmail.com>

--- tools/testing/selftests/bpf/bpf_kfuncs.h
+++ tools/testing/selftests/bpf/bpf_kfuncs.h
@@ -92,4 +92,9 @@ extern int bpf_set_dentry_xattr(struct dentry *dentry, const char *name__str,
 				const struct bpf_dynptr *value_p, int flags) __ksym __weak;
 extern int bpf_remove_dentry_xattr(struct dentry *dentry, const char *name__str) __ksym __weak;
 
+struct lsm_xattr_ctx;
+extern int bpf_init_inode_xattr(struct lsm_xattr_ctx *xattr_ctx,
+				const char *name__str,
+				const struct bpf_dynptr *value_p) __ksym __weak;
+
 #endif
