bb73efec6fb4193008c73c7ca5cbc9b90ff45621
Martin KaFai Lau <martin.lau@kernel.org>
2026-05-05 12:30:22 -0700

    bpf: tcp: Support the tcp related BPF_SOCK_OPS_*_CB as a struct_ops
    In LSFMMBPF 2025, I have talked about moving the existing BPF_SOCK_OPS_*_CB
    to a struct_ops interface [1].
    
    One user experience on the service is that a
    BPF_PROG_TYPE_SOCK_OPS program usually has a long switch case to handle
    all CB it needs to handle. This is inconvenient but not big enough to
    move to the new struct_ops interface.
    
    We managed to add new CB and the switch case in a BPF_PROG_TYPE_SOCK_OPS program
    is getting longer. This is inconvenient to write the bpf prog but not
    necessarily a breaking point to retire the interface.
    
    When we managed to find new use cases and adding new CB, the interface started
    to show its teeth. The sock_ops->op is a runtime enum to tell where the
    bpf prog is called (e.g. BPF_SOCK_OPS_TCP_LISTEN_CB).
    While adding more CB flags, we find cases that break the earlier assumption
    that is shared by all CB flags. For example, not all call sites have
    the tcp_sock locked, so adding is_locked_tcp_sock logic. Not all call sites
    have skb, then we added kernel code to handle that. Not all helpers are safe
    for sock_ops->op, we also added kernel code for that. When the new CB flags
    need arguments other than sk, we also need to extend 'struct bpf_sock_ops_kern'.
    The existing 'union { u32 args[4]; u32 replylong[4]; }' is not reliable in
    passing args to bpf prog when there are multiple progs attached to a cgroup.
    
    The above has already been solved in struct_ops. struct_ops can
    provide a better interface to continue extending operations for sock.
    Each ops can define its own argument instead of initializing everything
    in 'struct bpf_sock_ops_kern'. Each ops can define what kfunc is allowed.
    
    The patch has moved some of the BPF_SOCK_OPS_* callbacks to bpf_tcp_ops.
    I don't think BASE_RTT is useful. NEEDS_ECN should be done in
    bpf-tcp-cc instead. The tstamp ones should be a separate
    struct_ops (e.g. "bpf_sock_ops") that can work in both TCP and UDP.
    
    TODOs:
    - ACTIVE_ESTABLISHED_CB and PASSIVE_ESTABLISHED_CB
    - BPF_SOCK_OPS_*HDR related ops
    - RTO and BASE_RTT
    
    [1]: https://drive.google.com/file/d/1wjKZth6T0llLJ_ONPAL_6Q_jbxbAjByp/view?usp=sharing
    
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -2998,8 +2998,10 @@ void tcp_set_state(struct sock *sk, int state)
 	 */
 	BTF_TYPE_EMIT_ENUM(BPF_TCP_ESTABLISHED);
 
-	if (BPF_SOCK_OPS_TEST_FLAG(tcp_sk(sk), BPF_SOCK_OPS_STATE_CB_FLAG))
+	if (BPF_SOCK_OPS_TEST_FLAG(tcp_sk(sk), BPF_SOCK_OPS_STATE_CB_FLAG)) {
 		tcp_call_bpf_2arg(sk, BPF_SOCK_OPS_STATE_CB, oldstate, state);
+		bpf_tcp_ops_call(set_state, sk, state);
+	}
 
 	switch (state) {
 	case TCP_ESTABLISHED:
