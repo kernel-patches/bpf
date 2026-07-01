99183bc6a2549d79476f414c56b1174ce7a6697a
Alexis Lothoré (eBPF Foundation) <alexis.lothore@bootlin.com>
2026-07-01 12:02:49 +0200

    bpf: propagate original instruction offset when patching program
    When the verifier patches an ebpf program with bpf_patch_insn_data, it
    then calls adjust_insn_aux_data to make sure that insn_aux_data takes
    into account the newly inserted patch. Some of the data offset is pretty
    straightforward to deduce, it is for example the case for
    indirect_target, as any patch affecting indirect calls will
    systematically move the original instruction to the end of the new
    patch.
    
    In order to introduce KASAN support for eBPF JIT, we need to mark any
    load/store instruction that access non-stack memory, but updating this
    new marking after a patch is not as straightforward as for indirect
    calls: the original BPF_ST/BPF_STX/BPF_LDX can be at the beginning, at
    the end or somewhere in the middle of the new patch: we then need some
    additional info to properly update this marking.
    
    Add a new parameter to bpf_patch_insn_data and adjust_insn_aux_data to
    convey the info about the new location in the patch of the original
    instruction. This info does not always make sense depending on the
    generated patch (eg, some bpf helpers being inlined by the verifier, and
    so turned into multiple new instructions without any BPF_CALL remaning),
    the parameter can then be set to -1. It will be used in next patches to
    properly handle insn_aux_data adjustment for the new KASAN
    instrumentation
    
    Signed-off-by: Alexis Lothoré (eBPF Foundation) <alexis.lothore@bootlin.com>

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -1542,7 +1542,7 @@ struct bpf_prog *bpf_jit_blind_constants(struct bpf_verifier_env *env, struct bp
 			continue;
 
 		if (env)
-			tmp = bpf_patch_insn_data(env, i, insn_buff, rewritten);
+			tmp = bpf_patch_insn_data(env, i, insn_buff, rewritten, rewritten - 1);
 		else
 			tmp = bpf_patch_insn_single(clone, i, insn_buff, rewritten);
 
