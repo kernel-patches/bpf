a0036da59f25d7a0001f3a072c07920334583324
Selvamani Rajagopal <Selvamani.Rajagopal@onsemi.com>
2026-07-05 15:59:51 -0700

    net: ethernet: oa_tc6: Carrier off when disable_traffic is set
    Previously, TX queue interface was stopped when
    disable_traffic flag was set. It is more appropriate to
    disable the queue as there is no recovery, once
    disable_traffic is set. Carrier is also marked off
    
    Fixes: b542d13fab0f ("net: ethernet: oa_tc6: Interrupt is active low, level triggered.")
    Signed-off-by: Selvamani Rajagopal <Selvamani.Rajagopal@onsemi.com>
    
    changes in v3
      - New patch. Carrier marked off once disable_traffic is set
    
    Signed-off-by: NipaLocal <nipa@local>

--- drivers/net/ethernet/oa_tc6.c
+++ drivers/net/ethernet/oa_tc6.c
@@ -730,6 +730,12 @@ static void oa_tc6_disable_traffic(struct oa_tc6 *tc6)
 	skb = oa_tc6_detach_waiting_tx_skb(tc6);
 	spin_unlock_bh(&tc6->tx_skb_lock);
 
+	/* disable_traffic, when set, is a point of no
+	 * return to working state. Better to mark
+	 * the carrier off.
+	 */
+	netif_carrier_off(tc6->netdev);
+	netif_tx_disable(tc6->netdev);
 	oa_tc6_drop_tx_skb(tc6, skb);
 	oa_tc6_free_ongoing_skbs(tc6);
 	oa_tc6_write_register(tc6, OA_TC6_REG_INT_MASK0, regval);
