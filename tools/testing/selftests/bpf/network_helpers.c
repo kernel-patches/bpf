bc86e6692a54ed4d51b030ae1a79906461461347
Alexis Lothoré (eBPF Foundation) <alexis.lothore@bootlin.com>
2026-07-10 17:30:57 +0200

    selftests/bpf: keep polling connection that is still in progress
    Some tests, like tc_tunnel or tc_edt, sporadically fail in CI with the
    following logs:
    
      (network_helpers.c:309: errno: Operation now in progress) \
        Failed to connect to server
      send_and_test_data:FAIL:connect to server unexpected error: -115
    
    This is due to SO_RCVTIMEO and SO_SNDTIMEO being set on the client
    socket (see settimeo() in client_socket()), allowing connect() to return
    an error and to set errno to EINPROGRESS instead of ETIMEDOUT.
    Increasing the timeout value for those tests is likely not a good
    solution (and it has already been done by commit 2790db208b44
    ("selftests/bpf: Improve tc_tunnel test reliability")): some tests
    expect some data transfer to fail, and so the timeout value would
    increase overall test execution duration again (not only the connection,
    but any socket operation).
    
    Another solution is to allocate a timeout budget specific to the
    connection: we can apply a larger timeout only for connections, and once
    the connection is established, set back the timeout configured through
    opts->timeout_ms; this would allow connection to succeed under heavy CI
    load, while keeping timeout reasonable for the rest of the test traffic.
    
    Set a larger SO_SNDTIMEO/SO_RCVTIMEO for the connection step, and reset
    it back to the timeout configured by the test once the connection has
    succeeded.
    
    Fixes: 99126abec5e5 ("bpf: selftests: A few improvements to network_helpers.c")
    Signed-off-by: Alexis Lothoré (eBPF Foundation) <alexis.lothore@bootlin.com>

--- tools/testing/selftests/bpf/network_helpers.c
+++ tools/testing/selftests/bpf/network_helpers.c
@@ -14,6 +14,7 @@
 #include <sys/types.h>
 #include <sys/un.h>
 #include <sys/eventfd.h>
+#include <sys/poll.h>
 
 #include <linux/err.h>
 #include <linux/in.h>
@@ -48,6 +49,8 @@
 				##__VA_ARGS__);				\
 			errno = __save;					\
 })
+
+#define CONNECT_MIN_TIMEOUT_MS	5000
 
 struct ipv4_packet pkt_v4 = {
 	.eth.h_proto = __bpf_constant_htons(ETH_P_IP),
@@ -291,10 +294,18 @@ error_close:
 	return -1;
 }
 
+static int connect_timeout_ms(const struct network_helper_opts *opts)
+{
+	/* Some tests require a timeout larger than the default connect
+	 * timeout
+	 */
+	return MAX(opts->timeout_ms, CONNECT_MIN_TIMEOUT_MS);
+}
+
 int connect_to_addr(int type, const struct sockaddr_storage *addr, socklen_t addrlen,
 		    const struct network_helper_opts *opts)
 {
-	int fd;
+	int fd, err;
 
 	if (!opts)
 		opts = &default_opts;
@@ -305,13 +316,29 @@ int connect_to_addr(int type, const struct sockaddr_storage *addr, socklen_t add
 		return -1;
 	}
 
-	if (connect(fd, (const struct sockaddr *)addr, addrlen)) {
-		log_err("Failed to connect to server");
-		save_errno_close(fd);
-		return -1;
+	if (settimeo(fd, connect_timeout_ms(opts))) {
+		log_err("Failed to set connect timeout");
+		goto close;
+	}
+
+	err = connect(fd, (const struct sockaddr *)addr, addrlen);
+
+	if (err) {
+		log_err("Failed to connect");
+		goto close;
+	}
+
+	if (opts->timeout_ms != CONNECT_MIN_TIMEOUT_MS &&
+	    settimeo(fd, opts->timeout_ms)) {
+		log_err("Failed to set timeout for connected socket");
+		goto close;
 	}
 
 	return fd;
+
+close:
+	save_errno_close(fd);
+	return -1;
 }
 
 int connect_to_addr_str(int family, int type, const char *addr_str, __u16 port,
