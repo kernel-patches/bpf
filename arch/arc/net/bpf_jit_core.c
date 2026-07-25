c2db1570c79878f32060b0033617c030a3e5c4a5
Pu Lehui <pulehui@huawei.com>
2026-07-25 10:20:18 +0000

    ARC, bpf: Fix memory leak in bpf_jit_free
    If a subprogram fails to JIT in jit_subprogs(), previously JITed
    subprograms are freed. However, the generic bpf_jit_free() fails to
    free their leftover jit_data, causing a memory leak.
    
    Implement a custom bpf_jit_free() to free this data.
    
    Fixes: f122668ddcce ("ARC: Add eBPF JIT support")
    Reported-by: Sashiko <sashiko-bot@kernel.org>
    Signed-off-by: Pu Lehui <pulehui@huawei.com>

--- arch/arc/net/bpf_jit_core.c
+++ arch/arc/net/bpf_jit_core.c
@@ -1412,3 +1412,22 @@ struct bpf_prog *bpf_int_jit_compile(struct bpf_verifier_env *env, struct bpf_pr
 
 	return prog;
 }
+
+void bpf_jit_free(struct bpf_prog *fp)
+{
+        if (fp->jited) {
+		struct bpf_jit_data *jit_data = fp->aux->jit_data;
+                struct bpf_binary_header *hdr = bpf_jit_binary_hdr(fp);
+
+		/* Cleanup for earlier subprogs if jit_subprogs() aborts */
+		if (jit_data) {
+			kfree(jit_data->bpf2insn)
+			kfree(jit_data);
+		}
+
+                bpf_jit_binary_free(hdr);
+                WARN_ON_ONCE(!bpf_prog_kallsyms_verify_off(fp));
+        }
+
+        bpf_prog_unlock_free(fp);
+}
