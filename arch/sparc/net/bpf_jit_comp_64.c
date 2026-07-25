3e6bf8ca93603fd2faa009bb1d5e9305a1b8eb7d
Pu Lehui <pulehui@huawei.com>
2026-07-25 10:20:17 +0000

    bpf, sparc64: Fix memory leak in bpf_jit_free
    If a subprogram fails to JIT in jit_subprogs(), previously JITed
    subprograms are freed. However, the generic bpf_jit_free() fails to
    free their leftover jit_data, causing a memory leak.
    
    Implement a custom bpf_jit_free() to free this data.
    
    Fixes: 5f5a64111639 ("bpf: sparc64: Add JIT support for multi-function programs.")
    Reported-by: Sashiko <sashiko-bot@kernel.org>
    Signed-off-by: Pu Lehui <pulehui@huawei.com>

--- arch/sparc/net/bpf_jit_comp_64.c
+++ arch/sparc/net/bpf_jit_comp_64.c
@@ -1611,3 +1611,22 @@ out_err:
 	}
 	goto out_off;
 }
+
+void bpf_jit_free(struct bpf_prog *fp)
+{
+	if (fp->jited) {
+		struct bpf_jit_data *jit_data = fp->aux->jit_data;
+		struct bpf_binary_header *hdr = bpf_jit_binary_hdr(fp);
+
+		/* Cleanup for earlier subprogs if jit_subprogs() aborts */
+		if (jit_data) {
+			kfree(jit_data->ctx.offset);
+			kfree(jit_data);
+		}
+
+		bpf_jit_binary_free(hdr);
+		WARN_ON_ONCE(!bpf_prog_kallsyms_verify_off(fp));
+	}
+
+	bpf_prog_unlock_free(fp);
+}
