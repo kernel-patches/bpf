67a77213cc7a1c08342eec3d3387dc4b217a99bd
Leon Hwang <leon.hwang@linux.dev>
2026-08-07 00:31:24 +0800

    selftests/bpf: Test verifier log for global percpu data
    Add two tests to verify the verifier log
    "R%d points to percpu_array map which cannot be used as const string\n".
    
    Reviewed-by: Emil Tsalapatis <emil@etsalapatis.com>
    Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

--- tools/testing/selftests/bpf/prog_tests/global_data_init.c
+++ tools/testing/selftests/bpf/prog_tests/global_data_init.c
@@ -296,6 +296,11 @@ static void test_global_percpu_data_rdonly_direct_write(void)
 	close(map_fd);
 }
 
+static void test_global_percpu_data_verifier_log(void)
+{
+	RUN_TESTS(test_global_percpu_data);
+}
+
 void test_global_percpu_data(void)
 {
 	if (!feat_supported(NULL, FEAT_PERCPU_DATA)) {
@@ -311,4 +316,5 @@ void test_global_percpu_data(void)
 		test_global_percpu_data_rdonly_direct_read();
 	if (test__start_subtest("rdonly_direct_write"))
 		test_global_percpu_data_rdonly_direct_write();
+	test_global_percpu_data_verifier_log();
 }
