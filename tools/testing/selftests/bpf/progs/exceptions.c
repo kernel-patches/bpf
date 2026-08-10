b94a80f108f7e10030485eb3b16af3cba6054171
Feng Jiang <jiangfeng@kylinos.cn>
2026-08-10 07:29:45 +0000

    selftests/bpf: Enable stack argument tests for RV64
    Enable the stack argument selftests for RV64. The riscv guard is
    restricted to __riscv_xlen == 64 since stack arguments are only
    supported by the RV64 JIT.
    
    Keep the __BPF_FEATURE_STACK_ARGUMENT gate, defined by clang with
    BPF stack argument support [1].
    
    [1] https://github.com/llvm/llvm-project/pull/189060
    
    Signed-off-by: Feng Jiang <jiangfeng@kylinos.cn>

--- tools/testing/selftests/bpf/progs/exceptions.c
+++ tools/testing/selftests/bpf/progs/exceptions.c
@@ -379,8 +379,9 @@ int exception_bad_assert_range_with(struct __sk_buff *ctx)
 	return 1;
 }
 
-#if (defined(__TARGET_ARCH_x86) || defined(__TARGET_ARCH_arm64)) \
-	&& defined(__BPF_FEATURE_STACK_ARGUMENT)
+#if (defined(__TARGET_ARCH_x86) || defined(__TARGET_ARCH_arm64) || \
+	(defined(__TARGET_ARCH_riscv) && __riscv_xlen == 64)) && \
+	defined(__BPF_FEATURE_STACK_ARGUMENT)
 
 const volatile bool has_stack_arg = true;
 
