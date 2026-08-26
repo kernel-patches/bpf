63ba2ad0a53245ef7d97835fb3febc499891f181
Slava Imameev <slava.imameev@crowdstrike.com>
2026-08-26 14:18:21 -0400

    libbpf: BPF program load type enum
    Replacing the boolean field with an enum simplifies the addition
    of new load types. Currently, the bpf_program structure defines
    the autoload type using a boolean field. This field is now
    replaced with an enum, allowing new BPF program loading types
    to be introduced by extending the enum value range.
    
    Signed-off-by: Slava Imameev <slava.imameev@crowdstrike.com>
    Signed-off-by: Andrey Grodzovsky <andrey.grodzovsky@crowdstrike.com>

--- tools/lib/bpf/libbpf.h
+++ tools/lib/bpf/libbpf.h
@@ -2078,6 +2078,21 @@ LIBBPF_API int libbpf_unregister_prog_handler(int handler_id);
  */
 LIBBPF_API int bpf_program__clone(struct bpf_program *prog, const struct bpf_prog_load_opts *opts);
 
+/**
+ * The program load type:
+ *
+ * - BPF_PROG_LOAD_TYPE_DISABLED: the program is not loaded.
+ * - BPF_PROG_LOAD_TYPE_AUTO: the program is autoloaded when the bpf_object is loaded.
+ */
+enum bpf_prog_load_type {
+	BPF_PROG_LOAD_TYPE_DISABLED = 0,
+	BPF_PROG_LOAD_TYPE_AUTO,
+};
+
+LIBBPF_API int bpf_program__set_load_type(struct bpf_program *prog,
+					  enum bpf_prog_load_type loadtype);
+LIBBPF_API enum bpf_prog_load_type bpf_program__load_type(const struct bpf_program *prog);
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
