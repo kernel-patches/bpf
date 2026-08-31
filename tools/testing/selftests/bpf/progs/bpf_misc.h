422d7386c6a2b9fb440053c741d389a5839c5d81
Abhishek Dubey <adubey@linux.ibm.com>
2026-08-31 12:46:04 +0530

    selftests/bpf: Enable verifier selftest for powerpc64
    Enable "__arch_powerpc64" specifier in verifier selftest
    for ppc64. Power 32-bit would require separate handling.
    Changes tested for 64-bit only.
    
    Signed-off-by: Abhishek Dubey <adubey@linux.ibm.com>
    Signed-off-by: Saket Kumar Bhaskar <skb99@linux.ibm.com>
    Reviewed-by: Hari Bathini <hbathini@linux.ibm.com>
    Tested-by: Yeswanth Krishna Tellakula <yeswanth@linux.ibm.com>

--- tools/testing/selftests/bpf/progs/bpf_misc.h
+++ tools/testing/selftests/bpf/progs/bpf_misc.h
@@ -165,6 +165,7 @@
 #define __arch_riscv64		__arch("RISCV64")
 #define __arch_s390x		__arch("s390x")
 #define __arch_loongarch	__arch("LOONGARCH")
+#define __arch_powerpc64	__arch("POWERPC64")
 #define __caps_unpriv(caps)	__test_tag("test_caps_unpriv=" EXPAND_QUOTE(caps))
 #define __load_if_JITed()	__test_tag("load_mode=jited")
 #define __load_if_no_JITed()	__test_tag("load_mode=no_jited")
