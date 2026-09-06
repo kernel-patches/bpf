c1ff425d625eb2a4d2967e9889b85f202f24eb5d
Bradley Morgan <brads@mainlining.org>
2026-09-06 22:33:58 +0000

    bpf: btf: Make alloc_obj_fields array const
    The alloc_obj_fields array is never modified. Make the array itself
    const, not just the strings it points to.
    
    Signed-off-by: Bradley Morgan <brads@mainlining.org>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/20260906223358.25717-1-brads@mainlining.org

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -5797,7 +5797,7 @@ static int btf_parse_hdr(struct btf_verifier_env *env)
 	return btf_check_sec_info(env, btf_data_size);
 }
 
-static const char *alloc_obj_fields[] = {
+static const char * const alloc_obj_fields[] = {
 	"bpf_spin_lock",
 	"bpf_list_head",
 	"bpf_list_node",
