fd5fb5cf2dc0588fcf86e47d8c2bf291671f179e
Yonghong Song <yonghong.song@linux.dev>
2026-09-08 23:25:59 -0700

    bpf: Prepare kfunc arguments for the JIT from an ABI description
    The previous patch refuses a kfunc argument of more than one eightbyte.
    This patch allows up to 16 byte kfunc arguments.
    
    But different architectures have different ways to map bpf calling
    convention (no gap, no backfill) to native convention. Rather than have
    each arch open-code where it wants an argument, describe the following
    common parameters where each architecture can set their specific items:
    
      struct bpf_jit_arg_abi {
            u8   nr_arg_regs;
            bool even_reg_align;
            bool even_stack_align;
            bool split_at_boundary;
            bool backfill_after_stack;
      };
    
    The above four booleans cover x86-64, arm64, RISC-V LP64, PowerPC64
    ELFv2 etc. The bpf_jit_place_args() and bpf_jit_plan_arg_moves()
    utilizes the above information to do proper work to be used in JIT
    later on.
    
    Signed-off-by: Yonghong Song <yonghong.song@linux.dev>

--- include/linux/bpf_verifier.h
+++ include/linux/bpf_verifier.h
@@ -1512,6 +1512,7 @@ enum btf_member_kind {
 
 bool btf_struct_is_composed_of(struct bpf_verifier_env *env, const struct btf *btf,
 			       const struct btf_type *t, u32 member_kinds);
+u32 btf_func_arg_align(const struct btf *btf, const struct btf_type *t);
 
 int bpf_find_subprog(struct bpf_verifier_env *env, int off);
 bool bpf_is_throw_kfunc(struct bpf_insn *insn);
