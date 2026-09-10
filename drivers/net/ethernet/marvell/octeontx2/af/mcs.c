9c59e57e3cd9e26601c4752c00555bd6cc28396e
Baha Mesleh <baha.mesleh@nokia.com>
2026-09-10 11:56:57 +0530

    octeontx2-af: mcs: Fix SC resource cleanup loop
    The SC resource cleanup loop was incorrectly iterating over secy.max
    instead of sc.max. Use sc.max as the loop bound.
    
    Fixes: cfc14181d497 ("octeontx2-af: cn10k: mcs: Manage the MCS block hardware resources")
    Signed-off-by: Baha Mesleh <baha.mesleh@nokia.com>
    Signed-off-by: Subrat Pandey <subratp@marvell.com>
    Signed-off-by: NipaLocal <nipa@local>

--- drivers/net/ethernet/marvell/octeontx2/af/mcs.c
+++ drivers/net/ethernet/marvell/octeontx2/af/mcs.c
@@ -314,7 +314,7 @@ int mcs_clear_all_stats(struct mcs *mcs, u16 pcifunc, int dir)
 	}
 
 	/* Clear SC stats */
-	for (id = 0; id < map->secy.max; id++) {
+	for (id = 0; id < map->sc.max; id++) {
 		if (map->sc2pf_map[id] != pcifunc)
 			continue;
 		mcs_clear_stats(mcs, MCS_SC_STATS, id, dir);
@@ -738,7 +738,7 @@ int mcs_free_all_rsrc(struct mcs *mcs, int dir, u16 pcifunc)
 	}
 
 	/* free sc entries */
-	for (id = 0; id < map->secy.max; id++) {
+	for (id = 0; id < map->sc.max; id++) {
 		if (map->sc2pf_map[id] != pcifunc)
 			continue;
 		mcs_free_rsrc(&map->sc, map->sc2pf_map, id, pcifunc);
