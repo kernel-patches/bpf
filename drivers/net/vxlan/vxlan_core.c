b932ab28e4c3f16cfa46eae1856af07d6da59ba6
Eric Dumazet <edumazet@google.com>
2026-09-11 06:22:00 +0000

    vxlan: no longer rely on RTNL in vxlan_fill_info()
    Now that vxlan->cfg is RCU-protected, we can update vxlan_fill_info()
    to run under RCU read lock instead of relying on RTNL.
    
    This completes the transition to RTNL-less link info dumping for VXLAN.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Kuniyuki Iwashima <kuniyu@google.com>
    Signed-off-by: NipaLocal <nipa@local>

--- drivers/net/vxlan/vxlan_core.c
+++ drivers/net/vxlan/vxlan_core.c
@@ -4725,7 +4725,8 @@ static int vxlan_fill_info(struct sk_buff *skb, const struct net_device *dev)
 	struct ifla_vxlan_port_range ports;
 	const struct vxlan_config *cfg;
 
-	cfg = rtnl_dereference(vxlan->cfg);
+	rcu_read_lock();
+	cfg = rcu_dereference(vxlan->cfg);
 
 	if (nla_put_u32(skb, IFLA_VXLAN_ID, be32_to_cpu(cfg->vni)))
 		goto nla_put_failure;
@@ -4824,9 +4825,11 @@ static int vxlan_fill_info(struct sk_buff *skb, const struct net_device *dev)
 		    &cfg->reserved_bits))
 		goto nla_put_failure;
 
+	rcu_read_unlock();
 	return 0;
 
 nla_put_failure:
+	rcu_read_unlock();
 	return -EMSGSIZE;
 }
 
