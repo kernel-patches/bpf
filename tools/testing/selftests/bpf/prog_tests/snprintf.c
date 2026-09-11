bc495e558f6e62dd738a0c5b7e708a23de5091da
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2026-09-11 16:50:11 +0200

    selftests/bpf: Replace %pK output with 0
    Networking used to print the socket pointer with %p as output in the
    /proc interface. This was later changed to %pK in order not to reveal
    the actual pointer value. The output format has then been copied to bpf
    tests which produce the same format including the %pK format modifier.
    
    Networking recently replaced the socket output with a plain 0 because
    the socket pointer added no value to the output and the %pK should be
    removed fom library handling. The format remained otherwise unchanged in
    order not to break any tools which are parsing this information.
    
    This change removes the %pK modifier from bpf, following the change in
    networking.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- tools/testing/selftests/bpf/prog_tests/snprintf.c
+++ tools/testing/selftests/bpf/prog_tests/snprintf.c
@@ -16,7 +16,7 @@
 #define MIN_SYM_RET  sizeof(EXP_SYM_OUT)
 
 /* The third specifier, %p, is a hashed pointer which changes on every reboot */
-#define EXP_ADDR_OUT "0000000000000000 ffff00000add4e55 "
+#define EXP_ADDR_OUT "ffff00000add4e55 "
 #define EXP_ADDR_RET sizeof(EXP_ADDR_OUT "unknownhashedptr")
 
 #define EXP_STR_OUT  "str1         a  b c      d e longstr"
