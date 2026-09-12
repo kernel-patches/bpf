7e244309879d85f83b492c7c129d2fb8e1549d3d
Quanye Yang <quanyeyang@proton.me>
2026-09-12 23:43:34 +0800

    tcp: annotate lockless access to sk->sk_err
    BUG: KCSAN: data-race in do_recvmmsg / mptcp_recvmsg
    
    read-write (marked) to 0xffff8880134d391c of 4 bytes by task 2619 on cpu 1:
     instrument_atomic_read_write include/linux/instrumented.h:113 [inline]
     sock_error include/net/sock.h:2565 [inline]
     do_recvmmsg+0x50c/0x580 net/socket.c:3049
     __sys_recvmmsg net/socket.c:3144 [inline]
     __do_sys_recvmmsg net/socket.c:3167 [inline]
     __se_sys_recvmmsg net/socket.c:3160 [inline]
     __x64_sys_recvmmsg+0x161/0x180 net/socket.c:3160
     x64_sys_call+0x19c7/0x1ca0 arch/x86/include/generated/asm/syscalls_64.h:300
     do_syscall_x64 arch/x86/entry/syscall_64.c:61 [inline]
     do_syscall_64+0xde/0x3d0 arch/x86/entry/syscall_64.c:84
     entry_SYSCALL_64_after_hwframe+0x77/0x7f
    
    read to 0xffff8880134d391c of 4 bytes by task 2620 on cpu 0:
     tcp_recv_should_stop include/net/tcp.h:3086 [inline]
     mptcp_recvmsg+0x54d/0xd50 net/mptcp/protocol.c:2466
     inet_recvmsg+0x204/0x210 net/ipv4/af_inet.c:894
     sock_recvmsg_nosec net/socket.c:1151 [inline]
     sock_recvmsg+0x11a/0x140 net/socket.c:1173
     ____sys_recvmsg+0x14b/0x3c0 net/socket.c:2933
     ___sys_recvmsg+0x116/0x160 net/socket.c:2975
     __sys_recvmsg net/socket.c:3008 [inline]
     __do_sys_recvmsg net/socket.c:3014 [inline]
     __se_sys_recvmsg net/socket.c:3011 [inline]
     __x64_sys_recvmsg+0xeb/0x160 net/socket.c:3011
     x64_sys_call+0x1319/0x1ca0 arch/x86/include/generated/asm/syscalls_64.h:48
     do_syscall_x64 arch/x86/entry/syscall_64.c:61 [inline]
     do_syscall_64+0xde/0x3d0 arch/x86/entry/syscall_64.c:84
     entry_SYSCALL_64_after_hwframe+0x77/0x7f
    
    value changed: 0x0000006b -> 0x00000000
    
    Reported by Kernel Concurrency Sanitizer on:
    CPU: 0 UID: 0 PID: 2620 Comm: syz.2.33 Not tainted 7.2.0-g39d4f32c5d53 #76 PREEMPT(full)
    Hardware name: QEMU Ubuntu 26.04 PC (i440FX + PIIX, 1996), BIOS 1.17.0-debian-1.17.0-1ubuntu1 04/01/2014
    
    do_recvmmsg() and getsockopt(SO_ERROR) call sock_error() without the
    socket lock. sock_error() clears sk_err with xchg(), which races with
    unmarked loads of the same field.
    
    KCSAN reported the unmarked peek in tcp_recv_should_stop(). The same
    lockless writer races with the other plain sk_err reads on the
    TCP/MPTCP send, recv and splice paths.
    
    Annotate those peeks with READ_ONCE(). No extra ordering is needed:
    the value is only used to decide whether I/O should stop. This does
    not consume sk_err; the check-then-sock_error() TOCTOU on the no-data
    paths is a separate issue.
    
    Suggested-by: Jiayuan Chen <jiayuan.chen@linux.dev>
    Link: https://lore.kernel.org/netdev/8bbee583-6f21-4817-bfeb-2d60057380a3@linux.dev/
    Reported-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Closes: https://github.com/multipath-tcp/mptcp_net-next/issues/632
    Signed-off-by: Quanye Yang <quanyeyang@proton.me>
    Signed-off-by: NipaLocal <nipa@local>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -846,7 +846,7 @@ ssize_t tcp_splice_read(struct socket *sock, loff_t *ppos,
 				break;
 			if (sock_flag(sk, SOCK_DONE))
 				break;
-			if (sk->sk_err) {
+			if (READ_ONCE(sk->sk_err)) {
 				ret = sock_error(sk);
 				break;
 			}
@@ -1230,7 +1230,7 @@ restart:
 	mss_now = tcp_send_mss(sk, &size_goal, flags);
 
 	err = -EPIPE;
-	if (sk->sk_err || (sk->sk_shutdown & SEND_SHUTDOWN))
+	if (READ_ONCE(sk->sk_err) || (sk->sk_shutdown & SEND_SHUTDOWN))
 		goto do_error;
 
 	while (msg_data_left(msg)) {
@@ -2762,7 +2762,7 @@ static int tcp_recvmsg_locked(struct sock *sk, struct msghdr *msg, size_t len,
 			if (sock_flag(sk, SOCK_DONE))
 				break;
 
-			if (sk->sk_err) {
+			if (READ_ONCE(sk->sk_err)) {
 				copied = sock_error(sk);
 				break;
 			}
