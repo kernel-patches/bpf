ca6ca3a0584020f22d9d11162b999d37aefc064f
Caleb James DeLisle <cjd@cjdns.fr>
2026-09-15 17:46:41 +0000

    net: dsa: mediatek: support MDIO switch downstream of MMIO switch
    The MT7530 / MT7531 can connect via either MDIO or MMIO. In MMIO mode
    the switch provides its own MDIO bus which connects to its integrated
    PHYs, but also can be connected to other things, such as another
    switch.
    
    If the MDIO bus contains another switch, then the upstream switch
    will register but it will not setup the DSA tree because the
    downstream switch is missing. Without the complete DSA tree,
    mt753x_setup() will not be called on the switch and therefore
    mt7530_setup_mdio() is never called so the downstream switch never
    registers.
    
    Update mt7530_setup_mdio() so that it can be called multiple times,
    export the symbol, and call it again after dsa_register_switch()
    from mt7988_probe().
    
    Signed-off-by: Caleb James DeLisle <cjd@cjdns.fr>
    Signed-off-by: NipaLocal <nipa@local>

--- drivers/net/dsa/mt7530-mmio.c
+++ drivers/net/dsa/mt7530-mmio.c
@@ -61,7 +61,14 @@ mt7988_probe(struct platform_device *pdev)
 	if (IS_ERR(priv->regmap))
 		return PTR_ERR(priv->regmap);
 
-	return dsa_register_switch(priv->ds);
+	ret = dsa_register_switch(priv->ds);
+
+	if (ret)
+		return ret;
+
+	mt7530_setup_mdio(priv);
+
+	return 0;
 }
 
 static void mt7988_remove(struct platform_device *pdev)
--- drivers/net/dsa/mt7530.c
+++ drivers/net/dsa/mt7530.c
@@ -2404,7 +2404,7 @@ mt7530_free_mdio_irq(struct mt7530_priv *priv)
 	}
 }
 
-static int
+int
 mt7530_setup_mdio(struct mt7530_priv *priv)
 {
 	struct device_node *mnp, *np = priv->dev->of_node;
@@ -2413,6 +2413,10 @@ mt7530_setup_mdio(struct mt7530_priv *priv)
 	struct mii_bus *bus;
 	static int idx;
 	int ret = 0;
+
+	/* Already done */
+	if (priv->child_bus)
+		return 0;
 
 	mnp = of_get_child_by_name(np, "mdio");
 
@@ -2438,9 +2442,6 @@ mt7530_setup_mdio(struct mt7530_priv *priv)
 	bus->parent = dev;
 	bus->phy_mask = ~ds->phys_mii_mask;
 
-	if (priv->irq_domain && !mnp)
-		mt7530_setup_mdio_irq(priv);
-
 	priv->child_bus = bus;
 
 	ret = devm_of_mdiobus_register(dev, bus, mnp);
@@ -2455,6 +2456,7 @@ out:
 	of_node_put(mnp);
 	return ret;
 }
+EXPORT_SYMBOL_GPL(mt7530_setup_mdio);
 
 static int
 mt7530_setup(struct dsa_switch *ds)
@@ -3284,6 +3286,9 @@ mt753x_setup(struct dsa_switch *ds)
 	ret = mt7530_setup_mdio(priv);
 	if (ret)
 		return ret;
+
+	if (priv->irq_domain && ds->user_mii_bus)
+		mt7530_setup_mdio_irq(priv);
 
 	/* Initialise the PCS devices */
 	for (i = 0; i < priv->ds->num_ports; i++) {
