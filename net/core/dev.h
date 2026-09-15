5ddff929c4c921db2109ca6950e8591fe07bcdf4
Jakub Kicinski <kuba@kernel.org>
2026-09-15 20:20:59 +0200

    net: Add netdev_config helpers
    netdev_config manipulation will become slightly more complicated
    soon and will be used by both ethtool and the queue API.
    Encapsulate the logic in helper functions.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Björn Töpel <bjorn@kernel.org>
    Reviewed-by: Breno Leitao <leitao@debian.org>
    Signed-off-by: NipaLocal <nipa@local>

--- net/core/dev.h
+++ net/core/dev.h
@@ -102,6 +102,11 @@ extern struct rw_semaphore dev_addr_sem;
 extern struct list_head net_todo_list;
 void netdev_run_todo(void);
 
+int netdev_alloc_config(struct net_device *dev);
+void __netdev_free_config(struct netdev_config *cfg);
+void netdev_free_config(struct net_device *dev);
+int netdev_reconfig_start(struct net_device *dev);
+
 int netdev_queue_config_validate(struct net_device *dev, int rxq_idx,
 				 struct netdev_queue_config *qcfg,
 				 struct netlink_ext_ack *extack);
