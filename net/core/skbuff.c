41ff38c33f499034ac639d128061b541d19d6f11
Shihuang Liu <shlomojune6@gmail.com>
2026-09-19 21:36:04 +0800

    net: skbuff: fix pull-bound underflow in skb_checksum_setup_ipv6()
    skb_maybe_pull_tail() subtracts skb_headlen(skb) from the unsigned max
    argument and passes the result to __pskb_pull_tail() as a signed int.  The
    function does not ensure that max is at least skb_headlen(skb).
    
    This can happen while parsing IPv6 extension headers when an skb already
    has a linear area larger than MAX_IPV6_HDR_LEN.  Once the parser needs data
    beyond the linear area, max - skb_headlen(skb) wraps and is converted to a
    negative delta.  __pskb_pull_tail() then passes that negative length to
    skb_copy_bits(), where it can become a very large copy length.
    
    Pass the requested length itself as the pull bound at the three
    extension-header call sites, so the delta can no longer go negative.
    
    Fixes: 1431fb31ecba ("xen-netback: fix fragment detection in checksum setup")
    Suggested-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Shihuang Liu <shlomojune6@gmail.com>
    Signed-off-by: NipaLocal <nipa@local>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -5992,7 +5992,8 @@ static int skb_checksum_setup_ipv6(struct sk_buff *skb, bool recalculate)
 			err = skb_maybe_pull_tail(skb,
 						  off +
 						  sizeof(struct ipv6_opt_hdr),
-						  MAX_IPV6_HDR_LEN);
+						  off +
+						  sizeof(struct ipv6_opt_hdr));
 			if (err < 0)
 				goto out;
 
@@ -6007,7 +6008,8 @@ static int skb_checksum_setup_ipv6(struct sk_buff *skb, bool recalculate)
 			err = skb_maybe_pull_tail(skb,
 						  off +
 						  sizeof(struct ip_auth_hdr),
-						  MAX_IPV6_HDR_LEN);
+						  off +
+						  sizeof(struct ip_auth_hdr));
 			if (err < 0)
 				goto out;
 
@@ -6022,7 +6024,8 @@ static int skb_checksum_setup_ipv6(struct sk_buff *skb, bool recalculate)
 			err = skb_maybe_pull_tail(skb,
 						  off +
 						  sizeof(struct frag_hdr),
-						  MAX_IPV6_HDR_LEN);
+						  off +
+						  sizeof(struct frag_hdr));
 			if (err < 0)
 				goto out;
 
