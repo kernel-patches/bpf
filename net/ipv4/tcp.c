b9d61abafebd9637980e8ef5ffff3a97a956edbf
Kuniyuki Iwashima <kuniyu@google.com>
2026-09-20 19:56:13 +0000

    tcp: Split out __tcp_set_rcvlowat().
    We will add a kfunc for bpf_tcp_ops.{enqueue,dequeue}_rcvq()
    to adjust sk->sk_rcvlowat.
    
    These hooks are triggered
    
      * when the TCP stack enqueues an skb to sk->sk_receive_queue
      * after data is dequeued from sk->sk_receive_queue
    
    In the enqueue path, tcp_data_ready() is always called after
    the hooks in tcp_queue_rcv() and tcp_ofo_queue().
    
    If tcp_set_rcvlowat() were used as is, tcp_data_ready() could
    be called twice for the same skb, which is redundant and also
    confusing.
    
    Let's split out __tcp_set_rcvlowat() and add a flag to control
    wakeup behaviour.
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@google.com>
    Acked-by: Stanislav Fomichev <sdf@fomichev.me>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -1828,8 +1828,7 @@ int tcp_peek_len(struct socket *sock)
 	return tcp_inq(sock->sk);
 }
 
-/* Make sure sk_rcvbuf is big enough to satisfy SO_RCVLOWAT hint */
-int tcp_set_rcvlowat(struct sock *sk, int val)
+int __tcp_set_rcvlowat(struct sock *sk, int val, bool wakeup)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
 	int space, cap;
@@ -1842,7 +1841,8 @@ int tcp_set_rcvlowat(struct sock *sk, int val)
 	WRITE_ONCE(sk->sk_rcvlowat, val ? : 1);
 
 	/* Check if we need to signal EPOLLIN right now */
-	tcp_data_ready(sk);
+	if (wakeup)
+		tcp_data_ready(sk);
 
 	if (sk->sk_userlocks & SOCK_RCVBUF_LOCK)
 		return 0;
@@ -1855,6 +1855,12 @@ int tcp_set_rcvlowat(struct sock *sk, int val)
 			WRITE_ONCE(tp->window_clamp, val);
 	}
 	return 0;
+}
+
+/* Make sure sk_rcvbuf is big enough to satisfy SO_RCVLOWAT hint */
+int tcp_set_rcvlowat(struct sock *sk, int val)
+{
+	return __tcp_set_rcvlowat(sk, val, true);
 }
 
 void tcp_set_rcvbuf(struct sock *sk, int val)
