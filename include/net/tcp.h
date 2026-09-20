fd475d4f8942a57e7190936b010c27cb941dfd77
Kuniyuki Iwashima <kuniyu@google.com>
2026-09-20 19:56:14 +0000

    bpf: mptcp: Don't support BPF_SOCK_OPS_RCVQ_CB_FLAG.
    The next patch exposes a new kfunc calling __tcp_set_rcvlowat()
    to bpf_tcp_ops.
    
    MPTCP has its own sock->ops->set_rcvlowat() / mptcp_set_rcvlowat(),
    so we should not allow calling __tcp_set_rcvlowat() on MPTCP
    subflows.
    
    Let's disable BPF_SOCK_OPS_RCVQ_CB_FLAG for MPTCP for now.
    
    If needed in the future, bpf_tcp_ops_set_rcvlowat() could be
    extended to properly support MPTCP.
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@google.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -2932,6 +2932,16 @@ static inline int tcp_call_bpf_3arg(struct sock *sk, int op, u32 arg1, u32 arg2,
 	return tcp_call_bpf(sk, op, 3, args);
 }
 
+static inline int tcp_set_sock_ops_cb_flags(struct sock *sk, int val)
+{
+	if (sk_is_mptcp(sk) &&
+	    (val & BPF_SOCK_OPS_RCVQ_CB_FLAG))
+		return -EOPNOTSUPP;
+
+	tcp_sk(sk)->bpf_sock_ops_cb_flags = val;
+	return 0;
+}
+
 static inline void tcp_clear_sock_ops_cb_flags(struct sock *sk)
 {
 	tcp_sk(sk)->bpf_sock_ops_cb_flags = 0;
@@ -2952,6 +2962,11 @@ static inline int tcp_call_bpf_3arg(struct sock *sk, int op, u32 arg1, u32 arg2,
 				    u32 arg3)
 {
 	return -EPERM;
+}
+
+static inline int tcp_set_sock_ops_cb_flags(struct sock *sk, int val)
+{
+	return -EOPNOTSUPP;
 }
 
 static inline void tcp_clear_sock_ops_cb_flags(struct sock *sk)
