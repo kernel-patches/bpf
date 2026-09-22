456a389f594a1a46a2913cf082579a9ceec481eb
Nathan Whitehorn <nwhitehorn@pa.msu.edu>
2026-09-22 11:09:24 -0400

    net: macb: Poll for link state changes when using the internal PCS.
    When used with an offboard transceiver (e.g. an SFP), link state needs
    to be measured at the PCS rather than an onboard PHY. Poll the link
    state when the PCS is active to allow the kernel to detect link state
    changes in this case.
    
    Signed-off-by: Nathan Whitehorn <nwhitehorn@pa.msu.edu>
    Reviewed-by: Théo Lebrun <theo.lebrun@bootlin.com>
    Link: https://patch.msgid.link/20260922151429.23272-2-nwhitehorn@pa.msu.edu
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/cadence/macb_main.c
+++ drivers/net/ethernet/cadence/macb_main.c
@@ -1025,6 +1025,7 @@ static int macb_mii_probe(struct net_device *netdev)
 	struct macb *bp = netdev_priv(netdev);
 
 	bp->phylink_sgmii_pcs.ops = &macb_phylink_pcs_ops;
+	bp->phylink_sgmii_pcs.poll = true;
 	bp->phylink_usx_pcs.ops = &macb_phylink_usx_pcs_ops;
 
 	bp->phylink_config.dev = &netdev->dev;
