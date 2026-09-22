0bf42f7adeaa4593419477238c01f1defced1e0a
James Hilliard <james.hilliard1@gmail.com>
2026-09-21 18:45:26 -0600

    net: stmmac: sun8i: add support for Allwinner H616 EMAC1
    The H616 secondary EMAC uses a separate system-control clock register
    and supports only RMII at 10/100 Mbps. It connects internally to the
    co-packaged AC200 or AC300 EPHY and has no external PHY pins.
    
    Add an EMAC1 variant using the dedicated register and enable only RMII.
    Leave PHY initialization to the PHY driver instead of using the H3
    internal-PHY controls. No RX or TX clock delays are configured for this
    RMII-only variant.
    
    Co-developed-by: Richard Genoud <richard.genoud@bootlin.com>
    Signed-off-by: Richard Genoud <richard.genoud@bootlin.com>
    Reviewed-by: Maxime Chevallier <maxime.chevallier@bootlin.com>
    Reviewed-by: Andre Przywara <andre.przywara@arm.com>
    Reviewed-by: Alastair D'Silva <alastair@d-silva.org>
    Tested-by: Alastair D'Silva <alastair@d-silva.org>
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: NipaLocal <nipa@local>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-sun8i.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-sun8i.c
@@ -81,6 +81,13 @@ static const struct reg_field sun8i_syscon_reg_field = {
 	.msb = 31,
 };
 
+/* EMAC1 clock register @ 0x34 in the "system control" address range */
+static const struct reg_field sun8i_syscon_reg_field_emac1 = {
+	.reg = 0x34,
+	.lsb = 0,
+	.msb = 31,
+};
+
 /* EMAC clock register @ 0x164 in the CCU address range */
 static const struct reg_field sun8i_ccu_reg_field = {
 	.reg = 0x164,
@@ -141,6 +148,13 @@ static const struct emac_variant emac_variant_h6 = {
 	.support_rgmii = true,
 	.rx_delay_max = 31,
 	.tx_delay_max = 7,
+};
+
+static const struct emac_variant emac_variant_h616_emac1 = {
+	.syscon_field = &sun8i_syscon_reg_field_emac1,
+	/* The co-packaged AC200/AC300 PHY does not use the H3 PHY controls. */
+	.soc_has_internal_phy = false,
+	.support_rmii = true,
 };
 
 #define EMAC_BASIC_CTL0 0x00
@@ -1281,6 +1295,8 @@ static const struct of_device_id sun8i_dwmac_match[] = {
 		.data = &emac_variant_a64 },
 	{ .compatible = "allwinner,sun50i-h6-emac",
 		.data = &emac_variant_h6 },
+	{ .compatible = "allwinner,sun50i-h616-emac1",
+		.data = &emac_variant_h616_emac1 },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, sun8i_dwmac_match);
