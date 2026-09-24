b46abdd07e3b7b095af5fad8b93d53763b5389dd
Harshitha Ramamurthy <hramamurthy@google.com>
2026-09-24 18:53:07 +0000

    gve: introduce ctrl ops to set vectors and Qs
    Introduce following ctrl ops for Adminq mode.
    
    - set_num_queues to set maximum and default TX/RX queues in the
      alloc structures used in the initialization flow.
    - set_num_ntfy_blks op to set number of vectors(ntfy_blks) in the
      initialization flow.
    
    This is done by attaching the adminq_ prefix to the existing
    functions placed in the gve_adminq.c file.
    
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Reviewed-by: Jordan Rhee <jordanrhee@google.com>
    Reviewed-by: Przemek Kitszel <przemyslaw.kitszel@intel.com>
    Signed-off-by: Harshitha Ramamurthy <hramamurthy@google.com>
    Signed-off-by: NipaLocal <nipa@local>

--- drivers/net/ethernet/google/gve/gve_adminq.h
+++ drivers/net/ethernet/google/gve/gve_adminq.h
@@ -656,8 +656,8 @@ int gve_adminq_report_nic_ts(struct gve_priv *priv,
 struct gve_ptype_lut;
 int gve_adminq_get_ptype_map_dqo(struct gve_priv *priv,
 				 struct gve_ptype_lut *ptype_lut);
-int gve_set_num_ntfy_blks(struct gve_priv *priv);
-void gve_set_num_queues(struct gve_priv *priv);
+int gve_adminq_set_num_ntfy_blks(struct gve_priv *priv);
+void gve_adminq_set_num_queues(struct gve_priv *priv);
 int gve_adminq_map_db_bar(struct gve_priv *priv);
 void gve_adminq_unmap_db_bar(struct gve_priv *priv);
 #endif /* _GVE_ADMINQ_H */
